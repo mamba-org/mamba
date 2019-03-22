feff41601167df3505f8f264572f7e5648d066aa
Wolf Vollprecht <w.vollprecht@gmail.com>
2019-03-22 14:51:09 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -67,10 +67,10 @@ class BuildExt(build_ext):
         ct = self.compiler.compiler_type
 
         if sys.platform == 'darwin':
-            c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
+            self.c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
             if not has_flag(self.compiler, '-std=c++17'):
-                c_opts.remove('-std=c++17')
-                c_opts.append('-std=c++1z')
+                self.c_opts.remove('-std=c++17')
+                self.c_opts.append('-std=c++1z')
 
         opts = self.c_opts.get(ct, [])
         if ct == 'unix':

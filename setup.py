fa83b40bfc94149a2838d6062ad1c2fa5e592343
Wolf Vollprecht <w.vollprecht@gmail.com>
2019-03-22 14:01:36 +0100

    fix has_flag call in setup.py

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ class BuildExt(build_ext):
 
     if sys.platform == 'darwin':
         c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
-        if not has_flag('-std=c++17'):
+        if not has_flag(self.compiler, '-std=c++17'):
             c_opts.remove('-std=c++17')
             c_opts.append('-std=c++1z')
 

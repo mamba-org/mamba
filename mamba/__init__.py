fb40a25c25cafabd5428b82b4e8ed4afddf4cfbc
Wolf Vollprecht <w.vollprecht@gmail.com>
2019-03-25 17:32:48 +0100

    futurize mamba

--- mamba/__init__.py
+++ mamba/__init__.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
 from ._version import version_info, __version__
--- mamba/mamba.py
+++ mamba/mamba.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2019, QuantStack
 # SPDX-License-Identifier: BSD-3-Clause
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 import sys, os
 
 from conda.cli.main import generate_parser, init_loggers

6431441f716885aaa2dd52938881c1342c749a65
Wolf Vollprecht <w.vollprecht@gmail.com>
2019-12-11 14:37:59 +0100

    add missing importg

--- mamba/FastSubdirData.py
+++ mamba/FastSubdirData.py
@@ -4,7 +4,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import bz2
-import sys
+import sys, os
 from collections import defaultdict
 from contextlib import closing
 from errno import EACCES, ENODEV, EPERM

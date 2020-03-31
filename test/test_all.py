74def84e5e9c3ebca86d0e7e96f4eee837aec4dc
David Brochart <david.brochart@gmail.com>
2020-03-31 13:41:24 +0200

    Make glibc virtual package testable

--- test/test_all.py
+++ test/test_all.py
@@ -1,6 +1,16 @@
 import subprocess
 
 
+def get_glibc_version():
+    try:
+        output = subprocess.check_output(['ldd', '--version'])
+    except:
+        return
+    output.splitlines()
+    version = output.splitlines()[0].split()[-1]
+    return version.decode('ascii')
+
+
 def run(exe, channels, package):
     cmd = [exe, 'create', '-n', 'xxx', '--override-channels', '--strict-channel-priority', '--dry-run']
     for channel in channels:
@@ -15,6 +25,17 @@ def run_mamba_conda(channels, package):
 
 
 def test_all():
+    version = get_glibc_version()
+    with open('test/channel_a/linux-64/repodata.tpl') as f:
+        repodata = f.read()
+    with open('test/channel_a/linux-64/repodata.json', 'w') as f:
+        if version is not None:
+            glibc_placeholder = ', "__glibc=' + version + '"'
+        else:
+            glibc_placeholder = ''
+        repodata = repodata.replace('GLIBC_PLACEHOLDER', glibc_placeholder)
+        f.write(repodata)
+
     channels = ['./test/channel_b', './test/channel_a']
     package = 'a'
     run_mamba_conda(channels, package)

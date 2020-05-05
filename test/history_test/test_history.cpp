6966015667de8345b89027f5a8485e26c4b8611e
Mariana Meireles <mariana@psychonautgirl.space>
2020-05-05 11:11:01 +0200

    Fix history test trim it

--- test/history_test/test_history.cpp
+++ test/history_test/test_history.cpp
@@ -8,49 +8,44 @@ namespace mamba
 {
     TEST(history, parse)
     {
-        History history_instance("../history_test/");
+        History history_instance("history_test/");
 
         std::vector<History::UserRequest> user_reqs = history_instance.get_user_requests();
 
-        std::ifstream history_file("../history_test/conda-meta/history");
+        std::ifstream history_file("history_test/conda-meta/history");
         std::stringstream original_history_buffer;
         std::string line;
         while (getline(history_file, line))
         {
             original_history_buffer << line;
         }
-
-        std::ifstream  src("../history_test/conda-meta/history", std::ios::binary);
-        std::ofstream  dst("../history_test/conda-meta/aux_file",   std::ios::binary);
-
-        dst << src.rdbuf();
-        src.close();
-        dst.close();
-
         history_file.close();
+
+        std::ifstream src_beg("history_test/conda-meta/history", std::ios::binary);
+        std::ofstream dst_beg("history_test/conda-meta/aux_file",   std::ios::binary);
+        dst_beg << src_beg.rdbuf();
+        src_beg.close();
+        dst_beg.close();
 
         std::stringstream check_buffer;
         check_buffer << original_history_buffer.str() << original_history_buffer.str();
 
         history_instance.add_entry(user_reqs);
 
-        history_file.open("../history_test/conda-meta/history");
+        history_file.open("history_test/conda-meta/history");
         std::stringstream updated_history_buffer;
         while (getline(history_file, line))
         {
             updated_history_buffer << line;
         }
-
-        std::cout << updated_history_buffer.str() << std::endl;
-        ASSERT_EQ(updated_history_buffer.str(), check_buffer.str());
-
         history_file.close();
 
-        std::ofstream  src_out("../history_test/conda-meta/history", std::ios::binary);
-        std::ifstream  dst_out("../history_test/conda-meta/aux_file",   std::ios::binary);
+        ASSERT_EQ(updated_history_buffer.str(), check_buffer.str());
 
-        src_out << dst_out.rdbuf();
-        src.close();
-        dst_out.close();
+        std::ofstream src_end("history_test/conda-meta/history", std::ios::binary);
+        std::ifstream dst_end("history_test/conda-meta/aux_file",   std::ios::binary);
+        src_end << dst_end.rdbuf();
+        src_end.close();
+        dst_end.close();
     }
 }

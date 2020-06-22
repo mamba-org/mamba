d27cd2d7078cc52ceafe7d4d7e5499eb2fba29be
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-06-22 17:17:26 +0200

    initial micromamba with fully static linking on linux

--- src/main.cpp
+++ src/main.cpp
@@ -38,8 +38,59 @@ static struct {
 } create_options;
 
 static struct {
+    bool ssl_verify = true;
+    std::string cacert_path;
+} network_options;
+
+static struct {
     int verbosity = 0;
 } global_options;
+
+
+void init_network_parser(CLI::App* subcom)
+{
+    subcom->add_option("--ssl_verify", network_options.ssl_verify, "Enable or disable SSL verification");
+    subcom->add_option("--cacert_path", network_options.cacert_path, "Path for CA Certificate");
+}
+
+void set_network_options(Context& ctx)
+{
+    std::array<std::string, 6> cert_locations {
+        "/etc/ssl/certs/ca-certificates.crt",                // Debian/Ubuntu/Gentoo etc.
+        "/etc/pki/tls/certs/ca-bundle.crt",                  // Fedora/RHEL 6
+        "/etc/ssl/ca-bundle.pem",                            // OpenSUSE
+        "/etc/pki/tls/cacert.pem",                           // OpenELEC
+        "/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem", // CentOS/RHEL 7
+        "/etc/ssl/cert.pem",                                 // Alpine Linux
+    };
+
+    // ssl verify can be either an empty string (regular SSL verification),
+    // the string "<false>" to indicate no SSL verification, or a path to
+    // a directory with cert files, or a cert file.
+    if (network_options.ssl_verify == false)
+    {
+        ctx.ssl_verify = "<false>";
+    }
+    else if (!network_options.cacert_path.empty())
+    {
+        ctx.ssl_verify = network_options.cacert_path;
+    }
+    else
+    {
+        for (const auto& loc : cert_locations)
+        {
+            if (fs::exists(loc))
+            {
+                ctx.ssl_verify = loc;
+            }
+        }
+        if (ctx.ssl_verify.empty())
+        {
+            LOG_WARNING << "No ca certificates found, disabling SSL verification";
+            ctx.ssl_verify = "<false>";
+        }
+    }
+}
 
 void init_shell_parser(CLI::App* subcom)
 {
@@ -154,22 +205,27 @@ void install_specs(const std::vector<std::string>& specs)
 void init_install_parser(CLI::App* subcom)
 {
     subcom->add_option("specs", create_options.specs, "Specs to install into the new environment");
+    init_network_parser(subcom);
+
     subcom->callback([&]() {
+        set_network_options(Context::instance());
         install_specs(create_options.specs);
     });
 }
-
 
 void init_create_parser(CLI::App* subcom)
 {
     subcom->add_option("specs", create_options.specs, "Specs to install into the new environment");
     subcom->add_option("-p,--prefix", create_options.prefix, "Path to the Prefix");
+    init_network_parser(subcom);
     // subcom->add_option("-n,--name" create_options.name, "Prefix name");
 
     subcom->callback([&]() {
         auto& ctx = Context::instance();
         ctx.set_verbosity(global_options.verbosity);
         ctx.target_prefix = create_options.prefix;
+
+        set_network_options(ctx);
 
         if (fs::exists(ctx.target_prefix))
         {

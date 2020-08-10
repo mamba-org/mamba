bca8ca107833ca6242948b718053a40e20d0bc03
Mariana Meireles <mariana@psychonautgirl.space>
2020-08-10 20:26:31 +0200

    Makes mamba easy to find to other projects dependable on it

--- include/mamba/version.hpp
+++ include/mamba/version.hpp
@@ -4,12 +4,17 @@
 //
 // The full license is in the file LICENSE, distributed with this software.
 
-#define MAMBA_VERSION_MAJOR @MAMBA_VERSION_MAJOR@
-#define MAMBA_VERSION_MINOR @MAMBA_VERSION_MINOR@
-#define MAMBA_VERSION_PATCH @MAMBA_VERSION_PATCH@
+#define MAMBA_VERSION_MAJOR 0
+#define MAMBA_VERSION_MINOR 4
+#define MAMBA_VERSION_PATCH 4
+
+// Binary version
+#define MAMBA_BINARY_CURRENT 1
+#define MAMBA_BINARY_REVISION 0
+#define MAMBA_BINARY_AGE 0
 
 #define MAMBA_VERSION (MAMBA_VERSION_MAJOR * 10000 + MAMBA_VERSION_MINOR * 100 + MAMBA_VERSION_PATCH)
-#define MAMBA_VERSION_STRING "@MAMBA_VERSION_MAJOR@.@MAMBA_VERSION_MINOR@.@MAMBA_VERSION_PATCH@"
+#define MAMBA_VERSION_STRING "0.4.4"
 
 extern const char mamba_version[];
 extern int mamba_version_major;

a33ee2808471ab9e51e1bb0d60fad782291b7c12
Matthew R. Becker <becker.mr@gmail.com>
2020-12-11 12:53:34 -0600

    do not do prerelease here

--- .ci_support/release_micromamba.py
+++ .ci_support/release_micromamba.py
@@ -55,7 +55,7 @@ if __name__ == "__main__":
     rel = _make_or_get_release(tag, repo)
 
     name = f"micromamba-{subdir}"
-    if platform == "win":
+    if subdir.startswith("win"):
         dest_pth = f"{name}\\Library\\bin\\micromamba.exe"
         src_pth = "micromamba.exe"
         dest_exe = name + "_" + subdir + ".exe"
@@ -68,10 +68,7 @@ if __name__ == "__main__":
         tf.add(src_pth, arcname=dest_pth)
 
     print("uploading the tar.bz2 asset...", flush=True)
-    ast = _make_or_upload_asset(f"micromamba-{platform}.tar.bz2", rel)
+    ast = _make_or_upload_asset(f"{name}.tar.bz2", rel)
 
     print("uploading the exe asset...", flush=True)
     ast = _make_or_upload_asset(src_pth, rel, name=dest_exe)
-
-    # if we get here, we mark the release as not a pre-release
-    rel.update_release(tag, "", prerelease=False)

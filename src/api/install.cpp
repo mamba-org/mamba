44b3ce7ee9e00c73ef9dfe87c59ff513f205d7da
Adrien DELSALLE <ad.delsalle@gmail.com>
2021-04-20 19:31:01 +0200

    remove confirmation when root prefix exists in shell init only create base when doing install operation refactor remove tests

--- src/api/install.cpp
+++ src/api/install.cpp
@@ -27,6 +27,7 @@ namespace mamba
     {
         auto& config = Configuration::instance();
 
+        config.at("create_base").set_value(true);
         config.at("use_target_prefix_fallback").set_value(true);
         config.at("target_prefix_checks")
             .set_value(MAMBA_ALLOW_EXISTING_PREFIX | MAMBA_NOT_ALLOW_MISSING_PREFIX

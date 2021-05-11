8b3a5c85c0e11def2e9b68e462f77f9a355170a3
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-05-11 13:34:32 +0200

    add simple package server for signed repodata

--- test/testserver.sh
+++ test/testserver.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 rm -rf $CONDA_PREFIX/pkgs/test-package*
 ENV_NAME=testauth
@@ -17,3 +17,12 @@ python reposerver.py -d repo/ --auth token & PID=$!
 mamba create -y -q -n $ENV_NAME --override-channels -c http://localhost:8000/t/xy-12345678-1234-1234-1234-123456789012 test-package --json
 kill -TERM $PID
 rm -rf $CONDA_PREFIX/envs/$ENV_NAME
+
+if [[ "$(uname -s)" == "Linux" ]]; then
+	export KEY1=$(gpg --fingerprint "MAMBA1")
+	export KEY2=$(gpg --fingerprint "MAMBA2")
+
+	python reposerver.py -d repo/ --auth none --sign & PID=$!
+	sleep 5s
+	kill -TERM $PID
+fi

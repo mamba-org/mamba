4faccd2bb35c5c7a12085a5d95879ba007bb7a42
Mariana Meireles <mariana@psychonautgirl.space>
2021-05-26 15:12:01 +0200

    Add file method and change the way we look for valid srcs, defaulting it to .condarc in the home dir

--- src/micromamba/config.cpp
+++ src/micromamba/config.cpp
@@ -5,13 +5,13 @@
 // The full license is in the file LICENSE, distributed with this software.
 
 #include <fstream>
-#include <cstdlib>
-#include <typeinfo>
+#include <filesystem>
 
 #include "common_options.hpp"
 
 #include "mamba/api/config.hpp"
 #include "mamba/api/configuration.hpp"
+#include "mamba/core/fsutil.hpp"
 
 #include <yaml-cpp/yaml.h>
 
@@ -137,18 +137,36 @@ set_config_describe_command(CLI::App* subcom)
     });
 }
 
+
+void
+set_config_file_command(CLI::App* subcom)
+{
+    auto& config = Configuration::instance();
+
+    auto& file_path = config.insert(Configurable("config_set_file_path", fs::path())
+                                        .group("cli")
+                                        .description("File path to set configuration"),
+                                    true);
+    subcom->add_option("--file", file_path.set_cli_config(fs::path()), file_path.description());
+
+    path::touch(file_path.value().string(), true);
+}
+
 void
 set_config_prepend_command(CLI::App* subcom)
 {
+    set_config_file_command(subcom);
+
     auto& config = Configuration::instance();
 
     auto& prepend_map = config.insert(
         Configurable("prepend_map", std::vector<std::string>({ "" }))
             .group("Output, Prompt and Flow Control")
             .description("Add one configuration value to the beginning of a list key"));
-
     subcom->add_option(
         "prepend_map", prepend_map.set_cli_config({ "" }), prepend_map.description());
+
+    auto& file_path = config.at("config_set_file_path").get_wrapped<fs::path>();
 
     subcom->callback([&]() {
         config.at("use_target_prefix_fallback").set_value(true);
@@ -158,54 +176,52 @@ set_config_prepend_command(CLI::App* subcom)
                        | MAMBA_ALLOW_NOT_ENV_PREFIX | MAMBA_NOT_EXPECT_EXISTING_PREFIX);
         config.load();
 
-        auto valid_srcs = config.valid_sources();
+        fs::path rc_source = env::expand_user(env::home_directory() / ".condarc");
         std::ofstream rc_file;
 
         std::string prepend_key = prepend_map.value().front();
         std::string prepend_value = prepend_map.value().back();
 
-        for (auto s : valid_srcs)
-        {
-            // convert rc file to YAML::Node
-            YAML::Node rc_YAML = YAML::LoadFile(env::expand_user(s).string());
+        YAML::Node rc_YAML = YAML::LoadFile(rc_source.string());
 
-            // look for append key in YAML
-            for (auto v : rc_YAML)
+        if (!file_path.value().string().empty())
+        {
+            rc_source = env::expand_user(file_path.value()).string();
+        }
+
+        // look for append key in YAML
+        for (auto v : rc_YAML)
+        {
+            if (v.first.as<std::string>() == prepend_key)
             {
-                if (v.first.as<std::string>() == prepend_key)
+                // if key was found, look for the same value
+                for (std::size_t j = 0; j < rc_YAML[prepend_key].size(); j++)
                 {
-                    // if key was found, look for the same value
-                    for (std::size_t j = 0; j < rc_YAML[prepend_key].size(); j++)
+                    if (rc_YAML[prepend_key][j].as<std::string>() == prepend_value)
                     {
-                        if (rc_YAML[prepend_key][j].as<std::string>() == prepend_value)
-                        {
-                            // if value exists, remove it so it can be prepended to end of the list
-                            // later on
-                            rc_YAML[prepend_key].remove(j);
-                            break;
-                        }
+                        // if value exists, remove it so it can be prepended to end of the list
+                        // later on
+                        rc_YAML[prepend_key].remove(j);
+                        break;
                     }
                 }
             }
-
-            if (is_key_valid(prepend_key))
-            {
-                // prepend value to the end of the chosen config key list
-                rc_YAML[prepend_key] = insert_yaml_sequence(rc_YAML[prepend_key], prepend_value);
-            }
-            else
-            {
-                std::cout << "Prepend key is invalid or is not present in the file" << std::endl;
-            }
-            // if the rc file is being modified, it's necessary to rewrite it
-            rc_file.open(env::expand_user(s).string(), std::ofstream::in | std::ofstream::trunc);
-            rc_file << rc_YAML << std::endl;
         }
 
-        if (valid_srcs.empty())
+        if (is_key_valid(prepend_key))
         {
-            std::cout << "No valid sources were found to apply these changes" << std::endl;
+            // prepend value to the end of the chosen config key list
+            rc_YAML[prepend_key] = insert_yaml_sequence(rc_YAML[prepend_key], prepend_value);
         }
+        else
+        {
+            std::cout << "Prepend key is invalid or is not present in file " << rc_source
+                      << std::endl;
+        }
+        // if the rc file is being modified, it's necessary to rewrite it
+        rc_file.open(rc_source.string(), std::ofstream::in | std::ofstream::trunc);
+        rc_file << rc_YAML << std::endl;
+
         config.operation_teardown();
     });
 }
@@ -213,14 +229,17 @@ set_config_prepend_command(CLI::App* subcom)
 void
 set_config_append_command(CLI::App* subcom)
 {
+    set_config_file_command(subcom);
+
     auto& config = Configuration::instance();
 
     auto& append_map
         = config.insert(Configurable("append_map", std::vector<std::string>({ "" }))
                             .group("Output, Prompt and Flow Control")
                             .description("Add one configuration value to the end of a list key"));
-
     subcom->add_option("append_map", append_map.set_cli_config({ "" }), append_map.description());
+
+    auto& file_path = config.at("config_set_file_path").get_wrapped<fs::path>();
 
     subcom->callback([&]() {
         config.at("use_target_prefix_fallback").set_value(true);
@@ -230,53 +249,52 @@ set_config_append_command(CLI::App* subcom)
                        | MAMBA_ALLOW_NOT_ENV_PREFIX | MAMBA_NOT_EXPECT_EXISTING_PREFIX);
         config.load();
 
-        auto valid_srcs = config.valid_sources();
+        fs::path rc_source = env::expand_user(env::home_directory() / ".condarc");
         std::ofstream rc_file;
 
         std::string append_key = append_map.value().front();
         std::string append_value = append_map.value().back();
 
-        for (auto s : valid_srcs)
-        {
-            // convert rc file to YAML::Node
-            YAML::Node rc_YAML = YAML::LoadFile(env::expand_user(s).string());
+        YAML::Node rc_YAML = YAML::LoadFile(rc_source.string());
 
-            // look for append key in YAML
-            for (auto v : rc_YAML)
+        if (!file_path.value().string().empty())
+        {
+            rc_source = env::expand_user(file_path.value()).string();
+        }
+
+        // look for append key in YAML
+        for (auto v : rc_YAML)
+        {
+            if (v.first.as<std::string>() == append_key)
             {
-                if (v.first.as<std::string>() == append_key)
+                // if key was found, look for the same value
+                for (std::size_t j = 0; j < rc_YAML[append_key].size(); j++)
                 {
-                    // if key was found, look for the same value
-                    for (std::size_t j = 0; j < rc_YAML[append_key].size(); j++)
+                    if (rc_YAML[append_key][j].as<std::string>() == append_value)
                     {
-                        if (rc_YAML[append_key][j].as<std::string>() == append_value)
-                        {
-                            // if value exists, remove it so it can be appended to end of the list
-                            // later on
-                            rc_YAML[append_key].remove(j);
-                            break;
-                        }
+                        // if value exists, remove it so it can be appended to end of the list
+                        // later on
+                        rc_YAML[append_key].remove(j);
+                        break;
                     }
                 }
             }
-
-            if (is_key_valid(append_key))
-            {
-                rc_YAML[append_key].push_back(append_value);
-            }
-            else
-            {
-                std::cout << "Append key is invalid or is not present in the file" << std::endl;
-            }
-
-            // if the rc file is being modified, it's necessary to rewrite it
-            rc_file.open(env::expand_user(s).string(), std::ofstream::in | std::ofstream::trunc);
-            rc_file << rc_YAML << std::endl;
         }
-        if (valid_srcs.empty())
+
+        if (is_key_valid(append_key))
         {
-            std::cout << "No valid sources were found to apply these changes" << std::endl;
+            rc_YAML[append_key].push_back(append_value);
         }
+        else
+        {
+            std::cout << "Append key is invalid or is not present in file " << rc_source
+                      << std::endl;
+        }
+
+        // if the rc file is being modified, it's necessary to rewrite it
+        rc_file.open(rc_source.string(), std::ofstream::in | std::ofstream::trunc);
+        rc_file << rc_YAML << std::endl;
+
         config.operation_teardown();
     });
 }
@@ -284,13 +302,16 @@ set_config_append_command(CLI::App* subcom)
 void
 set_config_remove_key_command(CLI::App* subcom)
 {
+    set_config_file_command(subcom);
+
     auto& config = Configuration::instance();
 
     auto& remove_key = config.insert(Configurable("remove_key", std::string(""))
                                          .group("Output, Prompt and Flow Control")
                                          .description("Remove a configuration key and its values"));
-
     subcom->add_option("remove_key", remove_key.set_cli_config(""), remove_key.description());
+
+    auto& file_path = config.at("config_set_file_path").get_wrapped<fs::path>();
 
     subcom->callback([&]() {
         config.at("use_target_prefix_fallback").set_value(true);
@@ -300,32 +321,38 @@ set_config_remove_key_command(CLI::App* subcom)
                        | MAMBA_ALLOW_NOT_ENV_PREFIX | MAMBA_NOT_EXPECT_EXISTING_PREFIX);
         config.load();
 
-        auto valid_srcs = config.valid_sources();
+        fs::path rc_source = env::expand_user(env::home_directory() / ".condarc");
         std::ofstream rc_file;
+        bool key_removed = false;
 
-        for (auto s : valid_srcs)
+        if (!file_path.value().string().empty())
         {
-            // convert rc file to YAML::Node
-            YAML::Node rc_YAML = YAML::LoadFile(env::expand_user(s).string());
+            rc_source = env::expand_user(file_path.value()).string();
+        }
 
-            // look for key to remove in file
-            for (auto v : rc_YAML)
+        // convert rc file to YAML::Node
+        YAML::Node rc_YAML = YAML::LoadFile(rc_source.string());
+
+        // look for key to remove in file
+        for (auto v : rc_YAML)
+        {
+            if (v.first.as<std::string>() == remove_key.value())
             {
-                if (v.first.as<std::string>() == remove_key.value())
-                {
-                    rc_YAML.remove(remove_key.value());
-                    break;
-                }
+                rc_YAML.remove(remove_key.value());
+                key_removed = true;
+                break;
             }
+        }
 
-            // if the rc file is being modified, it's necessary to rewrite it
-            rc_file.open(env::expand_user(s).string(), std::ofstream::in | std::ofstream::trunc);
-            rc_file << rc_YAML << std::endl;
-        }
-        if (valid_srcs.empty())
+        if (!key_removed)
         {
-            std::cout << "No valid sources were found to apply these changes" << std::endl;
+            std::cout << "Key " << remove_key.value() << " is not present in file " << rc_source
+                      << std::endl;
         }
+
+        // if the rc file is being modified, it's necessary to rewrite it
+        rc_file.open(rc_source.string(), std::ofstream::in | std::ofstream::trunc);
+        rc_file << rc_YAML << std::endl;
         config.operation_teardown();
     });
 }
@@ -333,13 +360,16 @@ set_config_remove_key_command(CLI::App* subcom)
 void
 set_config_set_command(CLI::App* subcom)
 {
+    set_config_file_command(subcom);
+
     auto& config = Configuration::instance();
 
-    auto& set_key = config.insert(Configurable("set_key", std::vector<std::string>({}))
-                                      .group("Output, Prompt and Flow Control")
-                                      .description("Set configuration value on rc file"));
+    auto& set_value = config.insert(Configurable("set_value", std::vector<std::string>({}))
+                                        .group("Output, Prompt and Flow Control")
+                                        .description("Set configuration value on rc file"));
+    subcom->add_option("set_value", set_value.set_cli_config({}), set_value.description());
 
-    subcom->add_option("set_key", set_key.set_cli_config({}), set_key.description());
+    auto& file_path = config.at("config_set_file_path").get_wrapped<fs::path>();
 
     subcom->callback([&]() {
         config.at("use_target_prefix_fallback").set_value(true);
@@ -349,24 +379,25 @@ set_config_set_command(CLI::App* subcom)
                        | MAMBA_ALLOW_NOT_ENV_PREFIX | MAMBA_NOT_EXPECT_EXISTING_PREFIX);
         config.load();
 
-        auto valid_srcs = config.valid_sources();
+        fs::path rc_source = env::expand_user(env::home_directory() / ".condarc");
         std::ofstream rc_file;
 
-        for (auto s : valid_srcs)
+        if (!file_path.value().string().empty())
         {
-            YAML::Node rc_YAML = YAML::LoadFile(env::expand_user(s).string());
-            if (is_key_valid(set_key.value()[0]))
-            {
-                rc_YAML[set_key.value()[0]] = set_key.value()[1];
-            }
-            // if the rc file is being modified, it's necessary to rewrite it
-            rc_file.open(env::expand_user(s).string(), std::ofstream::in | std::ofstream::trunc);
-            rc_file << rc_YAML << std::endl;
+            rc_source = env::expand_user(file_path.value().string());
         }
-        if (valid_srcs.empty())
+
+        YAML::Node rc_YAML = YAML::LoadFile(rc_source.string());
+
+        if (is_key_valid(set_value.value().at(0)))
         {
-            std::cout << "No valid sources were found to apply these changes" << std::endl;
+            rc_YAML[set_value.value().at(0)] = set_value.value().at(1);
         }
+
+        // if the rc file is being modified, it's necessary to rewrite it
+        rc_file.open(rc_source.string(), std::ofstream::in | std::ofstream::trunc);
+        rc_file << rc_YAML << std::endl;
+
         config.operation_teardown();
     });
 }
@@ -374,13 +405,16 @@ set_config_set_command(CLI::App* subcom)
 void
 set_config_get_command(CLI::App* subcom)
 {
+    set_config_file_command(subcom);
+
     auto& config = Configuration::instance();
 
-    auto& get_key = config.insert(Configurable("get_key", std::string(""))
-                                      .group("Output, Prompt and Flow Control")
-                                      .description("Display configuration value from rc file"));
+    auto& get_value = config.insert(Configurable("get_value", std::string(""))
+                                        .group("Output, Prompt and Flow Control")
+                                        .description("Display configuration value from rc file"));
+    subcom->add_option("get_value", get_value.set_cli_config(""), get_value.description());
 
-    subcom->add_option("get_key", get_key.set_cli_config(""), get_key.description());
+    auto& file_path = config.at("config_set_file_path").get_wrapped<fs::path>();
 
     subcom->callback([&]() {
         config.at("use_target_prefix_fallback").set_value(true);
@@ -390,7 +424,35 @@ set_config_get_command(CLI::App* subcom)
                        | MAMBA_ALLOW_NOT_ENV_PREFIX | MAMBA_NOT_EXPECT_EXISTING_PREFIX);
         config.load();
 
-        std::cout << config.dump(MAMBA_SHOW_CONFIG_VALUES, { get_key.value() }) << std::endl;
+        fs::path rc_source = env::expand_user(env::home_directory() / ".condarc");
+        std::ofstream rc_file;
+
+        bool value_found = false;
+
+        if (!file_path.value().string().empty())
+        {
+            rc_source = env::expand_user(file_path.value()).string();
+        }
+
+        // convert rc file to YAML::Node
+        YAML::Node rc_YAML = YAML::LoadFile(rc_source.string());
+
+        // look for key
+        for (auto v : rc_YAML)
+        {
+            if (v.first.as<std::string>() == get_value.value())
+            {
+                std::cout << get_value.value() << ":" << v.second << std::endl;
+                value_found = true;
+                break;
+            }
+        }
+
+        if (!value_found)
+        {
+            std::cout << "Key " << get_value.value() << " is not present in file " << rc_source
+                      << std::endl;
+        }
 
         config.operation_teardown();
     });

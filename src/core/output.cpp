3d9b80fe5daec5a6627869f0c0ac77e629ffcaff
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-07-06 15:15:58 +0200

    hide tokens

--- src/core/output.cpp
+++ src/core/output.cpp
@@ -13,6 +13,7 @@
 #include <algorithm>
 #include <cstdlib>
 #include <iostream>
+#include <regex>
 
 #include "mamba/core/output.hpp"
 #include "mamba/core/thread_utils.hpp"
@@ -271,6 +272,14 @@ namespace mamba
         return ConsoleStream();
     }
 
+    std::string Console::hide_secrets(const std::string& str)
+    {
+        std::string copy = str;
+        copy = std::regex_replace(copy, token_re, "/t/*****");
+        copy = std::regex_replace(copy, http_basicauth_re, "://$1:*****@");
+        return copy;
+    }
+
     void Console::print(const std::string_view& str, bool force_print)
     {
         if (!(Context::instance().quiet || Context::instance().json) || force_print)
@@ -278,11 +287,12 @@ namespace mamba
             const std::lock_guard<std::mutex> lock(instance().m_mutex);
             if (instance().p_progress_manager)
             {
-                instance().p_progress_manager->print(str, instance().skip_progress_bars());
+                instance().p_progress_manager->print(hide_secrets(str),
+                                                     instance().skip_progress_bars());
             }
             else
             {
-                std::cout << str << std::endl;
+                std::cout << hide_secrets(str) << std::endl;
             }
         }
     }

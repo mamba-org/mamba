b91c83022329fdeaf7134de2937fbef06c95723c
Adrien Delsalle <ad.delsalle@gmail.com>
2021-10-26 18:31:24 +0200

    fix linter (#1241)

--- libmamba/src/api/configuration.cpp
+++ libmamba/src/api/configuration.cpp
@@ -639,11 +639,11 @@ namespace mamba
 
         insert(
             Configurable("custom_multichannels", &ctx.custom_multichannels)
-                   .group("Channels")
-                   .set_rc_configurable()
-                   .description("Custom multichannels")
-                   .long_description(
-                       "A dictionary with name: list of names/urls to use for custom multichannels."));
+                .group("Channels")
+                .set_rc_configurable()
+                .description("Custom multichannels")
+                .long_description(
+                    "A dictionary with name: list of names/urls to use for custom multichannels."));
 
         insert(Configurable("override_channels_enabled", &ctx.override_channels_enabled)
                    .group("Channels")

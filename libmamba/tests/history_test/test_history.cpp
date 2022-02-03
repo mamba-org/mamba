85950694484dcb482075eb5a31cd786dfff57ff4
Jonas Haag <jonas@lophus.org>
2022-02-03 21:11:24 +0100

    Add test for #1441 (#1444)

--- libmamba/tests/history_test/test_history.cpp
+++ libmamba/tests/history_test/test_history.cpp
@@ -9,11 +9,11 @@ namespace mamba
 {
     TEST(history, parse)
     {
-        History history_instance("history_test/");
+        History history_instance("history_test/parse");
 
         std::vector<History::UserRequest> user_reqs = history_instance.get_user_requests();
 
-        std::ifstream history_file("history_test/conda-meta/history");
+        std::ifstream history_file("history_test/parse/conda-meta/history");
         std::stringstream original_history_buffer;
         std::string line;
         while (getline(history_file, line))
@@ -22,8 +22,8 @@ namespace mamba
         }
         history_file.close();
 
-        std::ifstream src_beg("history_test/conda-meta/history", std::ios::binary);
-        std::ofstream dst_beg("history_test/conda-meta/aux_file", std::ios::binary);
+        std::ifstream src_beg("history_test/parse/conda-meta/history", std::ios::binary);
+        std::ofstream dst_beg("history_test/parse/conda-meta/aux_file", std::ios::binary);
         dst_beg << src_beg.rdbuf();
         src_beg.close();
         dst_beg.close();
@@ -37,7 +37,7 @@ namespace mamba
             history_instance.add_entry(req);
         }
 
-        history_file.open("history_test/conda-meta/history");
+        history_file.open("history_test/parse/conda-meta/history");
         std::stringstream updated_history_buffer;
         while (getline(history_file, line))
         {
@@ -53,4 +53,20 @@ namespace mamba
         src_end.close();
         dst_end.close();
     }
+
+#ifndef _WIN32
+    TEST(history, parse_segfault)
+    {
+        pid_t child = fork();
+        if (child) {
+            int wstatus;
+            waitpid(child, &wstatus, 0);
+            ASSERT_TRUE(WIFEXITED(wstatus));
+        } else {
+            History history_instance("history_test/parse_segfault");
+            history_instance.get_user_requests();
+            exit(0);
+        }
+    }
+#endif
 }  // namespace mamba

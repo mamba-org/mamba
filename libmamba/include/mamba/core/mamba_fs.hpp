01be7f2d393fbc853b28c3eb8f9e4911fa8ecefe
Wolf Vollprecht <w.vollprecht@gmail.com>
2022-02-16 20:56:45 +0100

    initial powerloader integration

--- libmamba/include/mamba/core/mamba_fs.hpp
+++ libmamba/include/mamba/core/mamba_fs.hpp
@@ -1,13 +1,7 @@
 #ifndef MAMBA_CORE_FS_HPP
 #define MAMBA_CORE_FS_HPP
 
-#ifdef MAMBA_USE_STD_FS
 #include <filesystem>
 #include <fstream>
 namespace fs = std::filesystem;
-#else
-#include "ghc/filesystem.hpp"
-namespace fs = ghc::filesystem;
-#endif
-
 #endif

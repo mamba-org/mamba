d8cd9c540c31ff10f2582807edfb05c38164e1cc
John H. Ayad <johnhany97@gmail.com>
2022-04-01 08:40:16 +0100

    Improvement: Log the thrown error while validating extracted package cache (#1608)

--- libmamba/src/core/package_cache.cpp
+++ libmamba/src/core/package_cache.cpp
@@ -292,10 +292,16 @@ namespace mamba
                                 << "' has invalid 'repodata_record.json' file: " << e.what();
                     valid = false;
                 }
-                catch (...)
+                catch (const std::runtime_error& re)
                 {
                     LOG_WARNING << "Extracted package cache '" << extracted_dir.string()
-                                << "' is invalid'";
+                                << " couldn't be validated due to runtime error: " << re.what();
+                    valid = false;
+                }
+                catch (const std::exception& ex)
+                {
+                    LOG_WARNING << "Extracted package cache '" << extracted_dir.string()
+                                << " couldn't be validated due to error: " << ex.what();
                     valid = false;
                 }
 

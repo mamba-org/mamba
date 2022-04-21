29a20f88771f51a3e24fda9183d3711a3cae4234
Wolf Vollprecht <w.vollprecht@gmail.com>
2022-04-21 15:56:13 +0200

    fix function to throw if necessary

--- libmambapy/src/main.cpp
+++ libmambapy/src/main.cpp
@@ -269,7 +269,15 @@ PYBIND11_MODULE(bindings, m)
             },
             py::return_value_policy::reference)
         .def("loaded", &MSubdirData::loaded)
-        .def("cache_path", &MSubdirData::cache_path);
+        .def("cache_path",
+             [](const MSubdirData& self) -> std::string
+             {
+                 auto cp = self.cache_path();
+                 if (cp)
+                     return cp.value();
+                 else
+                     throw cp.error();
+             });
 
     m.def("cache_fn_url", &cache_fn_url);
     m.def("create_cache_dir", &create_cache_dir);

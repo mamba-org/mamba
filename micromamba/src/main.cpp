8e5e0c8743ddafe9c046cd951f94b247b045ba65
Joël Lamotte (Klaim) <joel.lamotte@quantstack.net>
2022-04-27 18:26:11 +0200

    formatting

--- micromamba/src/main.cpp
+++ micromamba/src/main.cpp
@@ -16,7 +16,7 @@
 #include "mamba/core/execution.hpp"
 #include "mamba/core/util_os.hpp"
 #include "mamba/core/util_scope.hpp"
-#include "../src/core/progress_bar_impl.hpp" // FIXME: HACK HACK HACK
+#include "../src/core/progress_bar_impl.hpp"  // FIXME: HACK HACK HACK
 
 #include <CLI/CLI.hpp>
 
@@ -28,9 +28,7 @@ int
 main(int argc, char** argv)
 {
     mamba::MainExecutor scoped_threads;
-    mamba::on_scope_exit _cleanup{ []{
-        Console::instance().progress_bar_manager().terminate();
-    }};
+    mamba::on_scope_exit _cleanup{ [] { Console::instance().progress_bar_manager().terminate(); } };
 
     init_console();
     auto& ctx = Context::instance();

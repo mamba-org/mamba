bab526ef3a667885142a2800deb9e7e37bf4a16f
AntoinePrv <AntoinePrv@users.noreply.github.com>
2022-07-20 15:45:00 +0200

    Change unreliable random source

--- mamba/tests/testserver.sh
+++ mamba/tests/testserver.sh
@@ -17,26 +17,21 @@ readonly this_pid="$$"
 # On exit, kill all subprocess and cleanup test directory.
 trap 'rm -rf "${test_dir}"; pkill -P ${this_pid} || true' EXIT
 
-# 10 random characters
-function make-name {
-	LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 10
-}
-
 
 python "${reposerver}" -d "${repo}" --auth none & PID=$!
-mamba create -y -q -n "$(make-name)" --override-channels -c http://localhost:8000/ test-package --json
+mamba create -y -q -n "env-${RANDOM}" --override-channels -c http://localhost:8000/ test-package --json
 kill -TERM $PID
 
 python "${reposerver}" -d "${repo}" --auth basic --user user --password test & PID=$!
-mamba create -y -q -n "$(make-name)" --override-channels -c http://user:test@localhost:8000/ test-package --json
+mamba create -y -q -n "env-${RANDOM}" --override-channels -c http://user:test@localhost:8000/ test-package --json
 kill -TERM $PID
 
 python "${reposerver}" -d "${repo}" --auth basic --user user@email.com --password test & PID=$!
-mamba create -y -q -n "$(make-name)" --override-channels -c http://user@email.com:test@localhost:8000/ test-package --json
+mamba create -y -q -n "env-${RANDOM}" --override-channels -c http://user@email.com:test@localhost:8000/ test-package --json
 kill -TERM $PID
 
 python "${reposerver}" -d "${repo}" --token xy-12345678-1234-1234-1234-123456789012 & PID=$!
-mamba create -y -q -n "$(make-name)" --override-channels -c http://localhost:8000/t/xy-12345678-1234-1234-1234-123456789012 test-package --json
+mamba create -y -q -n "env-${RANDOM}" --override-channels -c http://localhost:8000/t/xy-12345678-1234-1234-1234-123456789012 test-package --json
 kill -TERM $PID
 
 if [[ "$(uname -s)" == "Linux" ]]; then
@@ -51,7 +46,7 @@ fi
 python "${reposerver}" -d "${repo}" --auth basic --user '' --password test --port 8005 & PID=$!
 python "${reposerver}" -d "${repo}" --auth basic --user '' --password test --port 8006 & PID2=$!
 python "${reposerver}" -d "${repo}" --auth basic --user '' --password test --port 8007 & PID3=$!
-mamba create -y -q -n "$(make-name)" --override-channels -c http://:test@localhost:8005/ -c http://:test@localhost:8006/ -c http://:test@localhost:8007/ test-package --json
+mamba create -y -q -n "env-${RANDOM}" --override-channels -c http://:test@localhost:8005/ -c http://:test@localhost:8006/ -c http://:test@localhost:8007/ test-package --json
 kill -TERM $PID
 kill -TERM $PID2
 kill -TERM $PID3

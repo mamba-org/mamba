fc57b6064d8f4f6e1d64c5e070e7b4b21dde3b52
crogoz <crogoz@palantir.com>
2022-08-09 19:27:43 +0300

    move to cpp

--- libmamba/include/mamba/core/solver_problems.hpp
+++ libmamba/include/mamba/core/solver_problems.hpp
@@ -39,33 +39,6 @@ namespace mamba
         std::optional<std::string> dep() const;
     };
 
-    inline std::string MSolverProblem::to_string() const
-    {
-        return solver_problemruleinfo2str(
-            solver, (SolverRuleinfo) type, source_id, target_id, dep_id);
-    }
-
-    inline std::optional<PackageInfo> MSolverProblem::target() const
-    {
-        if (target_id == 0 || target_id >= solver->pool->nsolvables)
-            return std::nullopt;
-        return pool_id2solvable(solver->pool, target_id);
-    }
-
-    inline std::optional<PackageInfo> MSolverProblem::source() const
-    {
-        if (source_id == 0 || source_id >= solver->pool->nsolvables)
-            return std::nullopt;
-        return pool_id2solvable(solver->pool, source_id);
-    }
-
-    inline std::optional<std::string> MSolverProblem::dep() const
-    {
-        if (!dep_id)
-            return std::nullopt;
-        return pool_dep2str(solver->pool, dep_id);
-    }
-
 }  // namespace mamba
 
 #endif  // MAMBA_SOLVER_PROBLEMS_HPP

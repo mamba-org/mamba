c95fe219ccfbe64d810d3dce80a2dd3289d8f7d4
Joël Lamotte (Klaim) <joel.lamotte@quantstack.net>
2022-09-05 18:27:02 +0200

    WIP: deactivate test that need to be rewritten using powerloader

--- libmamba/tests/test_transfer.cpp
+++ libmamba/tests/test_transfer.cpp
@@ -4,39 +4,39 @@
 
 namespace mamba
 {
-    TEST(transfer, file_not_exist)
-    {
-#ifdef __linux__
-        Context::instance().quiet = true;
-        {
-            const mamba::Channel& c = mamba::make_channel("conda-forge");
-            mamba::MultiDownloadTarget multi_dl;
-            mamba::MultiPackageCache pkg_cache({ "/tmp/" });
-            mamba::MSubdirData cf
-                = mamba::MSubdirData::create(
-                      c, "linux-64", "file:///nonexistent/repodata.json", pkg_cache)
-                      .value();
-            multi_dl.add(cf.target());
+//     TEST(transfer, file_not_exist)
+//     {
+// #ifdef __linux__
+//         Context::instance().quiet = true;
+//         {
+//             const mamba::Channel& c = mamba::make_channel("conda-forge");
+//             mamba::MultiDownloadTarget multi_dl;
+//             mamba::MultiPackageCache pkg_cache({ "/tmp/" });
+//             mamba::MSubdirData cf
+//                 = mamba::MSubdirData::create(
+//                       c, "linux-64", "file:///nonexistent/repodata.json", pkg_cache)
+//                       .value();
+//             multi_dl.add(cf.target());
 
-            // file:// url should not retry
-            EXPECT_EQ(cf.target()->can_retry(), false);
+//             // file:// url should not retry
+//             EXPECT_EQ(cf.target()->can_retry(), false);
 
-            multi_dl.download(MAMBA_DOWNLOAD_FAILFAST);
+//             multi_dl.download(MAMBA_DOWNLOAD_FAILFAST);
 
-            // File does not exist
-            EXPECT_EQ(cf.target()->result, 37);
-        }
-        {
-            const mamba::Channel& c = mamba::make_channel("conda-forge");
-            mamba::MultiDownloadTarget multi_dl;
-            mamba::MultiPackageCache pkg_cache({ "/tmp/" });
-            mamba::MSubdirData cf = mamba::MSubdirData::create(
-                                        c, "noarch", "file:///nonexistent/repodata.json", pkg_cache)
-                                        .value();
-            multi_dl.add(cf.target());
-            EXPECT_THROW(multi_dl.download(MAMBA_DOWNLOAD_FAILFAST), std::runtime_error);
-        }
-        Context::instance().quiet = false;
-#endif
-    }
+//             // File does not exist
+//             EXPECT_EQ(cf.target()->result, 37);
+//         }
+//         {
+//             const mamba::Channel& c = mamba::make_channel("conda-forge");
+//             mamba::MultiDownloadTarget multi_dl;
+//             mamba::MultiPackageCache pkg_cache({ "/tmp/" });
+//             mamba::MSubdirData cf = mamba::MSubdirData::create(
+//                                         c, "noarch", "file:///nonexistent/repodata.json", pkg_cache)
+//                                         .value();
+//             multi_dl.add(cf.target());
+//             EXPECT_THROW(multi_dl.download(MAMBA_DOWNLOAD_FAILFAST), std::runtime_error);
+//         }
+//         Context::instance().quiet = false;
+// #endif
+//     }
 }  // namespace mamba

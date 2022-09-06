1cda6ce09335c4fe584ba0cd4931ef099c157fe9
Adrian Freund <adrian.freund@quantco.com>
2022-09-06 15:16:45 +0200

    Add docstring to dump_proxy_connections.py

--- micromamba/tests/dump_proxy_connections.py
+++ micromamba/tests/dump_proxy_connections.py
@@ -1,3 +1,13 @@
+"""
+mitmproxy connection dumper plugin
+
+This script shouldn't be run or imported directly. Instead, it should be passed to mitmproxy as a script (-s).
+It will then dump all request urls in the file specified by the outfile option.
+
+We use this script instead of letting mitmdump do the dumping, because we only care about the urls, while mitmdump
+also dumps all message content.
+"""
+
 from mitmproxy import ctx
 from mitmproxy.addonmanager import Loader
 from mitmproxy.http import HTTPFlow

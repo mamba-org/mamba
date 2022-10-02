e14b8e4a089d3706b181c435195f85c447718b91
Pavel Zwerschke <pavel.zwerschke@quantco.com>
2022-10-02 20:18:33 +0200

    revert stuff

--- .github/workflows/base.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-export PYTHON_VERSION=$1
-
-# Enable 'conda' command to make 'conda activate' work
-CONDA_BASE=$(conda info --base)
-source ${CONDA_BASE}/etc/profile.d/conda.sh
--- .github/workflows/macos-conda-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -exo pipefail
-
-# Don't test cross-compiled result (there is no emulation) and use the latest MacOS SDK.
-if grep -q "osx-arm64" .ci_support/${CONDA_BUILD_YML}.yaml; then
-  CONDA_BUILD_ARGS="--no-test"
-fi
-conda mambabuild -m .ci_support/${CONDA_BUILD_YML}.yaml conda.recipe ${CONDA_BUILD_ARGS:-}

923e804fd3fde381a436c66d2c1be6812aac7e91
Pavel Zwerschke <pavel.zwerschke@quantco.com>
2022-10-02 19:00:15 +0200

    does this work?

--- .github/workflows/macos-conda-build.sh
+++ .github/workflows/macos-conda-build.sh
@@ -2,14 +2,8 @@
 
 set -exo pipefail
 
-mamba install -y conda-build
 # Don't test cross-compiled result (there is no emulation) and use the latest MacOS SDK.
 if grep -q "osx-arm64" .ci_support/${CONDA_BUILD_YML}.yaml; then
   CONDA_BUILD_ARGS="--no-test"
-#  export CONDA_BUILD_SYSROOT=$(xcrun --sdk macosx --show-sdk-path)
-#  cat <<EOF >> .ci_support/${CONDA_BUILD_YML}.yaml
-#CONDA_BUILD_SYSROOT:
-# - "${CONDA_BUILD_SYSROOT}"
-#EOF
 fi
 conda mambabuild -m .ci_support/${CONDA_BUILD_YML}.yaml conda.recipe ${CONDA_BUILD_ARGS:-}

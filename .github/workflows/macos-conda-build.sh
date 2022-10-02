ff12a50a88eb92ff9e4032617de55bee4d01504d
Pavel Zwerschke <pavel.zwerschke@quantco.com>
2022-10-02 17:00:48 +0200

    move to p-w-m

--- .github/workflows/macos-conda-build.sh
+++ .github/workflows/macos-conda-build.sh
@@ -12,4 +12,4 @@ if grep -q "osx-arm64" .ci_support/${CONDA_BUILD_YML}.yaml; then
 # - "${CONDA_BUILD_SYSROOT}"
 #EOF
 fi
-conda build -m .ci_support/${CONDA_BUILD_YML}.yaml conda.recipe ${CONDA_BUILD_ARGS:-}
+micromamba mambabuild -m .ci_support/${CONDA_BUILD_YML}.yaml conda.recipe ${CONDA_BUILD_ARGS:-}

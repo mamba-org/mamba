d830f24ebd1df0fbd78d23c3dfa9e4bf0d4df1fb
Pavel Zwerschke <pavel.zwerschke@quantco.com>
2022-10-02 16:04:57 +0200

    Add powerpc

--- .github/workflows/conda-build.sh
+++ .github/workflows/conda-build.sh
@@ -4,8 +4,13 @@ set -eo pipefail
 
 export CONDA_BUILD_YML=$1
 
+# add --no-test if we are not building for x86
+if [[ $CONDA_BUILD_YML != linux_64_ ]]; then
+  CONDA_BUILD_ARGS="--no-test"
+fi
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${SCRIPT_DIR}/base.sh $*
 conda activate base
 mamba install -y conda-build
-conda build -m .ci_support/${CONDA_BUILD_YML}.yaml conda.recipe
+conda build -m .ci_support/${CONDA_BUILD_YML}.yaml conda.recipe ${CONDA_BUILD_ARGS:-}

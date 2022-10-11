41e91e5906f76609f737e6b84a3f6ab1c38b7036
Hind Montassif <hind.montassif@gmail.com>
2022-10-11 10:42:40 +0200

    Add test env remove

--- micromamba/tests/test_env.py
+++ micromamba/tests/test_env.py
@@ -124,3 +124,19 @@ class TestEnv:
         assert ret["name"] == env_name
         assert set(ret["channels"]) == {"https://conda.anaconda.org/conda-forge"}
         assert "micromamba=0.24.0=0" in ret["dependencies"]
+
+    def test_env_remove(self):
+        env_name = "env-create-remove"
+        # Create env with xtensor
+        res = create("xtensor", "-n", env_name, "--json", no_dry_run=True)
+
+        env_json = run_env("list", "--json")
+        env_fp = str(self.root_prefix / "envs" / env_name)
+        assert env_fp in env_json["envs"]
+        assert Path(env_fp).expanduser().exists()
+
+        run_env("remove", "-n", env_name, "-y")
+        env_json = run_env("list", "--json")
+        env_fp = str(self.root_prefix / "envs" / env_name)
+        assert env_fp not in env_json["envs"]
+        assert not Path(env_fp).expanduser().exists()

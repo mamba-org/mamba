80d94152229aa128f9f203b8344f9573caf6b70d
AntoinePrv <AntoinePrv@users.noreply.github.com>
2022-11-22 11:46:53 +0100

    Fix Pybind init

--- libmambapy/src/main.cpp
+++ libmambapy/src/main.cpp
@@ -264,7 +264,7 @@ PYBIND11_MODULE(bindings, m)
         .def_readonly_static("problem_type", &PbGraph::ConstraintNode::problem_type);
 
     py::class_<PbGraph::conflicts_t>(pyPbGraph, "ConflictMap")
-        .def(py::init<>(), []() { return PbGraph::conflicts_t(); })
+        .def(py::init([]() { return PbGraph::conflicts_t(); }))
         .def("__len__", [](PbGraph::conflicts_t const& self) { return self.size(); })
         .def("__bool__", [](PbGraph::conflicts_t const& self) { return !self.empty(); })
         .def(

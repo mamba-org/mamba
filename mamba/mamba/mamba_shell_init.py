eae7c46dce792b19b7d5ade7917c6bb33e17c8cd
Jonas Haag <jonas@lophus.org>
2023-01-26 15:39:58 +0200

    Try different fix

--- mamba/mamba/mamba_shell_init.py
+++ mamba/mamba/mamba_shell_init.py
@@ -81,15 +81,12 @@ def shell_init(args):
         return initialize_dev(shell)
 
     else:
-        if hasattr(args, "no_user"):
-            # Conda <23.1.0, see https://github.com/mamba-org/mamba/issues/2248
-            for_user = args.user
-            if not (args.install and args.user and args.system):
-                for_user = True
-            if args.no_user:
-                for_user = False
-        else:
-            for_user = args.user and not args.system
+        for_user = args.user
+        if not (args.install and args.user and args.system):
+            for_user = True
+        # no_user removed in Conda 23.1.0, see https://github.com/mamba-org/mamba/issues/2248
+        if getattr(args, "no_user", False):
+            for_user = False
 
         anaconda_prompt = on_win and args.anaconda_prompt
         exit_code = initialize(

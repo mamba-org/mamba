7866468c516c004998756cb74434c01d61fbea5e
Wolf Vollprecht <w.vollprecht@gmail.com>
2023-03-08 15:54:10 +0100

    Micromamba server (#2185)
    Implemented a micromamba server

--- micromamba/src/umamba.hpp
+++ micromamba/src/umamba.hpp
@@ -82,4 +82,9 @@ set_search_command(CLI::App* subcom);
 void
 set_auth_command(CLI::App* subcom);
 
+#if !defined(_WIN32) && defined(MICROMAMBA_SERVER)
+void
+set_server_command(CLI::App* subcom);
+#endif
+
 #endif

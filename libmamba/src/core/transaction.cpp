2724fa75e38b1182d2227c97b5744381a511a1df
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2023-03-30 21:06:17 -0400

    Add suggestions from review

--- libmamba/src/core/transaction.cpp
+++ libmamba/src/core/transaction.cpp
@@ -1082,13 +1082,16 @@ namespace mamba
         }
         else
         {
+            // TODO: add executable name
+            // TODO: add environment name/prefix
             LOG_INFO << "Waiting for pyc compilation to finish";
             m_transaction_context.wait_for_pyc_compilation();
-            Console::stream() << "\nTransaction finished";
-            Console::stream() << "\nTo activate this environment, use:\n";
-            Console::stream() << "    $ micromamba activate <placeholder>";
-            Console::stream() << "\nOr to execute a single command in this environment,  use:\n";
-            Console::stream() << "    $ micromamba run -n <placeholder> mycommand\n";
+            Console::stream()
+                << "\nTransaction finished\n\n"
+                << "To activate this environment, use:\n\n"
+                << "    $ [micro]mamba activate <environment>\n\n"
+                << "Or to execute a single command in this environment, use:\n\n"
+                << "    $ [micro]mamba run -n <environment> mycommand\n";
 
             prefix.history().add_entry(m_history_entry);
         }

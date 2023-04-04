ea834b57879cdce2733bec8a9f5be92b3f88167a
Johan Mabille <johan.mabille@gmail.com>
2023-04-04 11:43:53 -0400

    Migrated to doctest (#2436)

--- libmamba/tests/src/core/test_invoke.cpp
+++ libmamba/tests/src/core/test_invoke.cpp
@@ -1,83 +1,92 @@
+// Copyright (c) 2019, QuantStack and Mamba Contributors
+//
+// Distributed under the terms of the BSD 3-Clause License.
+//
+// The full license is in the file LICENSE, distributed with this software.
+
 #include <exception>
 
-#include <gtest/gtest.h>
+#include <doctest/doctest.h>
 
 #include "mamba/core/invoke.hpp"
 #include "mamba/core/util_string.hpp"
 
 namespace mamba
 {
-    TEST(safe_invoke, executes_with_success)
+    TEST_SUITE("safe_invoke")
     {
-        bool was_called = false;
-        auto result = safe_invoke([&] { was_called = true; });
-        EXPECT_TRUE(result);
-        EXPECT_TRUE(was_called);
-    }
-
-    TEST(safe_invoke, catches_std_exceptions)
-    {
-        const auto message = "expected failure";
-        auto result = safe_invoke([&] { throw std::runtime_error(message); });
-        EXPECT_FALSE(result);
-        EXPECT_TRUE(ends_with(result.error().what(), message)) << result.error().what();
-    }
-
-    TEST(safe_invoke, catches_any_exceptions)
-    {
-        const auto message = "expected failure";
-        auto result = safe_invoke([&] { throw message; });
-        EXPECT_FALSE(result);
-        EXPECT_TRUE(ends_with(result.error().what(), "unknown error")) << result.error().what();
-    }
-
-    TEST(safe_invoke, safely_catch_moved_callable_destructor_exception)
-    {
-        bool did_move_happened = false;
-
-        struct DoNotDoThisAtHome
+        TEST_CASE("executes_with_success")
         {
-            bool& did_move_happened;
-            bool owner = true;
+            bool was_called = false;
+            auto result = safe_invoke([&] { was_called = true; });
+            CHECK(result);
+            CHECK(was_called);
+        }
 
-            DoNotDoThisAtHome(bool& move_happened)
-                : did_move_happened(move_happened)
-            {
-            }
+        TEST_CASE("catches_std_exceptions")
+        {
+            const auto message = "expected failure";
+            auto result = safe_invoke([&] { throw std::runtime_error(message); });
+            CHECK_FALSE(result);
+            CHECK_MESSAGE(ends_with(result.error().what(), message), result.error().what());
+        }
 
-            DoNotDoThisAtHome(DoNotDoThisAtHome&& other)
-                : did_move_happened(other.did_move_happened)
-            {
-                did_move_happened = true;
-                other.owner = false;
-            }
+        TEST_CASE("catches_any_exceptions")
+        {
+            const auto message = "expected failure";
+            auto result = safe_invoke([&] { throw message; });
+            CHECK_FALSE(result);
+            CHECK_MESSAGE(ends_with(result.error().what(), "unknown error"), result.error().what());
+        }
 
-            DoNotDoThisAtHome& operator=(DoNotDoThisAtHome&& other)
-            {
-                did_move_happened = other.did_move_happened;
-                did_move_happened = true;
-                other.owner = false;
-                return *this;
-            }
+        TEST_CASE("safely_catch_moved_callable_destructor_exception")
+        {
+            bool did_move_happened = false;
 
-            ~DoNotDoThisAtHome() noexcept(false)
+            struct DoNotDoThisAtHome
             {
-                if (owner)
+                bool& did_move_happened;
+                bool owner = true;
+
+                DoNotDoThisAtHome(bool& move_happened)
+                    : did_move_happened(move_happened)
                 {
-                    throw "watever";
                 }
-            }
 
-            void operator()() const
-            {
-                // do nothing
-            }
-        };
+                DoNotDoThisAtHome(DoNotDoThisAtHome&& other)
+                    : did_move_happened(other.did_move_happened)
+                {
+                    did_move_happened = true;
+                    other.owner = false;
+                }
 
-        auto result = safe_invoke(DoNotDoThisAtHome{ did_move_happened });
-        EXPECT_FALSE(result);
-        EXPECT_TRUE(ends_with(result.error().what(), "unknown error")) << result.error().what();
-        EXPECT_TRUE(did_move_happened);
+                DoNotDoThisAtHome& operator=(DoNotDoThisAtHome&& other)
+                {
+                    did_move_happened = other.did_move_happened;
+                    did_move_happened = true;
+                    other.owner = false;
+                    return *this;
+                }
+
+                ~DoNotDoThisAtHome() noexcept(false)
+                {
+                    if (owner)
+                    {
+                        throw "watever";
+                    }
+                }
+
+                void operator()() const
+                {
+                    // do nothing
+                }
+            };
+
+            auto result = safe_invoke(DoNotDoThisAtHome{ did_move_happened });
+            CHECK_FALSE(result);
+            CHECK_MESSAGE(ends_with(result.error().what(), "unknown error"), result.error().what());
+            CHECK(did_move_happened);
+        }
     }
 
 }
--- libmamba/tests/src/core/test_system_env.cpp
+++ libmamba/tests/src/core/test_system_env.cpp
@@ -6,7 +6,7 @@
 
 #include <string>
 
-#include <gtest/gtest.h>
+#include <doctest/doctest.h>
 
 #include "mamba/core/environment.hpp"
 #include "mamba/core/util_random.hpp"
@@ -17,52 +17,54 @@ namespace
     void test_set_get_unset_env_variables(const std::string& key, const std::string& value)
     {
         const bool did_set = mamba::env::set(key, value);
-        ASSERT_TRUE(did_set);
+        REQUIRE(did_set);
 
         const auto result = mamba::env::get(key);
-        ASSERT_TRUE(result.has_value());
-        EXPECT_EQ(*result, value);
+        REQUIRE(result.has_value());
+        CHECK_EQ(*result, value);
 
         mamba::env::unset(key);
         const auto result_empty = mamba::env::get(key);
-        ASSERT_FALSE(result_empty.has_value());
+        REQUIRE_FALSE(result_empty.has_value());
     }
 }
 
-TEST(system_env, set_get_unset_env_variables)
+TEST_SUITE("system_env")
 {
-    const auto key = mamba::generate_random_alphanumeric_string(128);
-    const auto value = mamba::generate_random_alphanumeric_string(128);
+    TEST_CASE("set_get_unset_env_variables")
+    {
+        const auto key = mamba::generate_random_alphanumeric_string(128);
+        const auto value = mamba::generate_random_alphanumeric_string(128);
 
-    test_set_get_unset_env_variables(key, value);
-}
+        test_set_get_unset_env_variables(key, value);
+    }
 
-TEST(system_env, set_get_unset_variables_unicode)
-{
-    const std::string key = u8"Joël私のにほん";
-    const std::string value = u8"Hello, I am Joël. 私のにほんごわへたです";
+    TEST_CASE("set_get_unset_variables_unicode")
+    {
+        const std::string key = u8"Joël私のにほん";
+        const std::string value = u8"Hello, I am Joël. 私のにほんごわへたです";
 
-    test_set_get_unset_env_variables(key, value);
-}
+        test_set_get_unset_env_variables(key, value);
+    }
 
 #ifdef _WIN32
 
-TEST(system_env, get_predefined_env_variable)
-{
-    // We check that normal pre-defined variables are accessible and do not fail access even if some
-    // of these values have unicode.
-    const std::vector<std::string> predefined_keys{ "PATH",        "OS",           "PATHEXT",
-                                                    "ProgramData", "SystemRoot",   "windir",
-                                                    "APPDATA",     "COMPUTERNAME", "TEMP",
-                                                    "UserName",    "USERPROFILE" };
-
-    for (const auto& key : predefined_keys)
+    TEST_CASE("get_predefined_env_variable")
     {
-        const auto maybe_value = mamba::env::get(key);
-        ASSERT_TRUE(maybe_value.has_value()) << "key = " + key;
-        const auto value = *maybe_value;
-        EXPECT_FALSE(value.empty()) << "key = " + key;
-    }
-}
+        // We check that normal pre-defined variables are accessible and do not fail access even if
+        // some of these values have unicode.
+        const std::vector<std::string> predefined_keys{ "PATH",        "OS",           "PATHEXT",
+                                                        "ProgramData", "SystemRoot",   "windir",
+                                                        "APPDATA",     "COMPUTERNAME", "TEMP",
+                                                        "UserName",    "USERPROFILE" };
 
+        for (const auto& key : predefined_keys)
+        {
+            const auto maybe_value = mamba::env::get(key);
+            REQUIRE_MESSAGE(maybe_value.has_value(), "key = ", key);
+            const auto value = *maybe_value;
+            CHECK_FALSE_MESSAGE(value.empty(), "key = ", key);
+        }
+    }
 #endif
+}

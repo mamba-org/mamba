48ac318162dc72ae9a1c4c7cd933018c80bfe12a
AntoinePrv <AntoinePrv@users.noreply.github.com>
2023-04-12 19:18:53 +0200

    Fix format

--- libmamba/src/core/solver.cpp
+++ libmamba/src/core/solver.cpp
@@ -23,7 +23,6 @@
 #include "mamba/core/satisfiability_error.hpp"
 #include "mamba/core/solver.hpp"
 #include "mamba/core/util_string.hpp"
-
 #include "solv-cpp/pool.hpp"
 #include "solv-cpp/queue.hpp"
 

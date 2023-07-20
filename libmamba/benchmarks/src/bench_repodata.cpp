dc52e4b0fe5fd3b42730696ae9ee73c7f7cfa455
AntoinePrv <AntoinePrv@users.noreply.github.com>
2023-07-20 11:50:03 +0200

    Add repodata benchmark

--- libmamba/benchmarks/src/bench_repodata.cpp
+++ libmamba/benchmarks/src/bench_repodata.cpp
@@ -9,7 +9,12 @@
 
 #include <benchmark/benchmark.h>
 
+#include "mamba/core/channel.hpp"
 #include "mamba/core/mamba_fs.hpp"
+#include "mamba/core/pool.hpp"
+#include "mamba/core/repo.hpp"
+
+using namespace mamba;
 
 namespace
 {
@@ -22,4 +27,26 @@ namespace
         }
         return { repodata_file_path };
     }
+
+    void bench_repodata_libsolv(benchmark::State& state)
+    {
+        for (auto _ : state)
+        {
+            ChannelContext chan_ctx = {};
+            MPool pool = { chan_ctx };
+            MRepo repo = { pool, "mychannel", repodata_file(), {}, MRepo::RepodataParser::libsolv };
+        }
+    }
+    BENCHMARK(bench_repodata_libsolv);
+
+    void bench_repodata_mamba(benchmark::State& state)
+    {
+        for (auto _ : state)
+        {
+            ChannelContext chan_ctx = {};
+            MPool pool = { chan_ctx };
+            MRepo repo = { pool, "mychannel", repodata_file(), {}, MRepo::RepodataParser::mamba };
+        }
+    }
+    BENCHMARK(bench_repodata_mamba);
 }

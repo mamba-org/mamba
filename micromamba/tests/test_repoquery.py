f40570bbfb0db001fbc758660c17b6258fd50826
AntoinePrv <AntoinePrv@users.noreply.github.com>
2023-07-20 15:59:01 +0200

    Refactor test_repoquery to use new fixtures

--- micromamba/tests/test_repoquery.py
+++ micromamba/tests/test_repoquery.py
@@ -1,175 +1,185 @@
-import os
 import platform
-import shutil
+from pathlib import Path
 
 import pytest
 
-from .helpers import create, get_env, random_string, umamba_repoquery
+from . import helpers
 
 
-class TestRepoquery:
-    env_name = random_string()
-    root_prefix = os.environ["MAMBA_ROOT_PREFIX"]
-    current_prefix = os.environ["CONDA_PREFIX"]
-    prefix = os.path.join(root_prefix, "envs", env_name)
+@pytest.fixture
+def yaml_env(tmp_prefix: Path) -> None:
+    helpers.install(
+        "--channel",
+        "conda-forge",
+        "--offline",
+        "yaml=0.2.5",
+        "pyyaml=6.0",
+        no_dry_run=True,
+    )
 
-    @classmethod
-    def setup_class(cls):
-        create(
-            "yaml=0.2.5",
-            "pyyaml=6.0",
-            "-c",
-            "conda-forge",
-            "-n",
-            TestRepoquery.env_name,
-            "--json",
-            no_dry_run=True,
-        )
-        os.environ["CONDA_PREFIX"] = TestRepoquery.prefix
 
-    @classmethod
-    def teardown_class(cls):
-        os.environ["CONDA_PREFIX"] = TestRepoquery.current_prefix
-        shutil.rmtree(get_env(TestRepoquery.env_name))
+@pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)
+def test_depends(yaml_env: Path):
+    res = helpers.umamba_repoquery("depends", "yaml=0.2.5", "--json")
 
-    # Testing `depends`
-    def test_depends(self):
-        res = umamba_repoquery("depends", "yaml=0.2.5", "--json")
+    assert res["query"]["query"] == "yaml =0.2.5*"
+    assert res["query"]["type"] == "depends"
 
-        assert res["query"]["query"] == "yaml =0.2.5*"
-        assert res["query"]["type"] == "depends"
+    pkgs = res["result"]["pkgs"]
+    assert any(x["channel"] == "conda-forge" for x in pkgs)
+    assert any(x["name"] == "yaml" for x in pkgs)
+    assert any(x["version"] == "0.2.5" for x in pkgs)
 
-        pkgs = res["result"]["pkgs"]
-        assert any(x["channel"] == "conda-forge" for x in pkgs)
-        assert any(x["name"] == "yaml" for x in pkgs)
-        assert any(x["version"] == "0.2.5" for x in pkgs)
+    if platform.system() == "Linux":
+        assert any(x["name"] == "libgcc-ng" for x in pkgs)
 
-        if platform.system() == "Linux":
-            assert any(x["name"] == "libgcc-ng" for x in pkgs)
 
-    def test_depends_remote(self):
-        res = umamba_repoquery("depends", "yaml", "--use-local=0")
+@pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)
+def test_depends_remote(yaml_env: Path):
+    res = helpers.umamba_repoquery("depends", "yaml", "--use-local=0")
 
-        assert 'No entries matching "yaml" found' in res
+    assert 'No entries matching "yaml" found' in res
 
-    def test_depends_not_installed(self):
-        res = umamba_repoquery("depends", "xtensor")
 
-        assert 'No entries matching "xtensor" found' in res
+@pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)
+def test_depends_not_installed(yaml_env: Path):
+    res = helpers.umamba_repoquery("depends", "xtensor")
 
-    def test_depends_not_installed_with_channel(self):
-        res = umamba_repoquery(
-            "depends", "-c", "conda-forge", "xtensor=0.24.5", "--json"
-        )
+    assert 'No entries matching "xtensor" found' in res
 
-        assert res["query"]["query"] == "xtensor =0.24.5*"
-        assert res["query"]["type"] == "depends"
-        assert "conda-forge" in res["result"]["graph_roots"][0]["channel"]
-        assert res["result"]["graph_roots"][0]["name"] == "xtensor"
-        assert res["result"]["graph_roots"][0]["version"] == "0.24.5"
 
-        pkgs = res["result"]["pkgs"]
+@pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)
+def test_depends_not_installed_with_channel(yaml_env: Path):
+    res = helpers.umamba_repoquery(
+        "depends", "-c", "conda-forge", "xtensor=0.24.5", "--json"
+    )
 
-        assert any(x["name"] == "xtensor" for x in pkgs)
-        assert any(x["name"] == "xtl" for x in pkgs)
+    assert res["query"]["query"] == "xtensor =0.24.5*"
+    assert res["query"]["type"] == "depends"
+    assert "conda-forge" in res["result"]["graph_roots"][0]["channel"]
+    assert res["result"]["graph_roots"][0]["name"] == "xtensor"
+    assert res["result"]["graph_roots"][0]["version"] == "0.24.5"
 
-        if platform.system() == "Linux":
-            assert any(x["name"] == "libgcc-ng" for x in pkgs)
-            assert any(x["name"] == "libstdcxx-ng" for x in pkgs)
+    pkgs = res["result"]["pkgs"]
 
-    def test_depends_recursive(self):
-        res = umamba_repoquery(
-            "depends", "-c", "conda-forge", "xtensor=0.24.5", "--recursive"
-        )
+    assert any(x["name"] == "xtensor" for x in pkgs)
+    assert any(x["name"] == "xtl" for x in pkgs)
 
-        if platform.system() == "Linux":
-            assert "libzlib" in res
-        elif platform.system() == "Darwin":
-            assert "libcxx" in res
-        elif platform.system() == "Windows":
-            assert "vc" in res
+    if platform.system() == "Linux":
+        assert any(x["name"] == "libgcc-ng" for x in pkgs)
+        assert any(x["name"] == "libstdcxx-ng" for x in pkgs)
 
-    def test_depends_tree(self):
-        res = umamba_repoquery(
-            "depends", "-c", "conda-forge", "xtensor=0.24.5", "--tree"
-        )
 
-        if platform.system() == "Linux":
-            assert "libzlib" in res
-        elif platform.system() == "Darwin":
-            assert "libcxx" in res
-        elif platform.system() == "Windows":
-            assert "vc" in res
+@pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)
+def test_depends_recursive(yaml_env: Path):
+    res = helpers.umamba_repoquery(
+        "depends", "-c", "conda-forge", "xtensor=0.24.5", "--recursive"
+    )
 
-    # Testing `whoneeds`
-    def test_whoneeds(self):
-        res = umamba_repoquery("whoneeds", "yaml", "--json")
+    if platform.system() == "Linux":
+        assert "libzlib" in res
+    elif platform.system() == "Darwin":
+        assert "libcxx" in res
+    elif platform.system() == "Windows":
+        assert "vc" in res
 
-        assert res["query"]["query"] == "yaml"
-        assert res["query"]["type"] == "whoneeds"
-        assert res["result"]["pkgs"][0]["channel"] == "conda-forge"
-        assert res["result"]["pkgs"][0]["name"] == "pyyaml"
-        assert res["result"]["pkgs"][0]["version"] == "6.0"
 
-    def test_whoneeds_remote(self):
-        res = umamba_repoquery("whoneeds", "yaml", "--use-local=0")
+@pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)
+def test_depends_tree(yaml_env: Path):
+    res = helpers.umamba_repoquery(
+        "depends", "-c", "conda-forge", "xtensor=0.24.5", "--tree"
+    )
 
-        assert 'No entries matching "yaml" found' in res
+    if platform.system() == "Linux":
+        assert "libzlib" in res
+    elif platform.system() == "Darwin":
+        assert "libcxx" in res
+    elif platform.system() == "Windows":
+        assert "vc" in res
 
-    def test_whoneeds_not_installed(self):
-        res = umamba_repoquery("whoneeds", "xtensor")
 
-        assert 'No entries matching "xtensor" found' in res
+@pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)
+def test_whoneeds(yaml_env: Path):
+    res = helpers.umamba_repoquery("whoneeds", "yaml", "--json")
 
-    def test_whoneeds_not_installed_with_channel(self):
-        res = umamba_repoquery(
-            "whoneeds", "-c", "conda-forge", "xtensor=0.24.5", "--json"
-        )
+    assert res["query"]["query"] == "yaml"
+    assert res["query"]["type"] == "whoneeds"
+    assert res["result"]["pkgs"][0]["channel"] == "conda-forge"
+    assert res["result"]["pkgs"][0]["name"] == "pyyaml"
+    assert res["result"]["pkgs"][0]["version"] == "6.0"
 
-        assert res["query"]["query"] == "xtensor =0.24.5*"
-        assert res["query"]["type"] == "whoneeds"
 
-        pkgs = res["result"]["pkgs"]
-        assert all("conda-forge" in x["channel"] for x in pkgs)
-        assert any(x["name"] == "cppcolormap" for x in pkgs)
-        assert any(x["name"] == "pyxtensor" for x in pkgs)
-        assert any(x["name"] == "qpot" for x in pkgs)
+@pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)
+def test_whoneeds_remote(yaml_env: Path):
+    res = helpers.umamba_repoquery("whoneeds", "yaml", "--use-local=0")
 
-    def test_whoneeds_tree(self):
-        res = umamba_repoquery(
-            "whoneeds", "-c", "conda-forge", "xtensor=0.24.5", "--tree"
-        )
+    assert 'No entries matching "yaml" found' in res
 
-        assert "cppcolormap" in res
-        assert "pyxtensor" in res
-        assert "qpot" in res
 
-    # Testing `search`
-    def test_remote_search_installed_pkg(self):
-        res = umamba_repoquery("search", "yaml")
+@pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)
+def test_whoneeds_not_installed(yaml_env: Path):
+    res = helpers.umamba_repoquery("whoneeds", "xtensor")
 
-        assert 'No entries matching "yaml" found' in res
-        assert (
-            "Channels may not be configured. Try giving a channel with '-c,--channel' option, or use `--use-local=1` to search for installed packages."
-            in res
-        )
+    assert 'No entries matching "xtensor" found' in res
 
-    def test_local_search_installed_pkg(self):
-        res = umamba_repoquery("search", "yaml", "--use-local=1", "--json")
 
-        assert res["query"]["query"] == "yaml"
-        assert res["query"]["type"] == "search"
-        assert res["result"]["pkgs"][0]["channel"] == "conda-forge"
-        assert res["result"]["pkgs"][0]["name"] == "yaml"
-        assert res["result"]["pkgs"][0]["version"] == "0.2.5"
+@pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)
+def test_whoneeds_not_installed_with_channel(yaml_env: Path):
+    res = helpers.umamba_repoquery(
+        "whoneeds", "-c", "conda-forge", "xtensor=0.24.5", "--json"
+    )
 
-    def test_remote_search_not_installed_pkg(self):
-        res = umamba_repoquery(
-            "search", "-c", "conda-forge", "xtensor=0.24.5", "--json"
-        )
+    assert res["query"]["query"] == "xtensor =0.24.5*"
+    assert res["query"]["type"] == "whoneeds"
 
-        assert res["query"]["query"] == "xtensor =0.24.5*"
-        assert res["query"]["type"] == "search"
-        assert "conda-forge" in res["result"]["pkgs"][0]["channel"]
-        assert res["result"]["pkgs"][0]["name"] == "xtensor"
-        assert res["result"]["pkgs"][0]["version"] == "0.24.5"
+    pkgs = res["result"]["pkgs"]
+    assert all("conda-forge" in x["channel"] for x in pkgs)
+    assert any(x["name"] == "cppcolormap" for x in pkgs)
+    assert any(x["name"] == "pyxtensor" for x in pkgs)
+    assert any(x["name"] == "qpot" for x in pkgs)
+
+
+@pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)
+def test_whoneeds_tree(yaml_env: Path):
+    res = helpers.umamba_repoquery(
+        "whoneeds", "-c", "conda-forge", "xtensor=0.24.5", "--tree"
+    )
+
+    assert "cppcolormap" in res
+    assert "pyxtensor" in res
+    assert "qpot" in res
+
+
+@pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)
+def test_remote_search_installed_pkg(yaml_env: Path):
+    res = helpers.umamba_repoquery("search", "yaml")
+
+    assert 'No entries matching "yaml" found' in res
+    assert (
+        "Channels may not be configured. Try giving a channel with '-c,--channel' option, or use `--use-local=1` to search for installed packages."
+        in res
+    )
+
+
+@pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)
+def test_local_search_installed_pkg(yaml_env: Path):
+    res = helpers.umamba_repoquery("search", "yaml", "--use-local=1", "--json")
+
+    assert res["query"]["query"] == "yaml"
+    assert res["query"]["type"] == "search"
+    assert res["result"]["pkgs"][0]["channel"] == "conda-forge"
+    assert res["result"]["pkgs"][0]["name"] == "yaml"
+    assert res["result"]["pkgs"][0]["version"] == "0.2.5"
+
+
+@pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)
+def test_remote_search_not_installed_pkg(yaml_env: Path):
+    res = helpers.umamba_repoquery(
+        "search", "-c", "conda-forge", "xtensor=0.24.5", "--json"
+    )
+
+    assert res["query"]["query"] == "xtensor =0.24.5*"
+    assert res["query"]["type"] == "search"
+    assert "conda-forge" in res["result"]["pkgs"][0]["channel"]
+    assert res["result"]["pkgs"][0]["name"] == "xtensor"
+    assert res["result"]["pkgs"][0]["version"] == "0.24.5"

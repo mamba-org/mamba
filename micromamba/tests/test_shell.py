7b02d16fac2c7a2dacf8c155291d238bcf03dffc
AntoinePrv <AntoinePrv@users.noreply.github.com>
2023-08-01 12:15:40 +0200

    Add PATH check for shell activation

--- micromamba/tests/test_shell.py
+++ micromamba/tests/test_shell.py
@@ -296,3 +296,18 @@ class TestShell:
             env={**os.environ, "SHELL": script_path},
         )
         assert ret.returncode == 42
+
+    def test_shell_run_activated(self):
+        """Verify environment properly activated in `micromamba shell`."""
+        skip_if_shell_incompat("bash")
+        create("-n", self.env_name)
+        p = subprocess.Popen(
+            [get_umamba(), "shell", "-n", self.env_name],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            stdin=subprocess.PIPE,
+            text=True,
+        )
+        stdout, stderr = p.communicate("echo $PATH")
+        assert p.poll() == 0  # Terminated with retcode 0
+        assert self.env_name in stdout.split(os.pathsep)[0]

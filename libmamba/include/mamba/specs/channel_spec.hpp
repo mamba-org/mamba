02beb7a55ecd0acedb752ae02b54c4d3b336c3fd
AntoinePrv <AntoinePrv@users.noreply.github.com>
2023-09-25 16:55:12 +0200

    Add ChannelSpec::Type

--- libmamba/include/mamba/specs/channel_spec.hpp
+++ libmamba/include/mamba/specs/channel_spec.hpp
@@ -27,6 +27,41 @@ namespace mamba::specs
     {
     public:
 
+        enum class Type
+        {
+            /**
+             * A URL to a full repo strucuture.
+             *
+             * Example "https://repo.anaconda.com/conda-forge".
+             */
+            URL,
+            /**
+             * A URL to a single package.
+             *
+             * Example "https://repo.anaconda.com/conda-forge/linux-64/pkg-0.0-bld.conda".
+             */
+            PackageURL,
+            /**
+             * An absolute path to a full repo strucuture.
+             *
+             * Example "/Users/name/conda-bld".
+             */
+            Path,
+            /**
+             * An absolute path to a single-package.
+             *
+             * Example "/tmp/pkg-0.0-bld.conda".
+             */
+            PackagePath,
+            /**
+             * A relative name.
+             *
+             * It needs to be resolved using a channel alias, or a custom channel.
+             * Example "conda-forge", "locals", "my-channel/my-label".
+             */
+            Name,
+        };
+
         inline static constexpr std::string_view default_name = "defaults";
         inline static constexpr std::string_view platform_separators = "|,;";
 

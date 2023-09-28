d9554827b50e3afcbd724124a1fe72c8c5f58ec3
jaimergp <jaimergp@users.noreply.github.com>
2023-09-28 16:10:00 +0200

    compatibility fixes for conda 23.9 and conda-libmamba-solver 23.9

--- mamba/mamba/mamba.py
+++ mamba/mamba/mamba.py
@@ -16,7 +16,6 @@ from conda.base.context import context
 from conda.cli.common import (
     check_non_admin,
     confirm_yn,
-    ensure_name_or_prefix,
     specs_from_url,
 )
 from conda.cli.install import check_prefix, clone, get_revision
@@ -239,8 +238,9 @@ def install(args, parser, command="install"):
         solver_task = api.SOLVER_UPDATE
         solver_options.clear()
 
-    if newenv:
-        ensure_name_or_prefix(args, command)
+    if newenv and not (args.name or args.prefix):
+        raise CondaValueError('either -n NAME or -p PREFIX option required,\n'
+                              'try "mamba %s -h" for more details' % command)
     prefix = context.target_prefix
     if newenv:
         check_prefix(prefix, json=context.json)
@@ -801,10 +801,15 @@ Examples:
 
     import argparse
     from argparse import SUPPRESS
+    try:
+        p = sub_parsers.add_parser(
+            "repoquery", description=descr, help=help_cli, epilog=example
+        )
+    except argparse.ArgumentError as exc:
+        if "conflicting subparser" in str(exc):
+            # conda-libmamba-solver's repoquery is already registered 
+            return
 
-    p = sub_parsers.add_parser(
-        "repoquery", description=descr, help=help_cli, epilog=example
-    )
     subsub_parser = p.add_subparsers(dest="subcmd")
     package_cmds = argparse.ArgumentParser(add_help=False)
     package_cmds.add_argument("package_query", help="the target package")
@@ -879,8 +884,8 @@ def _wrapped_main(*args, **kwargs):
 
     context.__init__(argparse_args=parsed_args)
     context.__initialized__ = True
-
-    init_loggers(context)
+    
+    init_loggers()
     result = do_call(parsed_args, p)
     exit_code = getattr(
         result, "rc", result

31471776c4e9cd1fd6ffbffbb6cad632001a7e26
AntoinePrv <AntoinePrv@users.noreply.github.com>
2023-10-19 18:31:00 +0200

    Add specs::AutheticationDataBase

--- libmamba/include/mamba/specs/authentification_info.hpp
+++ libmamba/include/mamba/specs/authentification_info.hpp
@@ -8,10 +8,13 @@
 #define MAMBA_SPECS_AUTHENTIFICATION_INFO_HPP
 
 #include <string>
+#include <unordered_map>
 #include <variant>
 
 namespace mamba::specs
 {
+    class CondaURL;
+
     /** User and password authetification set in the URL. */
     struct BasicHTTPAuthentication
     {
@@ -32,5 +35,57 @@ namespace mamba::specs
     };
 
     using AuthenticationInfo = std::variant<BasicHTTPAuthentication, BearerToken, CondaToken>;
+
+    class AuthenticationDataBase : private std::unordered_map<std::string, AuthenticationInfo>
+    {
+    public:
+
+        using Base = std::unordered_map<std::string, AuthenticationInfo>;
+
+        using typename Base::key_type;
+        using typename Base::mapped_type;
+        using typename Base::value_type;
+        using typename Base::size_type;
+        using typename Base::iterator;
+        using typename Base::const_iterator;
+
+        using Base::Base;
+
+        using Base::begin;
+        using Base::end;
+        using Base::cbegin;
+        using Base::cend;
+
+        using Base::empty;
+        using Base::size;
+        using Base::max_size;
+
+        using Base::clear;
+        using Base::insert;
+        using Base::insert_or_assign;
+        using Base::emplace;
+        using Base::emplace_hint;
+        using Base::try_emplace;
+        using Base::erase;
+        using Base::swap;
+        using Base::extract;
+        using Base::merge;
+
+        using Base::reserve;
+
+        using Base::at;
+
+        [[nodiscard]] auto at_compatible(const key_type& key) -> mapped_type&;
+        [[nodiscard]] auto at_compatible(const key_type& key) const -> const mapped_type&;
+
+        using Base::find;
+
+        auto find_compatible(const key_type& key) -> iterator;
+        auto find_compatible(const key_type& key) const -> const_iterator;
+
+        [[nodiscard]] auto contains(const key_type& key) const -> bool;
+
+        [[nodiscard]] auto contains_compatible(const key_type& key) const -> bool;
+    };
 }
 #endif
---
+++ libmamba/src/specs/authentication_info.cpp
@@ -0,0 +1,88 @@
+// Copyright (c) 2019, QuantStack and Mamba Contributors
+//
+// Distributed under the terms of the BSD 3-Clause License.
+//
+// The full license is in the file LICENSE, distributed with this software.
+
+#include <stdexcept>
+
+#include <fmt/format.h>
+
+#include "mamba/specs/authentification_info.hpp"
+#include "mamba/util/string.hpp"
+
+namespace mamba::specs
+{
+
+    auto AuthenticationDataBase::at_compatible(const key_type& key) -> mapped_type&
+    {
+        if (auto it = find_compatible(key); it != end())
+        {
+            return it->second;
+        }
+        throw std::out_of_range(fmt::format(R"(No entry for key "{}")", key));
+    }
+
+    auto AuthenticationDataBase::at_compatible(const key_type& key) const -> const mapped_type&
+    {
+        return const_cast<AuthenticationDataBase*>(this)->at_compatible(key);
+    }
+
+    auto AuthenticationDataBase::find_compatible(const key_type& key) -> iterator
+    {
+        // First we start by adding a final '/' if absent, so that "mamba.org/" can
+        // be find by query "mamba.org".
+        std::string maybe_key = {};
+        if (!util::ends_with(key, '/'))
+        {
+            maybe_key = util::concat(key, '/');
+        }
+        else
+        {
+            maybe_key = key;
+        }
+
+        // We look for successsive parent dirs as queries with and without trailing '/';
+        // "mamba.org/channel/" > "mamba.org/channel" > "mamba.org/" > "mamba.org"
+        auto it = Base::find(maybe_key);
+        while (it == Base::end())
+        {
+            const auto pos = maybe_key.rfind('/');
+            if ((pos + 1) == maybe_key.size())
+            {
+                // Try again without final '/'
+                maybe_key.erase(pos);
+                it = Base::find(maybe_key);
+            }
+            else if (pos != std::string::npos)
+            {
+                // Try again without final element
+                maybe_key.erase(pos + 1);
+                it = Base::find(maybe_key);
+            }
+            else
+            {
+                // Nothing else to try
+                break;
+            }
+        }
+        return it;
+    }
+
+    auto AuthenticationDataBase::find_compatible(const key_type& key) const -> const_iterator
+    {
+        return static_cast<const_iterator>(
+            const_cast<AuthenticationDataBase*>(this)->find_compatible(key)
+        );
+    }
+
+    auto AuthenticationDataBase::contains(const key_type& key) const -> bool
+    {
+        return find(key) != end();
+    }
+
+    auto AuthenticationDataBase::contains_compatible(const key_type& key) const -> bool
+    {
+        return find_compatible(key) != end();
+    }
+}

a3fad25fbe1916bd8de0626131c3bde4c630c7d4
AntoinePrv <AntoinePrv@users.noreply.github.com>
2023-11-22 15:33:07 +0100

    Bind ChannelSpecs

--- libmambapy/src/libmambapy/bindings/specs.cpp
+++ libmambapy/src/libmambapy/bindings/specs.cpp
@@ -4,6 +4,8 @@
 //
 // The full license is in the file LICENSE, distributed with this software.
 
+#include <memory>
+
 #include <pybind11/pybind11.h>
 
 #include "mamba/specs/channel_spec.hpp"
@@ -14,11 +16,49 @@
 
 namespace mambapy
 {
+    template <typename T>
+    auto copy(const T& x) -> std::unique_ptr<T>
+    {
+        return std::make_unique<T>(x);
+    }
+
+    template <typename T>
+    auto deepcopy(const T& x, const pybind11::dict& /* memo */) -> std::unique_ptr<T>
+    {
+        return std::make_unique<T>(x);
+    }
+
     void bind_submodule_specs(pybind11::module_ m)
     {
         namespace py = pybind11;
         using namespace mamba::specs;
 
-        py::class_<ChannelSpec>(m, "ChannelSpec");
+        auto py_channel_spec = py::class_<ChannelSpec>(m, "ChannelSpec");
+
+        py::enum_<ChannelSpec::Type>(py_channel_spec, "Type")
+            .value("URL", ChannelSpec::Type::URL)
+            .value("PackageURL", ChannelSpec::Type::PackageURL)
+            .value("Path", ChannelSpec::Type::Path)
+            .value("PackagePath", ChannelSpec::Type::PackagePath)
+            .value("Name", ChannelSpec::Type::Name)
+            .value("Unknown", ChannelSpec::Type::Unknown);
+
+        py_channel_spec  //
+            .def_static("parse", ChannelSpec::parse)
+            .def(
+                py::init<std::string, ChannelSpec::dynamic_platform_set, ChannelSpec::Type>(),
+                py::arg("location"),
+                py::arg("platform_filters"),
+                py::arg("type") = ChannelSpec::Type::Unknown
+            )
+            .def("__copy__", &copy<ChannelSpec>)
+            .def("__deepcopy__", &deepcopy<ChannelSpec>, pybind11::arg("memo"))
+            .def_property("type", &ChannelSpec::type, &ChannelSpec::set_type)
+            .def_property("location", &ChannelSpec::location, &ChannelSpec::set_location)
+            .def_property(
+                "platform_filters",
+                &ChannelSpec::platform_filters,
+                &ChannelSpec::set_platform_filters
+            );
     }
 }
--- libmambapy/tests/test_specs.py
+++ libmambapy/tests/test_specs.py
@@ -1,5 +1,46 @@
+import copy
+
 import libmambapy
 
 
+def test_channel_spec_type():
+    Type = libmambapy.specs.ChannelSpec.Type
+
+    assert Type.URL.name == "URL"
+    assert Type.PackageURL.name == "PackageURL"
+    assert Type.Path.name == "Path"
+    assert Type.PackagePath.name == "PackagePath"
+    assert Type.Name.name == "Name"
+    assert Type.Unknown.name == "Unknown"
+
+
 def test_channel_spec():
-    libmambapy.specs.ChannelSpec
+    ChannelSpec = libmambapy.specs.ChannelSpec
+
+    # Constructor
+    spec = ChannelSpec(
+        location="<unknown>",
+        platform_filters=set(),
+        type=ChannelSpec.Type.Unknown,
+    )
+    assert spec.location == "<unknown>"
+    assert spec.platform_filters == set()
+    assert spec.type == ChannelSpec.Type.Unknown
+
+    #  Parser
+    spec = ChannelSpec.parse("conda-forge[linux-64]")
+    assert spec.location == "conda-forge"
+    assert spec.platform_filters == {"linux-64"}
+    assert spec.type == ChannelSpec.Type.Name
+
+    #  Setters
+    spec.location = "https://mamba.pm/"
+    assert spec.location == "https://mamba.pm/"
+    spec.platform_filters = {"win-64"}
+    assert spec.platform_filters == {"win-64"}
+    spec.type = ChannelSpec.Type.URL
+    assert spec.type == ChannelSpec.Type.URL
+
+    #  Copy
+    other = copy.deepcopy(spec)
+    assert other.location == spec.location

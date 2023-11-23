891d443962c9f99061bc56094d30c835dd1b2070
AntoinePrv <AntoinePrv@users.noreply.github.com>
2023-11-23 17:53:15 +0100

    Bind CondaURL

--- libmambapy/src/libmambapy/bindings/specs.cpp
+++ libmambapy/src/libmambapy/bindings/specs.cpp
@@ -6,9 +6,11 @@
 
 #include <memory>
 
+#include <pybind11/operators.h>
 #include <pybind11/pybind11.h>
 
 #include "mamba/specs/channel_spec.hpp"
+#include "mamba/specs/conda_url.hpp"
 #include "mamba/specs/platform.hpp"
 
 #include "bindings.hpp"
@@ -35,6 +37,12 @@ namespace mambapy
     auto deepcopy(const T& x, const pybind11::dict& /* memo */) -> std::unique_ptr<T>
     {
         return std::make_unique<T>(x);
+    }
+
+    template <typename T>
+    auto hash(const T& x) -> std::size_t
+    {
+        return std::hash<T>()(x);
     }
 
     void bind_submodule_specs(pybind11::module_ m)
@@ -65,6 +73,219 @@ namespace mambapy
             .def_static("count", &known_platforms_count)
             .def_static("build_platform", &build_platform);
         py::implicitly_convertible<py::str, Platform>();
+
+        auto py_conda_url = py::class_<CondaURL>(m, "CondaURL");
+
+        py::enum_<CondaURL::Credentials>(py_conda_url, "Credentials")
+            .value("Hide", CondaURL::Credentials::Hide)
+            .value("Show", CondaURL::Credentials::Show)
+            .value("Remove", CondaURL::Credentials::Remove)
+            .def(py::init(&enum_from_str<CondaURL::Credentials>));
+        py::implicitly_convertible<py::str, CondaURL::Credentials>();
+
+        py_conda_url  //
+            .def_static("parse", &CondaURL::parse)
+            .def(py::init())
+            .def("__copy__", &copy<CondaURL>)
+            .def("__deepcopy__", &deepcopy<CondaURL>)
+            .def(py::self == py::self)
+            .def(py::self != py::self)
+            .def("__hash__", &hash<CondaURL>)
+            .def("scheme", [](const CondaURL& self) { return self.scheme(); })
+            .def(
+                "set_scheme",
+                [](CondaURL& self, std::string_view str) { return self.set_scheme(str); }
+            )
+            .def("clear_scheme", [](CondaURL& self) { return self.clear_scheme(); })
+            .def(
+                "scheme_is_defaulted",
+                [](const CondaURL& self) { return self.scheme_is_defaulted(); }
+            )
+            .def(
+                "user",
+                [](const CondaURL& self, bool decode) {
+                    return decode ? self.user(CondaURL::Decode::yes)
+                                  : self.user(CondaURL::Decode::no);
+                },
+                py::arg("decode") = true
+            )
+            .def(
+                "set_user",
+                [](CondaURL& self, std::string_view str, bool encode)
+                {
+                    if (encode)
+                    {
+                        self.set_user(str, CondaURL::Encode::yes);
+                    }
+                    self.set_user(std::string(str), CondaURL::Encode::no);
+                },
+                py::arg("user"),
+                py::arg("encode") = true
+            )
+            .def("clear_user", [](CondaURL& self) { return self.clear_user(); })
+            .def("has_user", [](const CondaURL& self) { return self.has_user(); })
+            .def(
+                "password",
+                [](const CondaURL& self, bool decode) {
+                    return decode ? self.password(CondaURL::Decode::yes)
+                                  : self.password(CondaURL::Decode::no);
+                },
+                py::arg("decode") = true
+            )
+            .def(
+                "set_password",
+                [](CondaURL& self, std::string_view str, bool encode)
+                {
+                    if (encode)
+                    {
+                        self.set_password(str, CondaURL::Encode::yes);
+                    }
+                    self.set_password(std::string(str), CondaURL::Encode::no);
+                },
+                py::arg("password"),
+                py::arg("encode") = true
+            )
+            .def("clear_password", [](CondaURL& self) { return self.clear_password(); })
+            .def("has_password", [](const CondaURL& self) { return self.has_password(); })
+            .def("authentication", [](const CondaURL& self) { return self.authentication(); })
+            .def(
+                "host",
+                [](const CondaURL& self, bool decode)
+                {
+                    return decode ? self.host(CondaURL::Decode::yes)
+                                  : std::string(self.host(CondaURL::Decode::no));
+                },
+                py::arg("decode") = true
+            )
+            .def(
+                "set_host",
+                [](CondaURL& self, std::string_view str, bool encode)
+                {
+                    if (encode)
+                    {
+                        return self.set_host(str, CondaURL::Encode::yes);
+                    }
+                    return self.set_host(std::string(str), CondaURL::Encode::no);
+                },
+                py::arg("host"),
+                py::arg("encode") = true
+            )
+            .def("clear_host", [](CondaURL& self) { return self.clear_host(); })
+            .def("host_is_defaulted", [](const CondaURL& self) { return self.host_is_defaulted(); })
+            .def("port", [](const CondaURL& self) { return self.port(); })
+            .def("set_port", [](CondaURL& self, std::string_view str) { self.set_port(str); })
+            .def("clear_port", [](CondaURL& self) { return self.clear_port(); })
+            .def("authority", [](const CondaURL& self) { return self.authority(); })
+            .def(
+                "path",
+                [](const CondaURL& self, bool decode) {
+                    return decode ? self.path(CondaURL::Decode::yes)
+                                  : self.path(CondaURL::Decode::no);
+                },
+                py::arg("decode") = true
+            )
+            .def(
+                "set_path",
+                [](CondaURL& self, std::string_view str, bool encode)
+                {
+                    if (encode)
+                    {
+                        return self.set_path(str, CondaURL::Encode::yes);
+                    }
+                    return self.set_path(std::string(str), CondaURL::Encode::no);
+                },
+                py::arg("path"),
+                py::arg("encode") = true
+            )
+            .def("clear_path", [](CondaURL& self) { return self.clear_path(); })
+            .def(
+                "append_path",
+                [](CondaURL& self, std::string_view str, bool encode)
+                {
+                    if (encode)
+                    {
+                        return self.append_path(str, CondaURL::Encode::yes);
+                    }
+                    return self.append_path(std::string(str), CondaURL::Encode::no);
+                },
+                py::arg("path"),
+                py::arg("encode") = true
+            )
+            .def("pretty_path", [](const CondaURL& self) { return self.pretty_path(); })
+            .def("token", &CondaURL::token)
+            .def("set_token", &CondaURL::set_token)
+            .def("clear_token", &CondaURL::clear_token)
+            .def("has_token", &CondaURL::has_token)
+            .def(
+                "path_without_token",
+                [](const CondaURL& self, bool decode)
+                {
+                    return decode ? self.path_without_token(CondaURL::Decode::yes)
+                                  : std::string(self.path_without_token(CondaURL::Decode::no));
+                },
+                py::arg("decode") = true
+            )
+            .def(
+                "set_path_without_token",
+                [](CondaURL& self, std::string_view str, bool encode)
+                {
+                    if (encode)
+                    {
+                        return self.set_path_without_token(str, CondaURL::Encode::yes);
+                    }
+                    return self.set_path_without_token(std::string(str), CondaURL::Encode::no);
+                },
+                py::arg("path_without_token"),
+                py::arg("encode") = true
+            )
+            .def("clear_path_without_token", &CondaURL::clear_path_without_token)
+            .def("platform", &CondaURL::platform)
+            .def("set_platform", [](CondaURL& self, Platform plat) { self.set_platform(plat); })
+            .def("clear_platform", &CondaURL::clear_platform)
+            .def(
+                "package",
+                [](const CondaURL& self, bool decode)
+                {
+                    return decode ? self.package(CondaURL::Decode::yes)
+                                  : std::string(self.package(CondaURL::Decode::no));
+                },
+                py::arg("decode") = true
+            )
+            .def(
+                "set_package",
+                [](CondaURL& self, std::string_view str, bool encode)
+                {
+                    if (encode)
+                    {
+                        return self.set_package(str, CondaURL::Encode::yes);
+                    }
+                    return self.set_package(std::string(str), CondaURL::Encode::no);
+                },
+                py::arg("package"),
+                py::arg("encode") = true
+            )
+            .def("clear_package", &CondaURL::clear_package)
+            .def("str", &CondaURL::str, py::arg("credentials") = CondaURL::Credentials::Hide)
+            .def(
+                "__repr__",
+                [](const CondaURL& self) { return self.str(CondaURL::Credentials::Hide); }
+            )
+            .def(
+                "pretty_str",
+                [](const CondaURL& self,
+                   bool strip_scheme,
+                   std::string_view rstrip_path,
+                   CondaURL::Credentials credentials)
+                {
+                    auto const scheme = strip_scheme ? CondaURL::StripScheme::yes
+                                                     : CondaURL::StripScheme::no;
+                    const char rstrip = rstrip_path.empty() ? '\0' : rstrip_path.front();
+                    return self.pretty_str(scheme, rstrip, credentials);
+                },
+                py::arg("strip_scheme") = false,
+                py::arg("rstrip_path") = "/",
+                py::arg("credentials") = CondaURL::Credentials::Hide
+            );
 
         auto py_channel_spec = py::class_<ChannelSpec>(m, "ChannelSpec");
 
--- libmambapy/tests/test_specs.py
+++ libmambapy/tests/test_specs.py
@@ -36,6 +36,158 @@ def test_platform():
         Platform("linux-64") == Platform.linux_64
 
 
+def test_conda_url_credentials():
+    Credentials = libmambapy.specs.CondaURL.Credentials
+
+    assert Credentials.Hide.name == "Hide"
+    assert Credentials.Show.name == "Show"
+    assert Credentials.Remove.name == "Remove"
+    assert Credentials("Show") == Credentials.Show
+
+
+def test_conda_url_setters():
+    CondaURL = libmambapy.specs.CondaURL
+    url = CondaURL()
+
+    # Scheme
+    assert url.scheme_is_defaulted()
+    url.set_scheme("ftp")
+    assert not url.scheme_is_defaulted()
+    assert url.scheme() == "ftp"
+    assert url.clear_scheme() == "ftp"
+    assert url.scheme() == "https"
+    assert url.scheme_is_defaulted()
+    # User
+    assert url.user() == ""
+    assert not url.has_user()
+    url.set_user("user@email.com")
+    assert url.user() == "user@email.com"
+    url.set_user("none%40email.com", encode=False)
+    assert url.user() == "none@email.com"
+    assert url.clear_user() == "none%40email.com"
+    assert url.user() == ""
+    # Password
+    assert url.password() == ""
+    assert not url.has_password()
+    url.set_password("some#pass")
+    assert url.password() == "some#pass"
+    url.set_password("s%23pass", encode=False)
+    assert url.password() == "s#pass"
+    assert url.clear_password() == "s%23pass"
+    assert url.password() == ""
+    # Host
+    assert url.host_is_defaulted()
+    assert url.host() == "localhost"
+    url.set_host("mamba.org")
+    assert not url.host_is_defaulted()
+    assert url.host() == "mamba.org"
+    assert url.clear_host() == "mamba.org"
+    assert url.host() == "localhost"
+    assert url.host_is_defaulted()
+    # Port
+    assert url.port() == ""
+    url.set_port("33")
+    assert url.port() == "33"
+    assert url.clear_port() == "33"
+    assert url.port() == ""
+    # Path
+    assert url.path() == "/"
+    url.set_path("some path")
+    assert url.path() == "/some path"
+    url.set_path("some%20", encode=False)
+    assert url.path() == "/some "
+    url.append_path(" foo")
+    assert url.path() == "/some / foo"
+    url.append_path("%23", encode=False)
+    assert url.path() == "/some / foo/#"
+    assert url.clear_path() == "/some%20/%20foo/%23"
+    # Token
+    assert not url.has_token()
+    assert url.token() == ""
+    url.set_token("mytoken")
+    assert url.has_token()
+    assert url.token() == "mytoken"
+    assert url.clear_token()
+    assert url.token() == ""
+    # Path without token
+    url.set_token("mytoken")
+    assert url.path_without_token() == "/"
+    url.set_path_without_token("my path")
+    assert url.path_without_token() == "/my path"
+    url.set_path_without_token("bar%20", encode=False)
+    assert url.path_without_token() == "/bar "
+    assert url.clear_path_without_token()
+    # Platform
+    url.set_path_without_token("conda-forge/win-64", encode=False)
+    assert url.platform().name == "win_64"
+    url.set_platform("linux_64")
+    assert url.platform().name == "linux_64"
+    assert url.clear_platform()
+    # Package
+    assert url.package() == ""
+    url.set_package("pkg.conda")
+    assert url.package() == "pkg.conda"
+    assert url.clear_package()
+
+
+def test_conda_url_parse():
+    CondaURL = libmambapy.specs.CondaURL
+
+    url = CondaURL.parse(
+        "https://user%40mail.com:pas%23@repo.mamba.pm:400/t/xy-12345678-1234/%20conda/linux-64/pkg.conda"
+    )
+    assert url.scheme() == "https"
+    assert url.user() == "user@mail.com"
+    assert url.user(decode=False) == "user%40mail.com"
+    assert url.password() == "pas#"
+    assert url.password(decode=False) == "pas%23"
+    assert url.authentication() == "user%40mail.com:pas%23"
+    assert url.host() == "repo.mamba.pm"
+    assert url.host(decode=False) == "repo.mamba.pm"
+    assert url.port() == "400"
+    assert url.path() == "/t/xy-12345678-1234/ conda/linux-64/pkg.conda"
+    assert url.path(decode=False) == "/t/xy-12345678-1234/%20conda/linux-64/pkg.conda"
+    assert url.token() == "xy-12345678-1234"
+    assert url.path_without_token() == "/ conda/linux-64/pkg.conda"
+    assert url.path_without_token(decode=False) == "/%20conda/linux-64/pkg.conda"
+    assert url.platform().name == "linux_64"
+    assert url.package() == "pkg.conda"
+
+    assert (
+        url.str()
+        == "https://user%40mail.com:*****@repo.mamba.pm:400/t/*****/%20conda/linux-64/pkg.conda"
+    )
+    assert (
+        url.str(credentials="Show")
+        == "https://user%40mail.com:pas%23@repo.mamba.pm:400/t/xy-12345678-1234/%20conda/linux-64/pkg.conda"
+    )
+    assert repr(url) == (
+        "https://user%40mail.com:*****@repo.mamba.pm:400/t/*****/%20conda/linux-64/pkg.conda"
+    )
+    assert (
+        url.pretty_str(strip_scheme=True, credentials="Hide", rstrip_path="/")
+        == "user@mail.com:*****@repo.mamba.pm:400/t/*****/ conda/linux-64/pkg.conda"
+    )
+
+
+def test_conda_url_op():
+    CondaURL = libmambapy.specs.CondaURL
+    url = CondaURL.parse(
+        "https://user%40mail.com:pas%23@repo.mamba.pm:400/t/xy-12345678-1234/%20conda/linux-64/pkg.conda"
+    )
+
+    #  Copy
+    other = copy.deepcopy(url)
+    assert other.str() == url.str()
+
+    # Comparison
+    assert hash(url) != 0
+    assert url == url
+    assert other == url
+    other.set_host("somehost.com")
+    assert other != url
+
+
 def test_channel_spec_type():
     Type = libmambapy.specs.ChannelSpec.Type
 

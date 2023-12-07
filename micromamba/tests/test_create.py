d6e1ea4112fc20d606ab944be68a435d3fef5c97
Josh Chorlton <jchorlton@gmail.com>
2023-12-07 19:22:08 +0000

    add test for only one yaml specifies channel

--- micromamba/tests/test_create.py
+++ micromamba/tests/test_create.py
@@ -348,6 +348,36 @@ def test_multiple_spec_files_different_types(tmp_home, tmp_root_prefix, tmp_path
     assert "found multiple spec file types" in info.value.stderr.decode()
 
 
+@pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)
+def test_multiple_yaml_specs_only_one_has_channels(tmp_home, tmp_root_prefix, tmp_path):
+    env_prefix = tmp_path / "myenv"
+
+    cmd = ["-p", env_prefix]
+
+    spec_file_1 = tmp_path / f"env1.yaml"
+    file_content = ["dependencies: [xtensor]"]
+    with open(spec_file_1, "w") as f:
+        f.write("\n".join(file_content))
+
+    spec_file_2 = tmp_path / f"env2.yaml"
+    file_content = [
+        "dependencies: [xsimd]",
+        "channels: [bioconda]",
+    ]
+    with open(spec_file_2, "w") as f:
+        f.write("\n".join(file_content))
+
+    cmd += ["-f", spec_file_1, "-f", spec_file_2]
+
+    res = helpers.create(
+        *cmd,
+        "--print-config-only",
+        default_channel=False
+    )
+    assert res['channels'] == ['bioconda']
+    assert res['specs'] == ['xtensor', 'xsimd']
+
+
 def test_multiprocessing():
     if platform.system() == "Windows":
         return

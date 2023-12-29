5482fb12d7e08e7ea361ab2bb6af3af4ceeb160c
AntoinePrv <AntoinePrv@users.noreply.github.com>
2023-12-29 09:01:19 +0100

    Add util/parsers skeletton

---
+++ libmamba/src/util/parsers.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2023, QuantStack and Mamba Contributors
+//
+// Distributed under the terms of the BSD 3-Clause License.
+//
+// The full license is in the file LICENSE, distributed with this software.
+
+#include "mamba/util/parsers.hpp"
+
+namespace mamba::util
+{
+
+}

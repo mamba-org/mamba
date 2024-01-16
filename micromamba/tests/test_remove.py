087a0363f359a75753beddbbda27a4e46250b9cb
AntoinePrv <AntoinePrv@users.noreply.github.com>
2024-01-16 14:11:38 +0100

    Refactor TestRemove to use fixtures

--- micromamba/tests/test_remove.py
+++ micromamba/tests/test_remove.py
@@ -15,176 +15,153 @@ from . import helpers
 __this_dir__ = Path(__file__).parent.resolve()
 
 
-@pytest.mark.skipif(
-    helpers.dry_run_tests == helpers.DryRun.ULTRA_DRY, reason="Running ultra dry tests"
-)
-class TestRemove:
-    current_root_prefix = os.environ["MAMBA_ROOT_PREFIX"]
-    current_prefix = os.environ["CONDA_PREFIX"]
-    cache = os.path.join(current_root_prefix, "pkgs")
+@pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)
+@pytest.mark.parametrize("env_selector", ["", "name", "prefix"])
+def test_remove(tmp_home, tmp_root_prefix, env_selector, tmp_xtensor_env, tmp_env_name):
+    env_pkgs = [p["name"] for p in helpers.umamba_list("-p", tmp_xtensor_env, "--json")]
 
-    env_name = helpers.random_string()
-    root_prefix = os.path.expanduser(os.path.join("~", "tmproot" + helpers.random_string()))
-    prefix = os.path.join(root_prefix, "envs", env_name)
+    if env_selector == "prefix":
+        res = helpers.remove("xtensor", "-p", tmp_xtensor_env, "--json")
+    elif env_selector == "name":
+        res = helpers.remove("xtensor", "-n", tmp_env_name, "--json")
+    else:
+        res = helpers.remove("xtensor", "--dry-run", "--json")
 
-    @staticmethod
-    @pytest.fixture(scope="class")
-    def root(existing_cache):
-        os.environ["MAMBA_ROOT_PREFIX"] = TestRemove.root_prefix
-        os.environ["CONDA_PREFIX"] = TestRemove.prefix
-        helpers.create("-n", "base", no_dry_run=True)
-        helpers.create("xtensor", "-n", TestRemove.env_name, no_dry_run=True)
+    keys = {"dry_run", "success", "prefix", "actions"}
+    assert keys.issubset(set(res.keys()))
+    assert res["success"]
+    assert len(res["actions"]["UNLINK"]) == len(env_pkgs)
+    for p in res["actions"]["UNLINK"]:
+        assert p["name"] in env_pkgs
+    assert res["actions"]["PREFIX"] == str(tmp_xtensor_env)
 
-        yield
 
-        os.environ["MAMBA_ROOT_PREFIX"] = TestRemove.current_root_prefix
-        os.environ["CONDA_PREFIX"] = TestRemove.current_prefix
-        shutil.rmtree(TestRemove.root_prefix)
+@pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)
+@pytest.mark.skipif(sys.platform == "win32", reason="This test is currently failing on Windows")
+def test_remove_orphaned(tmp_home, tmp_root_prefix, tmp_xtensor_env, tmp_env_name):
+    env_pkgs = [p["name"] for p in helpers.umamba_list("-p", tmp_xtensor_env, "--json")]
+    helpers.install("xframe", "-n", tmp_env_name, no_dry_run=True)
 
-    @staticmethod
-    @pytest.fixture
-    def env_created(root):
-        if helpers.dry_run_tests == helpers.DryRun.OFF:
-            helpers.install("xtensor", "-n", TestRemove.env_name)
+    res = helpers.remove("xframe", "-p", tmp_xtensor_env, "--json")
 
-    @pytest.mark.parametrize("env_selector", ["", "name", "prefix"])
-    def test_remove(self, env_selector, env_created):
-        env_pkgs = [p["name"] for p in helpers.umamba_list("-p", TestRemove.prefix, "--json")]
+    keys = {"dry_run", "success", "prefix", "actions"}
+    assert keys.issubset(set(res.keys()))
+    assert res["success"]
+    assert len(res["actions"]["UNLINK"]) == 1
+    assert res["actions"]["UNLINK"][0]["name"] == "xframe"
+    assert res["actions"]["PREFIX"] == str(tmp_xtensor_env)
 
-        if env_selector == "prefix":
-            res = helpers.remove("xtensor", "-p", TestRemove.prefix, "--json")
-        elif env_selector == "name":
-            res = helpers.remove("xtensor", "-n", TestRemove.env_name, "--json")
+    res = helpers.remove("xtensor", "-p", tmp_xtensor_env, "--json")
+
+    keys = {"dry_run", "success", "prefix", "actions"}
+    assert keys.issubset(set(res.keys()))
+    assert res["success"]
+    assert len(res["actions"]["UNLINK"]) == len(env_pkgs) + (
+        1 if helpers.dry_run_tests == helpers.DryRun.DRY else 0
+    )
+    for p in res["actions"]["UNLINK"]:
+        assert p["name"] in env_pkgs
+    assert res["actions"]["PREFIX"] == str(tmp_xtensor_env)
+
+
+@pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)
+def test_remove_force(tmp_home, tmp_root_prefix, tmp_xtensor_env, tmp_env_name):
+    # check that we can remove a package without solving the environment (putting
+    # it in a bad state, actually)
+    helpers.install("xframe", "-n", tmp_env_name, no_dry_run=True)
+
+    res = helpers.remove("xtl", "-p", tmp_xtensor_env, "--json", "--force")
+
+    keys = {"dry_run", "success", "prefix", "actions"}
+    assert keys.issubset(set(res.keys()))
+    assert res["success"]
+    assert len(res["actions"]["UNLINK"]) == 1
+    assert res["actions"]["UNLINK"][0]["name"] == "xtl"
+    assert res["actions"]["PREFIX"] == str(tmp_xtensor_env)
+
+
+@pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)
+def test_remove_no_prune_deps(tmp_home, tmp_root_prefix, tmp_xtensor_env, tmp_env_name):
+    helpers.install("xframe", "-n", tmp_env_name, no_dry_run=True)
+
+    res = helpers.remove("xtensor", "-p", tmp_xtensor_env, "--json", "--no-prune-deps")
+
+    keys = {"dry_run", "success", "prefix", "actions"}
+    assert keys.issubset(set(res.keys()))
+    assert res["success"]
+    assert len(res["actions"]["UNLINK"]) == 2
+    removed_names = [x["name"] for x in res["actions"]["UNLINK"]]
+    assert "xtensor" in removed_names
+    assert "xframe" in removed_names
+    assert res["actions"]["PREFIX"] == str(tmp_xtensor_env)
+
+
+@pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)
+def test_remove_in_use(tmp_home, tmp_root_prefix, tmp_xtensor_env, tmp_env_name):
+    helpers.install("python=3.9", "-n", tmp_env_name, "--json", no_dry_run=True)
+    if platform.system() == "Windows":
+        pyexe = Path(tmp_xtensor_env) / "python.exe"
+    else:
+        pyexe = Path(tmp_xtensor_env) / "bin" / "python"
+
+    env = helpers.get_fake_activate(tmp_xtensor_env)
+
+    pyproc = subprocess.Popen(
+        pyexe, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, env=env
+    )
+    time.sleep(1)
+
+    helpers.remove("python", "-v", "-p", tmp_xtensor_env, no_dry_run=True)
+
+    if platform.system() == "Windows":
+        pyexe_trash = Path(str(pyexe) + ".mamba_trash")
+        assert pyexe.exists() is False
+        pyexe_trash_exists = pyexe_trash.exists()
+        trash_file = Path(tmp_xtensor_env) / "conda-meta" / "mamba_trash.txt"
+
+        if pyexe_trash_exists:
+            assert pyexe_trash.exists()
+            assert trash_file.exists()
+            all_trash_files = list(Path(tmp_xtensor_env).rglob("*.mamba_trash"))
+
+            with open(trash_file, "r") as fi:
+                lines = [x.strip() for x in fi.readlines()]
+                assert all([line.endswith(".mamba_trash") for line in lines])
+                assert len(all_trash_files) == len(lines)
+                linesp = [Path(tmp_xtensor_env) / line for line in lines]
+                for atf in all_trash_files:
+                    assert atf in linesp
         else:
-            res = helpers.remove("xtensor", "--dry-run", "--json")
+            assert trash_file.exists() is False
+            assert pyexe_trash.exists() is False
+        # No change if file still in use
+        helpers.install("cpp-filesystem", "-n", tmp_env_name, "--json", no_dry_run=True)
 
-        keys = {"dry_run", "success", "prefix", "actions"}
-        assert keys.issubset(set(res.keys()))
-        assert res["success"]
-        assert len(res["actions"]["UNLINK"]) == len(env_pkgs)
-        for p in res["actions"]["UNLINK"]:
-            assert p["name"] in env_pkgs
-        assert res["actions"]["PREFIX"] == TestRemove.prefix
+        if pyexe_trash_exists:
+            assert trash_file.exists()
+            assert pyexe_trash.exists()
 
-    @pytest.mark.skipif(sys.platform == "win32", reason="This test is currently failing on Windows")
-    def test_remove_orphaned(self, env_created):
-        env_pkgs = [p["name"] for p in helpers.umamba_list("-p", TestRemove.prefix, "--json")]
-        helpers.install("xframe", "-n", TestRemove.env_name, no_dry_run=True)
-
-        res = helpers.remove("xframe", "-p", TestRemove.prefix, "--json")
-
-        keys = {"dry_run", "success", "prefix", "actions"}
-        assert keys.issubset(set(res.keys()))
-        assert res["success"]
-        assert len(res["actions"]["UNLINK"]) == 1
-        assert res["actions"]["UNLINK"][0]["name"] == "xframe"
-        assert res["actions"]["PREFIX"] == TestRemove.prefix
-
-        res = helpers.remove("xtensor", "-p", TestRemove.prefix, "--json")
-
-        keys = {"dry_run", "success", "prefix", "actions"}
-        assert keys.issubset(set(res.keys()))
-        assert res["success"]
-        assert len(res["actions"]["UNLINK"]) == len(env_pkgs) + (
-            1 if helpers.dry_run_tests == helpers.DryRun.DRY else 0
-        )
-        for p in res["actions"]["UNLINK"]:
-            assert p["name"] in env_pkgs
-        assert res["actions"]["PREFIX"] == TestRemove.prefix
-
-    def test_remove_force(self, env_created):
-        # check that we can remove a package without solving the environment (putting
-        # it in a bad state, actually)
-        helpers.install("xframe", "-n", TestRemove.env_name, no_dry_run=True)
-
-        res = helpers.remove("xtl", "-p", TestRemove.prefix, "--json", "--force")
-
-        keys = {"dry_run", "success", "prefix", "actions"}
-        assert keys.issubset(set(res.keys()))
-        assert res["success"]
-        assert len(res["actions"]["UNLINK"]) == 1
-        assert res["actions"]["UNLINK"][0]["name"] == "xtl"
-        assert res["actions"]["PREFIX"] == TestRemove.prefix
-
-    def test_remove_no_prune_deps(self, env_created):
-        helpers.install("xframe", "-n", TestRemove.env_name, no_dry_run=True)
-
-        res = helpers.remove("xtensor", "-p", TestRemove.prefix, "--json", "--no-prune-deps")
-
-        keys = {"dry_run", "success", "prefix", "actions"}
-        assert keys.issubset(set(res.keys()))
-        assert res["success"]
-        assert len(res["actions"]["UNLINK"]) == 2
-        removed_names = [x["name"] for x in res["actions"]["UNLINK"]]
-        assert "xtensor" in removed_names
-        assert "xframe" in removed_names
-        assert res["actions"]["PREFIX"] == TestRemove.prefix
-
-    def test_remove_in_use(self, env_created):
-        helpers.install("python=3.9", "-n", self.env_name, "--json", no_dry_run=True)
-        if platform.system() == "Windows":
-            pyexe = Path(self.prefix) / "python.exe"
+            with open(trash_file, "r") as fi:
+                lines = [x.strip() for x in fi.readlines()]
+                assert all([line.endswith(".mamba_trash") for line in lines])
+                assert len(all_trash_files) == len(lines)
+                linesp = [Path(tmp_xtensor_env) / line for line in lines]
+                for atf in all_trash_files:
+                    assert atf in linesp
         else:
-            pyexe = Path(self.prefix) / "bin" / "python"
-
-        env = helpers.get_fake_activate(self.prefix)
-
-        pyproc = subprocess.Popen(
-            pyexe, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, env=env
-        )
-        time.sleep(1)
-
-        helpers.remove("python", "-v", "-p", self.prefix, no_dry_run=True)
-
-        if platform.system() == "Windows":
-            pyexe_trash = Path(str(pyexe) + ".mamba_trash")
-            assert pyexe.exists() is False
-            pyexe_trash_exists = pyexe_trash.exists()
-            trash_file = Path(self.prefix) / "conda-meta" / "mamba_trash.txt"
-
-            if pyexe_trash_exists:
-                assert pyexe_trash.exists()
-                assert trash_file.exists()
-                all_trash_files = list(Path(self.prefix).rglob("*.mamba_trash"))
-
-                with open(trash_file, "r") as fi:
-                    lines = [x.strip() for x in fi.readlines()]
-                    assert all([line.endswith(".mamba_trash") for line in lines])
-                    assert len(all_trash_files) == len(lines)
-                    linesp = [Path(self.prefix) / line for line in lines]
-                    for atf in all_trash_files:
-                        assert atf in linesp
-            else:
-                assert trash_file.exists() is False
-                assert pyexe_trash.exists() is False
-            # No change if file still in use
-            helpers.install("cpp-filesystem", "-n", self.env_name, "--json", no_dry_run=True)
-
-            if pyexe_trash_exists:
-                assert trash_file.exists()
-                assert pyexe_trash.exists()
-
-                with open(trash_file, "r") as fi:
-                    lines = [x.strip() for x in fi.readlines()]
-                    assert all([line.endswith(".mamba_trash") for line in lines])
-                    assert len(all_trash_files) == len(lines)
-                    linesp = [Path(self.prefix) / line for line in lines]
-                    for atf in all_trash_files:
-                        assert atf in linesp
-            else:
-                assert trash_file.exists() is False
-                assert pyexe_trash.exists() is False
-
-            subprocess.Popen("TASKKILL /F /PID {pid} /T".format(pid=pyproc.pid))
-            # check that another env mod clears lingering trash files
-            time.sleep(0.5)
-            helpers.install("xsimd", "-n", self.env_name, "--json", no_dry_run=True)
             assert trash_file.exists() is False
             assert pyexe_trash.exists() is False
 
-        else:
-            assert pyexe.exists() is False
-            pyproc.kill()
+        subprocess.Popen("TASKKILL /F /PID {pid} /T".format(pid=pyproc.pid))
+        # check that another env mod clears lingering trash files
+        time.sleep(0.5)
+        helpers.install("xsimd", "-n", tmp_env_name, "--json", no_dry_run=True)
+        assert trash_file.exists() is False
+        assert pyexe_trash.exists() is False
+
+    else:
+        assert pyexe.exists() is False
+        pyproc.kill()
 
 
 class TestRemoveConfig:

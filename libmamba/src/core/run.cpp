136773da76792cf41fc75a7df908045c6330ea6b
Mihai Babiac <mihai.babiac@sony.com>
2024-01-30 14:25:15 +0100

    Make signal propagation work more reliably
    We save and restore the signal mask and the signal handlers, as well as
    stop and restart our `default_signal_handler`.

--- libmamba/src/core/run.cpp
+++ libmamba/src/core/run.cpp
@@ -17,6 +17,7 @@ extern "C"
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 }
 #else
@@ -35,6 +36,7 @@ extern "C"
 #include "mamba/core/execution.hpp"
 #include "mamba/core/output.hpp"
 #include "mamba/core/run.hpp"
+#include "mamba/core/thread_utils.hpp"
 #include "mamba/core/util_os.hpp"
 #include "mamba/util/environment.hpp"
 #include "mamba/util/random.hpp"
@@ -283,6 +285,40 @@ namespace mamba
     }
 #endif
 
+    // Wait for a process to finish
+    // Similar to reproc::process.wait, but also handles suspension
+    int wait_for_process(pid_t pid)
+    {
+        int status;
+        pid_t wpid;
+        while (1)
+        {
+            wpid = waitpid(pid, &status, WUNTRACED);
+            std::cout << "Stopped waiting for process. Status: " << status << std::endl;
+            if (wpid == pid && WIFSTOPPED(status))
+            {
+                // If the child was suspended (e.g. CTRL-Z),
+                // suspend entire process group as well
+                kill(0, SIGSTOP);
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        // Compute the return status mimicking the private logic in reproc++
+        if (wpid < 0)
+        {
+            status = -errno;
+        }
+        else
+        {
+            status = WIFEXITED(status) ? WEXITSTATUS(status) : WTERMSIG(status) + 128;
+        }
+        return status;
+    }
+
     int run_in_environment(
         const Context& context,
         const fs::u8path& prefix,
@@ -442,23 +478,39 @@ namespace mamba
             }
 
 #ifndef _WIN32
-            MainExecutor::instance().schedule(
-                []()
-                {
-                    auto forward_signal = [](int signum) { kill(pid, signum); };
+            sigset_t empty_sigmask, prev_sigmask;
+            sigemptyset(&empty_sigmask);
 
-                    for (int signum = 1; signum < NSIG; signum++)
-                    {
-                        if (signum != SIGCHLD)
-                        {
-                            struct sigaction sa;
-                            sa.sa_flags = 0;
-                            sa.sa_handler = forward_signal;
-                            sigaction(signum, &sa, nullptr);
-                        }
-                    }
+            // Stop the normal signal handler so that SIGINT is forwarded as well,
+            // and unblock all signals, while saving the previous state
+            // TODO: Is this the right place to change the mask and is this the right order?
+            stop_receiver_thread();
+            pthread_sigmask(SIG_SETMASK, &empty_sigmask, &prev_sigmask);
+
+            std::map<int, struct sigaction> prev_actions;
+
+            // Signal handler lambda forwarding all signals except CHLD to the child
+            auto forward_signal = [](int signum)
+            {
+                if (signum != SIGCHLD)
+                {
+                    std::cout << "mamba forwarding signal " << signum << std::endl;
+                    kill(pid, signum);
                 }
-            );
+            };
+
+            // Set handlers for all signals and save the previous state.
+            for (int signum = 1; signum < NSIG; signum++)
+            {
+                struct sigaction sa, prev_sa;
+                sa.sa_flags = SA_RESTART;
+                sa.sa_handler = forward_signal;
+                int err = sigaction(signum, &sa, &prev_sa);
+                if (!err)
+                {
+                    prev_actions[signum] = prev_sa;
+                }
+            }
 #endif
 
             // check if we need this
@@ -467,19 +519,25 @@ namespace mamba
                 opt.redirect.parent = true;
             }
 
-
             ec = reproc::drain(proc, reproc::sink::null, reproc::sink::null);
 
-            do
-            {
-                std::tie(status, ec) = proc.stop(opt.stop);
-            } while (ec.value() == 4);  // we get error code 4 in case of a signal; Just call stop
-                                        // again
+#ifndef _WIN32
+            status = wait_for_process(pid);
 
+            // Restore the signal handlers and the signal mask
+            set_default_signal_handler();
+            pthread_sigmask(SIG_SETMASK, &prev_sigmask, nullptr);
+            for (const auto& [signum, sa] : prev_actions)
+            {
+                sigaction(signum, &sa, nullptr);
+            }
+#else
+            std::tie(status, ec) = proc.stop(opt.stop);
             if (ec)
             {
                 std::cerr << ec.message() << " ; error code " << ec.value() << '\n';
             }
+#endif
         }
         // exit with status code from reproc
         return status;

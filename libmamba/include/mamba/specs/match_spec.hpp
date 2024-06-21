b8795f7d70673ef7775efe7b9a18f4bd629c05af
Julien Jerphanion <git@jjerphan.xyz>
2024-06-21 18:15:12 +0200

    Add a test for resolvo
    Many changes are required to be able to use hashmaps.
    
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- libmamba/include/mamba/specs/match_spec.hpp
+++ libmamba/include/mamba/specs/match_spec.hpp
@@ -196,7 +196,6 @@ namespace mamba::specs
                 return !(*this == other);
             }
 
-            friend struct std::hash<ExtraMembers>;
         };
         friend struct std::hash<MatchSpec::ExtraMembers>;
 

e78169c353a3fc9869a9275b3861783ab58d8598
Julien Jerphanion <git@jjerphan.xyz>
2024-08-28 16:25:06 +0200

    Decrease timout to 15 seconds and use dry-run
    A dry run takes slightly less than 10 seconds.
    
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- micromamba/tests/test_create.py
+++ micromamba/tests/test_create.py
@@ -145,7 +145,7 @@ def test_env_lockfile_different_install_after_create(tmp_home, tmp_root_prefix, 
 
 # Only run this test on Linux, as it is the only platform where xeus-cling
 # (which is part of the environment) is available.
-@pytest.timeout(30)
+@pytest.mark.timeout(15)
 @pytest.mark.skipif(platform.system() != "Linux", reason="Test only available on Linux")
 @pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)
 def test_env_logging_overhead_regression(tmp_home, tmp_root_prefix, tmp_path):
@@ -156,9 +156,8 @@ def test_env_logging_overhead_regression(tmp_home, tmp_root_prefix, tmp_path):
 
     shutil.copyfile(__this_dir__ / "env-logging-overhead-regression.yaml", create_spec_file)
 
-    # Must not hang and must complete successfully
-    res = helpers.create("-p", env_prefix, "-f", create_spec_file, "-y", "--json")
-    assert res["success"]
+    # Must not hang
+    helpers.create("-p", env_prefix, "-f", create_spec_file, "-y", "--json", "--dry-run")
 
 
 @pytest.mark.parametrize("shared_pkgs_dirs", [True], indirect=True)

0ffa9af2953ea35bd6a4edf52a9549cd49b6c747
Johan Mabille <johan.mabille@gmail.com>
2024-09-30 19:37:55 +0200

    Fixed channel output in umamba list

--- libmamba/src/api/list.cpp
+++ libmamba/src/api/list.cpp
@@ -73,7 +73,7 @@ namespace mamba
 
                     if (regex.empty() || std::regex_search(pkg_info.name, spec_pat))
                     {
-                        auto channels = channel_context.make_channel(pkg_info.package_url);
+                        auto channels = channel_context.make_channel(pkg_info.channel);
                         assert(channels.size() == 1);  // A URL can only resolve to one channel
                         obj["base_url"] = channels.front().url().str(specs::CondaURL::Credentials::Remove
                         );
@@ -113,7 +113,7 @@ namespace mamba
                     }
                     else
                     {
-                        auto channels = channel_context.make_channel(package.second.package_url);
+                        auto channels = channel_context.make_channel(package.second.channel);
                         assert(channels.size() == 1);  // A URL can only resolve to one channel
                         formatted_pkgs.channel = channels.front().display_name();
                     }

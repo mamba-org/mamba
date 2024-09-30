3ce863e43c6d56e64bd8c3a944254b9bb82ca3ef
Johan Mabille <johan.mabille@gmail.com>
2024-09-30 18:37:23 +0200

    Added tests for umamba list --full-name

--- micromamba/src/list.cpp
+++ micromamba/src/list.cpp
@@ -28,10 +28,11 @@ init_list_parser(CLI::App* subcom, Configuration& config)
                                         .description("Only search for full names, i.e., ^<regex>$."));
     subcom->add_flag("-f,--full-name", full_name.get_cli_config<bool>(), full_name.description());
     
-    auto& canonical = config.insert(Configurable("canonical", false)
+    // TODO: implement this in libmamba/list.cpp
+    /*auto& canonical = config.insert(Configurable("canonical", false)
                                         .group("cli")
                                         .description("Output canonical names of packages only."));
-    subcom->add_flag("-c,--canonical", canonical.get_cli_config<bool>(), canonical.description());
+    subcom->add_flag("-c,--canonical", canonical.get_cli_config<bool>(), canonical.description());*/
 }
 
 void

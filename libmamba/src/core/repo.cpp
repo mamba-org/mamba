a2f6ed18effae21f184e42ade6c0e0eaaade3f58
Hind Montassif <hind.montassif@gmail.com>
2024-10-10 10:13:42 +0200

    Fix linter

--- libmamba/src/core/repo.cpp
+++ libmamba/src/core/repo.cpp
@@ -336,7 +336,8 @@ namespace mamba
             }
             catch (const std::exception& e)
             {
-                LOG_WARNING << "Could not write libsolv solv file for " << json_file << ": " << e.what();
+                LOG_WARNING << "Could not write libsolv solv file for " << json_file << ": "
+                            << e.what();
             }
         }
     }

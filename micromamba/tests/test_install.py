820a5923b4edd633479ecf5eaa62c61756b4a3b9
Hind Montassif <hind.montassif@gmail.com>
2024-10-17 13:50:06 +0200

    Correctly rename test to be run

--- micromamba/tests/test_install.py
+++ micromamba/tests/test_install.py
@@ -614,7 +614,7 @@ class TestInstall:
         reinstall_res = helpers.install("xtensor", "--json")
         assert "actions" not in reinstall_res
 
-    def install_local_package(self):
+    def test_install_local_package(self):
         """Attempts to install a .tar.bz2 package from a local directory."""
         file_path = Path(__file__).parent / "data" / "cph_test_data-0.0.1-0.tar.bz2"
 

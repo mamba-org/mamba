91db136a2cd31d4f3d55a620b5a0cf4b9dc96b03
Hind Montassif <hind.montassif@gmail.com>
2024-10-18 16:04:35 +0200

    Adjust comment

--- libmamba/include/mamba/specs/package_info.hpp
+++ libmamba/include/mamba/specs/package_info.hpp
@@ -50,7 +50,7 @@ namespace mamba::specs
         std::vector<std::string> track_features = {};
         std::vector<std::string> dependencies = {};
         std::vector<std::string> constrains = {};
-        // WARNING Be aware that `defaulted_keys` value, if set,
+        // WARNING Be aware that `defaulted_keys` value, if set later,
         // is not passed when going through `make_package_info` from libsolv
         std::vector<std::string> defaulted_keys = {};
         NoArchType noarch = NoArchType::No;
@@ -58,8 +58,9 @@ namespace mamba::specs
         std::size_t timestamp = 0;
         // FIXME this is a temporary hack to accommodate Python wheels but wheels and conda
         // PackageInfo, should really be split in different types.
-        // WARNING Be aware that `package_type` value, if set,
-        // is not passed when going through `make_package_info` from libsolv
+        // WARNING Be aware that `package_type` value,
+        // if set later to anything other than default (PackageType::Unknown),
+        // will not be passed when going through `make_package_info` from libsolv
         PackageType package_type = PackageType::Unknown;
 
         [[nodiscard]] static auto from_url(std::string_view url) -> expected_parse_t<PackageInfo>;

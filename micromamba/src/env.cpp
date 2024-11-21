69d5f0429828e76212daa6d5978643e398c57a17
Julien Jerphanion <git@jjerphan.xyz>
2024-11-21 10:13:40 +0100

    fixup! Address review comments

--- micromamba/src/env.cpp
+++ micromamba/src/env.cpp
@@ -230,7 +230,7 @@ set_env_command(CLI::App* com, Configuration& config)
                 }
 
                 // Add a `pip` subsection in `dependencies` listing wheels installed from PyPI
-                if (!pip_versions_map.empty() > 0)
+                if (!pip_versions_map.empty())
                 {
                     dependencies << (first_dependency_printed ? ",\n" : "") << "     { \"pip\": [\n";
                     first_dependency_printed = false;
@@ -321,7 +321,7 @@ set_env_command(CLI::App* com, Configuration& config)
                     }
                 }
                 // Add a `pip` subsection in `dependencies` listing wheels installed from PyPI
-                if (!pip_versions_map.empty() > 0)
+                if (!pip_versions_map.empty())
                 {
                     dependencies << "  - pip:\n";
                     for (const auto& [k, v] : pip_versions_map)

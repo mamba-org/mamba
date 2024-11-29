1228f10e45c6863156c16f72b024175161aefe32
Klaim (Joël Lamotte) <142265+Klaim@users.noreply.github.com>
2024-11-29 12:03:53 +0100

    added updated generated sources

--- libmambapy/src/libmambapy/version.py
+++ libmambapy/src/libmambapy/version.py
@@ -1,2 +1,5 @@
 version_info = ("2", "0", "4")
+version_prerelease = ""
 __version__ = ".".join(map(str, version_info))
+if version_prerelease != "":
+    __version__ = "{}.{}".format(__version__, version_prerelease)

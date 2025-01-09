624edf8a9e95709678ddfbe8f6d4da98e073c755
Julien Jerphanion <git@jjerphan.xyz>
2025-01-09 15:05:57 +0100

    test: Monkeypatch environment variable
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>
    
    Co-authored-by: jaimergp <jaimergp@users.noreply.github.com>

--- micromamba/tests/test_create.py
+++ micromamba/tests/test_create.py
@@ -1639,36 +1639,34 @@ def test_ca_certificates(tmp_path):
 
     assert root_prefix_ca_certificates_used or fall_back_certificates_used
 
-def test_glob_in_build_string(tmp_path):
+
+def test_glob_in_build_string(monkeypatch, tmp_path):
     # Non-regression test for https://github.com/mamba-org/mamba/issues/3699
     env_prefix = tmp_path / "test_glob_in_build_string"
 
     pytorch_match_spec = "pytorch=2.3.1=py3.10_cuda11.8*"
 
     # Export CONDA_OVERRIDE_GLIBC=2.17 to force the solver to use the glibc 2.17 package
-    try:
-        os.environ["CONDA_OVERRIDE_GLIBC"] = "2.17"
+    monkeypatch.setenv("CONDA_OVERRIDE_GLIBC", "2.17")
 
-        # Should run without error
-        out = helpers.create(
-            "-p",
-            env_prefix,
-            pytorch_match_spec,
-            "-c",
-            "pytorch",
-            "-c",
-            "nvidia/label/cuda-11.8.0",
-            "-c",
-            "nvidia",
-            "-c",
-            "conda-forge",
-            "--platform",
-            "linux-64",
-            "--dry-run",
-            "--json",
-        )
-    finally:
-        os.environ.pop("CONDA_OVERRIDE_GLIBC", None)
+    # Should run without error
+    out = helpers.create(
+        "-p",
+        env_prefix,
+        pytorch_match_spec,
+        "-c",
+        "pytorch",
+        "-c",
+        "nvidia/label/cuda-11.8.0",
+        "-c",
+        "nvidia",
+        "-c",
+        "conda-forge",
+        "--platform",
+        "linux-64",
+        "--dry-run",
+        "--json",
+    )
 
     # Check that a build of pytorch 2.3.1 with `py3.10_cuda11.8_cudnn8.7.0_0` as a build string is found
     assert any(

04faa2dab4b4024f397cc2815f14affc871562f9
Hind Montassif <hind.montassif@gmail.com>
2025-01-31 15:53:41 +0100

    Fix generic_string and add tests

--- libmamba/tests/src/core/test_transaction_context.cpp
+++ libmamba/tests/src/core/test_transaction_context.cpp
@@ -38,11 +38,15 @@ namespace mamba
         {
             REQUIRE(get_python_site_packages_short_path("").string() == "");
 
-            auto path = get_python_site_packages_short_path("3.5.0").string();
+            auto path = get_python_site_packages_short_path("3.5.0");
+            auto path_str = path.string();
+            auto path_gen_str = path.generic_string();
 #ifdef _WIN32
-            REQUIRE(path == "Lib\\site-packages");
+            REQUIRE(path_str == "Lib\\site-packages");
+            REQUIRE(path_gen_str == "Lib/site-packages");
 #else
-            REQUIRE(path == "lib/python3.5.0/site-packages");
+            REQUIRE(path_str == "lib/python3.5.0/site-packages");
+            REQUIRE(path_gen_str == "lib/python3.5.0/site-packages");
 #endif
         }
 
@@ -58,25 +62,36 @@ namespace mamba
 
         TEST_CASE("get_python_noarch_target_path")
         {
-            auto random_path = get_python_noarch_target_path("some_lib/some_folder", "bla").string();
+            auto random_path = get_python_noarch_target_path("some_lib/some_folder", "bla");
+            auto random_path_str = random_path.string();
+            auto random_path_gen_str = random_path.generic_string();
+
             auto sp_path = get_python_noarch_target_path(
-                               "site-packages/some_random_package",
-                               "target_site_packages_short_path"
-            )
-                               .string();
+                "site-packages/some_random_package",
+                "target_site_packages_short_path"
+            );
+            auto sp_path_str = sp_path.string();
+            auto sp_path_gen_str = sp_path.generic_string();
+
             auto ps_path = get_python_noarch_target_path(
-                               "python-scripts/some_random_file",
-                               "target_site_packages_short_path"
-            )
-                               .string();
+                "python-scripts/some_random_file",
+                "target_site_packages_short_path"
+            );
+            auto ps_path_str = ps_path.string();
+            auto ps_path_gen_str = ps_path.generic_string();
+
+            REQUIRE(random_path_gen_str == "some_lib/some_folder");
+            REQUIRE(sp_path_gen_str == "target_site_packages_short_path/some_random_package");
 #ifdef _WIN32
-            REQUIRE(random_path == "some_lib\\some_folder");
-            REQUIRE(sp_path == "target_site_packages_short_path\\some_random_package");
-            REQUIRE(ps_path == "Scripts\\some_random_file");
+            REQUIRE(random_path_str == "some_lib\\some_folder");
+            REQUIRE(sp_path_str == "target_site_packages_short_path\\some_random_package");
+            REQUIRE(ps_path_str == "Scripts\\some_random_file");
+            REQUIRE(ps_path_gen_str == "Scripts/some_random_file");
 #else
-            REQUIRE(random_path == "some_lib/some_folder");
-            REQUIRE(sp_path == "target_site_packages_short_path/some_random_package");
-            REQUIRE(ps_path == "bin/some_random_file");
+            REQUIRE(random_path_str == "some_lib/some_folder");
+            REQUIRE(sp_path_str == "target_site_packages_short_path/some_random_package");
+            REQUIRE(ps_path_str == "bin/some_random_file");
+            REQUIRE(ps_path_gen_str == "bin/some_random_file");
 #endif
         }
     }

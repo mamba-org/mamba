b50a6bb23f7098078d95e7e33ad7d9e8f645b4e0
Hind Montassif <hind.montassif@gmail.com>
2025-01-31 13:45:11 +0100

    Fix win tests

--- libmamba/tests/src/core/test_transaction_context.cpp
+++ libmamba/tests/src/core/test_transaction_context.cpp
@@ -40,7 +40,7 @@ namespace mamba
 
             auto path = get_python_site_packages_short_path("3.5.0").string();
 #ifdef _WIN32
-            REQUIRE(path == "Lib/site-packages");
+            REQUIRE(path == "Lib\\site-packages");
 #else
             REQUIRE(path == "lib/python3.5.0/site-packages");
 #endif
@@ -58,27 +58,25 @@ namespace mamba
 
         TEST_CASE("get_python_noarch_target_path")
         {
-            REQUIRE(
-                get_python_noarch_target_path("lib/site-packages", "bla").string() == "lib/site-packages"
-            );
-            REQUIRE(
-                get_python_noarch_target_path(
-                    "site-packages/some_random_package",
-                    "target_site_packages_short_path"
-                )
-                    .string()
-                == "target_site_packages_short_path/some_random_package"
-            );
-
-            auto path = get_python_noarch_target_path(
-                            "python-scripts/some_random_file",
-                            "target_site_packages_short_path"
+            auto random_path = get_python_noarch_target_path("some_lib/some_folder", "bla").string();
+            auto sp_path = get_python_noarch_target_path(
+                               "site-packages/some_random_package",
+                               "target_site_packages_short_path"
             )
-                            .string();
+                               .string();
+            auto ps_path = get_python_noarch_target_path(
+                               "python-scripts/some_random_file",
+                               "target_site_packages_short_path"
+            )
+                               .string();
 #ifdef _WIN32
-            REQUIRE(path == "Scripts/some_random_file");
+            REQUIRE(random_path == "some_lib\\some_folder");
+            REQUIRE(sp_path == "target_site_packages_short_path\\some_random_package");
+            REQUIRE(ps_path == "Scripts\\some_random_file");
 #else
-            REQUIRE(path == "bin/some_random_file");
+            REQUIRE(random_path == "some_lib/some_folder");
+            REQUIRE(sp_path == "target_site_packages_short_path/some_random_package");
+            REQUIRE(ps_path == "bin/some_random_file");
 #endif
         }
     }

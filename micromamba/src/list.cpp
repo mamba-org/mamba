b5e197fc1421dc356e5057e061767e6d29c7b7b2
Sandrine Pataut <pataut.sandrine@gmail.com>
2025-02-04 19:25:10 +0100

    feat: add export flag to list command (#3780)

--- micromamba/src/list.cpp
+++ micromamba/src/list.cpp
@@ -53,9 +53,19 @@ init_list_parser(CLI::App* subcom, Configuration& config)
     auto& canonical = config.insert(
         Configurable("canonical", false)
             .group("cli")
-            .description("Output canonical names of packages only. Ignored if --explicit.")
+            .description("Output canonical names of packages only. Ignored if --explicit is also provided."
+            )
     );
     subcom->add_flag("-c,--canonical", canonical.get_cli_config<bool>(), canonical.description());
+
+    auto& export_ = config.insert(
+        Configurable("export", false)
+            .group("cli")
+            .description(
+                "Output explicit, machine-readable requirement strings instead of human-readable lists of packages. Ignored if --explicit or --canonical is also provided."
+            )
+    );
+    subcom->add_flag("-e,--export", export_.get_cli_config<bool>(), export_.description());
 }
 
 void

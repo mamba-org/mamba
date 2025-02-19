fb125aeb9b463c1cc6e3dc11e2199b3b84ba5dc4
Sandrine Pataut <pataut.sandrine@gmail.com>
2025-02-19 10:56:41 +0100

    address review comment

--- libmamba/src/api/info.cpp
+++ libmamba/src/api/info.cpp
@@ -4,8 +4,6 @@
 //
 // The full license is in the file LICENSE, distributed with this software.
 
-#include <iostream>
-
 #include "mamba/api/configuration.hpp"
 #include "mamba/api/info.hpp"
 #include "mamba/core/channel_context.hpp"

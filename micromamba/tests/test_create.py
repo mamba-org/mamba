55c5caf327389b753c35252337c3f7132f8a10ed
Burt Holzman <burt@fnal.gov>
2025-02-24 16:34:29 -0600

    Remove debug statement from tests

--- micromamba/tests/test_create.py
+++ micromamba/tests/test_create.py
@@ -688,9 +688,6 @@ def test_create_envs_dirs(tmp_root_prefix: Path, tmp_path: Path, monkeypatch):
             check=True,
         )
 
-        res = subprocess.run(["icacls", noperm_root_dir], stdout=subprocess.PIPE, text=True)
-        print(res)
-
     else:
         os.chmod(noperm_root_dir, 0o555)
 

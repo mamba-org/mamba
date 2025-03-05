85d793595e8879d7b2bde48f4e0af67ccab44b5d
Sandrine Pataut <pataut.sandrine@gmail.com>
2025-03-05 11:30:34 +0100

    edit tests and copyright year for new files

--- micromamba/tests/test_info.py
+++ micromamba/tests/test_info.py
@@ -79,22 +79,7 @@ def test_not_env(tmp_home, tmp_root_prefix, prefix_selection, existing_prefix):
     assert f"base environment : {tmp_root_prefix}" in infos
 
 
-@pytest.mark.parametrize("base_flag", ["", "--base"])
-@pytest.mark.parametrize("envs_flag", ["", "-e", "--envs"])
-@pytest.mark.parametrize("json_flag", ["", "--json"])
-@pytest.mark.parametrize("prefix_selection", [None, "prefix", "name"])
-def test_subcommand(
-    tmp_home, tmp_root_prefix, prefix_selection, base_flag, envs_flag, json_flag, monkeypatch
-):
-    monkeypatch.setenv("CONDA_PREFIX", str(tmp_root_prefix))
-
-    if prefix_selection == "prefix":
-        infos = helpers.info("-p", tmp_root_prefix, base_flag, envs_flag, json_flag)
-    elif prefix_selection == "name":
-        infos = helpers.info("-n", "base", base_flag, envs_flag, json_flag)
-    else:
-        infos = helpers.info(base_flag, envs_flag, json_flag)
-
+def flags_test(tmp_root_prefix, infos, base_flag, envs_flag, json_flag):
     items = [
         "libmamba version",
         "mamba version",
@@ -140,3 +125,46 @@ def test_subcommand(
             assert all(i in infos.keys() for i in items)
         else:
             assert all(f"{i} :" in infos for i in items)
+
+
+@pytest.mark.parametrize("base_flag", ["", "--base"])
+@pytest.mark.parametrize("envs_flag", ["", "-e", "--envs"])
+@pytest.mark.parametrize("json_flag", ["", "--json"])
+@pytest.mark.parametrize("prefix_selection", [None, "prefix", "name"])
+def test_base_flags(
+    tmp_home, tmp_root_prefix, prefix_selection, base_flag, envs_flag, json_flag, monkeypatch
+):
+    monkeypatch.setenv("CONDA_PREFIX", str(tmp_root_prefix))
+
+    if prefix_selection == "prefix":
+        infos = helpers.info("-p", tmp_root_prefix, base_flag, envs_flag, json_flag)
+    elif prefix_selection == "name":
+        infos = helpers.info("-n", "base", base_flag, envs_flag, json_flag)
+    else:
+        infos = helpers.info(base_flag, envs_flag, json_flag)
+
+    flags_test(tmp_root_prefix, infos, base_flag, envs_flag, json_flag)
+
+
+@pytest.mark.parametrize("base_flag", ["", "--base"])
+@pytest.mark.parametrize("envs_flag", ["", "-e", "--envs"])
+@pytest.mark.parametrize("json_flag", ["", "--json"])
+@pytest.mark.parametrize("prefix_selection", [None, "prefix", "name"])
+def test_env_flags(
+    tmp_home,
+    tmp_root_prefix,
+    tmp_env_name,
+    tmp_prefix,
+    prefix_selection,
+    base_flag,
+    envs_flag,
+    json_flag,
+):
+    if prefix_selection == "prefix":
+        infos = helpers.info("-p", tmp_prefix, base_flag, envs_flag, json_flag)
+    elif prefix_selection == "name":
+        infos = helpers.info("-n", tmp_env_name, base_flag, envs_flag, json_flag)
+    else:
+        infos = helpers.info(base_flag, envs_flag, json_flag)
+
+    flags_test(tmp_root_prefix, infos, base_flag, envs_flag, json_flag)

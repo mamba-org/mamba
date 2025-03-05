b6b1143b1be1d59a06e5ae1847e6e6a25c242b11
Sandrine Pataut <pataut.sandrine@gmail.com>
2025-03-05 16:11:55 +0100

    feat: add envs flag to info command (#3837)

---
+++ libmamba/src/api/env.cpp
@@ -0,0 +1,78 @@
+// Copyright (c) 2025, QuantStack and Mamba Contributors
+//
+// Distributed under the terms of the BSD 3-Clause License.
+//
+// The full license is in the file LICENSE, distributed with this software.
+
+#include <iostream>
+
+#include "mamba/api/configuration.hpp"
+#include "mamba/api/env.hpp"
+#include "mamba/core/environments_manager.hpp"
+#include "mamba/util/string.hpp"
+
+namespace mamba
+{
+    void print_envs(Configuration& config)
+    {
+        config.load();
+        mamba::detail::print_envs_impl(config);
+    }
+
+    namespace detail
+    {
+        std::string get_env_name(const Context& ctx, const mamba::fs::u8path& px)
+        {
+            auto& ed = ctx.envs_dirs[0];
+            if (px == ctx.prefix_params.root_prefix)
+            {
+                return "base";
+            }
+            else if (util::starts_with(px.string(), ed.string()))
+            {
+                return mamba::fs::relative(px, ed).string();
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        void print_envs_impl(const Configuration& config)
+        {
+            const auto& ctx = config.context();
+
+            EnvironmentsManager env_manager{ ctx };
+
+            if (ctx.output_params.json)
+            {
+                nlohmann::json res;
+                const auto pfxs = env_manager.list_all_known_prefixes();
+                std::vector<std::string> envs(pfxs.size());
+                std::transform(
+                    pfxs.begin(),
+                    pfxs.end(),
+                    envs.begin(),
+                    [](const mamba::fs::u8path& path) { return path.string(); }
+                );
+                res["envs"] = envs;
+                std::cout << res.dump(4) << std::endl;
+                return;
+            }
+
+            // format and print table
+            printers::Table t({ "Name", "Active", "Path" });
+            t.set_alignment(
+                { printers::alignment::left, printers::alignment::left, printers::alignment::left }
+            );
+            t.set_padding({ 2, 2, 2 });
+
+            for (auto& env : env_manager.list_all_known_prefixes())
+            {
+                bool is_active = (env == ctx.prefix_params.target_prefix);
+                t.add_row({ get_env_name(ctx, env), is_active ? "*" : "", env.string() });
+            }
+            t.print(std::cout);
+        }
+    }
+}
--- micromamba/tests/test_info.py
+++ micromamba/tests/test_info.py
@@ -79,21 +79,7 @@ def test_not_env(tmp_home, tmp_root_prefix, prefix_selection, existing_prefix):
     assert f"base environment : {tmp_root_prefix}" in infos
 
 
-@pytest.mark.parametrize("base_flag", ["", "--base"])
-@pytest.mark.parametrize("json_flag", ["", "--json"])
-@pytest.mark.parametrize("prefix_selection", [None, "prefix", "name"])
-def test_base_subcommand(
-    tmp_home, tmp_root_prefix, prefix_selection, base_flag, json_flag, monkeypatch
-):
-    monkeypatch.setenv("CONDA_PREFIX", str(tmp_root_prefix))
-
-    if prefix_selection == "prefix":
-        infos = helpers.info("-p", tmp_root_prefix, base_flag, json_flag)
-    elif prefix_selection == "name":
-        infos = helpers.info("-n", "base", base_flag, json_flag)
-    else:
-        infos = helpers.info(base_flag, json_flag)
-
+def flags_test(tmp_root_prefix, infos, base_flag, envs_flag, json_flag):
     items = [
         "libmamba version",
         "mamba version",
@@ -117,13 +103,68 @@ def test_base_subcommand(
         else:
             assert all(
                 (f"{i} :" not in infos) | (f"\n{i} :" not in infos) for i in items
-            )  # f"\n{i} :" is to handle the case of the "environment" item
+            )  # f"\n{i} :" is there to handle the case of the "environment" item (which appears in "base environment" as well)
             base_environment_path = infos.replace("base environment :", "").strip()
         assert os.path.exists(base_environment_path)
         assert base_environment_path == str(tmp_root_prefix)
+    elif envs_flag in ["-e", "--envs"]:
+        if json_flag == "--json":
+            assert str(tmp_root_prefix) in infos["envs"]
+        else:
+            assert "Name" in infos
+            assert "base" in infos
+            assert str(tmp_root_prefix) in infos
+            all_lines = infos.splitlines()
+            for line in all_lines:
+                if "*" in line:
+                    active_env_l = line
+            assert str(tmp_root_prefix) in active_env_l
     else:
         items += ["base environment"]
         if json_flag == "--json":
             assert all(i in infos.keys() for i in items)
         else:
             assert all(f"{i} :" in infos for i in items)
+
+
+@pytest.mark.parametrize("base_flag", ["", "--base"])
+@pytest.mark.parametrize("envs_flag", ["", "-e", "--envs"])
+@pytest.mark.parametrize("json_flag", ["", "--json"])
+@pytest.mark.parametrize("prefix_selection", [None, "prefix", "name"])
+def test_base_flags(
+    tmp_home, tmp_root_prefix, prefix_selection, base_flag, envs_flag, json_flag, monkeypatch
+):
+    monkeypatch.setenv("CONDA_PREFIX", str(tmp_root_prefix))
+
+    if prefix_selection == "prefix":
+        infos = helpers.info("-p", tmp_root_prefix, base_flag, envs_flag, json_flag)
+    elif prefix_selection == "name":
+        infos = helpers.info("-n", "base", base_flag, envs_flag, json_flag)
+    else:
+        infos = helpers.info(base_flag, envs_flag, json_flag)
+
+    flags_test(tmp_root_prefix, infos, base_flag, envs_flag, json_flag)
+
+
+@pytest.mark.parametrize("base_flag", ["", "--base"])
+@pytest.mark.parametrize("envs_flag", ["", "-e", "--envs"])
+@pytest.mark.parametrize("json_flag", ["", "--json"])
+@pytest.mark.parametrize("prefix_selection", [None, "prefix", "name"])
+def test_env_flags(
+    tmp_home,
+    tmp_root_prefix,
+    tmp_env_name,
+    tmp_prefix,
+    prefix_selection,
+    base_flag,
+    envs_flag,
+    json_flag,
+):
+    if prefix_selection == "prefix":
+        infos = helpers.info("-p", tmp_prefix, base_flag, envs_flag, json_flag)
+    elif prefix_selection == "name":
+        infos = helpers.info("-n", tmp_env_name, base_flag, envs_flag, json_flag)
+    else:
+        infos = helpers.info(base_flag, envs_flag, json_flag)
+
+    flags_test(tmp_root_prefix, infos, base_flag, envs_flag, json_flag)

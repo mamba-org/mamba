1e66fabb8faabac0098d55666745feb3d1c223c3
Hind Montassif <hind.montassif@gmail.com>
2025-03-07 14:13:31 +0100

    release libmamba 2.0.7, micromamba 2.0.7, libmambapy 2.0.7

--- libmambapy/src/libmambapy/version.py
+++ libmambapy/src/libmambapy/version.py
@@ -1,5 +1,5 @@
 version_info = ("2", "0", "7")
-version_prerelease = "rc1"
+version_prerelease = ""
 __version__ = ".".join(map(str, version_info))
 if version_prerelease != "":
     __version__ = f"{__version__}.{version_prerelease}"

7c4ba67f64a277dba061f66234874ee09935f7eb
Klaim (Joël Lamotte) <142265+Klaim@users.noreply.github.com>
2025-03-14 18:15:02 +0100

    flat_set: allow searching using compatible type instead of value_type

--- libmamba/include/mamba/util/flat_set.hpp
+++ libmamba/include/mamba/util/flat_set.hpp
@@ -112,7 +112,11 @@ namespace mamba::util
         auto erase(const_iterator first, const_iterator last) -> const_iterator;
         auto erase(const value_type& value) -> size_type;
 
-        auto contains(const value_type&) const -> bool;
+        template<class T,
+            // TODO: C++ >= 20 replace by concept
+            class = std::enable_if_t< std::is_convertible_v<T, value_type> || std::is_convertible_v<value_type, T> >
+        >
+        auto contains(const T& value) const -> bool;
 
     private:
 
@@ -411,7 +415,8 @@ namespace mamba::util
     }
 
     template <typename K, typename C, typename A>
-    auto flat_set<K, C, A>::contains(const value_type& value) const -> bool
+    template<class T, class>
+    auto flat_set<K, C, A>::contains(const T& value) const -> bool
     {
         return std::binary_search(begin(), end(), value);
     }

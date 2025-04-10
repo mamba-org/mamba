e7d5f93a5c2974ae08ecd7001d0181cab749e08a
Sandrine Pataut <pataut.sandrine@gmail.com>
2025-04-10 16:50:40 +0200

    apply the same fix to removed packages to avoid future bugs

--- libmamba/src/core/transaction.cpp
+++ libmamba/src/core/transaction.cpp
@@ -429,21 +429,21 @@ namespace mamba
         // Channels coming from the repodata (packages to install) don't have the same channel
         // format than packages coming from the prefix (packages to remove). We set all the channels
         // to be URL like (i.e. explicit). Below is a loop to fix the channel of the linked
-        // packages. Ideally, this should be normalised when reading the data.
-        for_each_to_install(
-            m_solution.actions,
-            [&](specs::PackageInfo& pkg)
-            {
-                auto unresolved_pkg_channel = mamba::specs::UnresolvedChannel::parse(pkg.channel).value();
-                auto pkg_channel = mamba::specs::Channel::resolve(
-                                       unresolved_pkg_channel,
-                                       channel_context.params()
-                )
-                                       .value();
-                auto channel_url = pkg_channel[0].platform_url(pkg.platform).str();
-                pkg.channel = channel_url;
-            }
-        );
+        // packages (fix applied to the unlinked packages to avoid potential bugs). Ideally, this
+        // should be normalised when reading the data.
+        const auto fix_channel = [&](specs::PackageInfo& pkg)
+        {
+            auto unresolved_pkg_channel = mamba::specs::UnresolvedChannel::parse(pkg.channel).value();
+            auto pkg_channel = mamba::specs::Channel::resolve(
+                                   unresolved_pkg_channel,
+                                   channel_context.params()
+            )
+                                   .value();
+            auto channel_url = pkg_channel[0].platform_url(pkg.platform).str();
+            pkg.channel = channel_url;
+        };
+        for_each_to_install(m_solution.actions, fix_channel);
+        for_each_to_remove(m_solution.actions, fix_channel);
 
         const auto link = [&](const specs::PackageInfo& pkg)
         {

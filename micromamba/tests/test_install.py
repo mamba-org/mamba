65183cc89b758fcc35b7ae29753f8774ff5c58a1
Sandrine Pataut <pataut.sandrine@gmail.com>
2025-05-23 17:42:50 +0200

    add test

--- micromamba/tests/test_install.py
+++ micromamba/tests/test_install.py
@@ -6,6 +6,7 @@ import sys
 from pathlib import Path
 from packaging.version import Version
 
+import re
 import pytest
 
 # Need to import everything to get fixtures
@@ -844,3 +845,19 @@ def test_dry_run_pip_section(tmp_home, tmp_root_prefix, tmp_path):
     # Check that the packages are not installed using `pip`
     res = helpers.umamba_run("-p", env_prefix, "pip", "list")
     assert "numpy" not in res
+
+
+def test_history(tmp_home, tmp_root_prefix):
+    env_name = "myenv"
+    helpers.create("-n", env_name, "python=3.8")
+    helpers.install("-n", env_name, "xtl")
+    helpers.uninstall("-n", env_name, "xtl")
+
+    effective_prefix = tmp_root_prefix / "envs" / "myenv"
+    history_path = effective_prefix / "conda-meta" / "history"
+    assert (history_path).exists()
+    with open(history_path) as f:
+        history = f.read()
+    print(history)
+    assert len(re.findall(r"\+https:\/\/conda.anaconda.org\/conda-forge\/.+::xtl", history)) > 0
+    assert len(re.findall(r"-https:\/\/conda.anaconda.org\/conda-forge\/.+::xtl", history)) > 0

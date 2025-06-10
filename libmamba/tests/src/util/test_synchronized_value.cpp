6225b161af07b8fc9224900377f2911676abc11b
Joël Lamotte (Klaim) <joel.lamotte@quantstack.net>
2025-06-10 16:26:26 +0200

    util: synchronized_value basic interface and tests

--- libmamba/tests/src/util/test_synchronized_value.cpp
+++ libmamba/tests/src/util/test_synchronized_value.cpp
@@ -4,6 +4,23 @@
 //
 // The full license is in the file LICENSE, distributed with this software.
 
+#include <mutex>
+#include <shared_mutex>
 
 #include "mamba/util/synchronized_value.hpp"
 
+namespace mamba::util
+{
+    static_assert(BasicLockable<std::mutex>);
+    static_assert(BasicLockable<std::recursive_mutex>);
+    static_assert(BasicLockable<std::shared_mutex>);
+
+    static_assert(Lockable<std::mutex>);
+    static_assert(Lockable<std::recursive_mutex>);
+    static_assert(Lockable<std::shared_mutex>);
+
+
+    static_assert(Mutex<std::mutex>);
+    static_assert(Mutex<std::recursive_mutex>);
+    static_assert(Mutex<std::shared_mutex>);
+}

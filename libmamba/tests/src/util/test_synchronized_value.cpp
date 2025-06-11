70267df6156e67e70508b587c02bfaf845c3dab9
Joël Lamotte (Klaim) <joel.lamotte@quantstack.net>
2025-06-11 17:59:53 +0200

    wip basic impl and tests

--- libmamba/tests/src/util/test_synchronized_value.cpp
+++ libmamba/tests/src/util/test_synchronized_value.cpp
@@ -1,8 +1,10 @@
-// Copyright (c) 2023, QuantStack and Mamba Contributors
+// Copyright (c) 2025, QuantStack and Mamba Contributors
 //
 // Distributed under the terms of the BSD 3-Clause License.
 //
 // The full license is in the file LICENSE, distributed with this software.
+
+#include <catch2/catch_all.hpp>
 
 #include <mutex>
 #include <shared_mutex>
@@ -23,4 +25,22 @@ namespace mamba::util
     static_assert(Mutex<std::mutex>);
     static_assert(Mutex<std::recursive_mutex>);
     static_assert(Mutex<std::shared_mutex>);
+}
+
+// TODO: parametrize with a various set of basic types
+
+namespace {
+    TEST_CASE("synchronized_value")
+    {
+        SECTION("default constructible")
+        {
+            mamba::util::synchronized_value<int> a;
+        }
+
+        SECTION("basic value access")
+        {
+            mamba::util::synchronized_value<int> a{ 42 };
+            REQUIRE(a.value() == 42);
+        }
+    }
 }

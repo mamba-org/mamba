4e9f5bedb68f90d8b4c6403d1e77410492b58c11
Pierrick Roger <pierrick.rogermele@cea.fr>
2025-07-10 13:32:20 +0200

    Add a function to check permissions on a path.

--- libmamba/src/fs/filesystem.cpp
+++ libmamba/src/fs/filesystem.cpp
@@ -12,7 +12,10 @@
 
 #ifndef _WIN32
 #include <fcntl.h>
+#include <grp.h>
+#include <pwd.h>
 #include <sys/stat.h>
+#include <unistd.h>
 // We can use the presence of UTIME_OMIT to detect platforms that provide
 // utimensat.
 #if defined(UTIME_OMIT)
@@ -77,6 +80,76 @@ namespace mamba::fs
 #else
         auto new_time = fs::file_time_type::clock::now();
         std::filesystem::last_write_time(path, new_time, ec);
+#endif
+    }
+
+
+    bool has_modification_rights_on(const u8path& path) {
+
+#if defined(_WIN32) // All Windows platforms
+        return true;
+
+#else // UNIX-like platforms
+
+        using std::filesystem::perms;
+
+        // Get permissions
+        auto perm = std::filesystem::status(path).permissions();
+
+        // Everybody can modify
+        if (perms::none != (perm & perms::others_write))
+            return true;
+
+        // Check user and/or group
+        if (perms::none != (perm & (perms::owner_write | perms::group_write))) {
+
+            // Get path status
+            struct stat info;
+            if (stat(path.string().c_str(), &info))
+                throw std::runtime_error("Unable to get status information for"
+                                         " path " + path.string() + ": "
+                                         + strerror(errno));
+
+            // Check user rights
+            if (perms::none != (perm & perms::owner_write)
+                && info.st_uid == geteuid())
+                return true;
+
+            // Check group rights
+            if (perms::none != (perm & perms::group_write)) {
+
+                // Check main group
+                if (info.st_gid == getegid())
+                    return true;
+
+                // Get user name
+                auto pwd = getpwuid(geteuid());
+                if (! pwd)
+                    throw std::runtime_error("Unable to get user name from user"
+                                             " ID "
+                                             + std::to_string(info.st_uid)
+                                             + ": " + strerror(errno));
+
+                // Get first group IDs
+                std::vector<gid_t> groups(16);
+                int ngroups = groups.size();
+                int n = getgrouplist(pwd->pw_name, info.st_gid,
+                                     groups.data(), &ngroups);
+                if (n < 0) {
+                    groups.resize(static_cast<size_t>(n));
+                    static_cast<void>(getgrouplist(pwd->pw_name, info.st_gid,
+                                                   groups.data(), &ngroups));
+                }
+                else
+                    groups.resize(static_cast<size_t>(ngroups));
+
+                // Find if file group is in list
+                auto it = std::find(groups.begin(), groups.end(), info.st_uid);
+                if (it != groups.end()) 
+                    return true;
+          }
+        }
+        return false;
 #endif
     }
 }

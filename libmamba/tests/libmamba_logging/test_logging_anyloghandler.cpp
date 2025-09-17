4e1fae398af232657b6ddbc78ab1df2f27fae066
Klaim (Joël Lamotte) <142265+Klaim@users.noreply.github.com>
2025-09-17 17:22:22 +0200

    tests: moved utility into header

--- libmamba/tests/libmamba_logging/test_logging_anyloghandler.cpp
+++ libmamba/tests/libmamba_logging/test_logging_anyloghandler.cpp
@@ -13,15 +13,12 @@
 #include <mamba/core/logging.hpp>
 #include <mamba/util/synchronized_value.hpp>
 
+#include "test_logging_common.hpp"
+
 namespace mamba::logging
 {
     namespace testing
     {
-        struct NotALogHandler
-        {
-        };
-
-        static_assert(not LogHandler<NotALogHandler>);
 
         template <class T>
         concept CanGoIntoAnyLogHandler = requires(T x, AnyLogHandler& handler) {
@@ -33,147 +30,8 @@ namespace mamba::logging
         static_assert(not CanGoIntoAnyLogHandler<NotALogHandler>);
         static_assert(not CanGoIntoAnyLogHandler<NotALogHandler*>);
 
-        struct Stats
-        {
-            std::size_t start_count = 0;
-            std::size_t stop_count = 0;
-            std::size_t log_count = 0;
-            std::size_t log_level_change_count = 0;
-            std::size_t params_change_count = 0;
-            std::size_t backtrace_enabled_count = 0;
-            std::size_t backtrace_disabled_count = 0;
-            std::size_t backtrace_log_count = 0;
-            std::size_t backtrace_log_no_guard_count = 0;
-            std::size_t flush_all_count = 0;
-            std::size_t flush_specific_source_count = 0;
-            std::size_t flush_threshold_change_count = 0;
-
-            LoggingParams current_params = {};
-            std::size_t backtrace_size = 0;
-            log_level flush_threshold = log_level::off;
-
-            auto operator==(const Stats& other) const noexcept -> bool = default;
-        };
-
-        struct LogHandler_Tester
-        {
-            struct Impl
-            {
-                util::synchronized_value<Stats> stats;
-            };
-
-            std::unique_ptr<Impl> pimpl = std::make_unique<Impl>();
-
-            LogHandler_Tester() = default;
-
-            LogHandler_Tester(LogHandler_Tester&&) noexcept = default;
-            LogHandler_Tester& operator=(LogHandler_Tester&&) noexcept = default;
-
-            LogHandler_Tester(const LogHandler_Tester&) = delete;
-            LogHandler_Tester& operator=(const LogHandler_Tester&) = delete;
-
-            auto start_log_handling(LoggingParams params, const std::vector<log_source>&) -> void
-            {
-                auto stats = pimpl->stats.synchronize();
-                ++stats->start_count;
-                stats->current_params = std::move(params);
-            }
-
-            auto stop_log_handling(stop_reason) -> void
-            {
-                ++pimpl->stats->stop_count;
-            }
-
-            auto set_log_level(log_level new_level) -> void
-            {
-                auto stats = pimpl->stats.synchronize();
-                ++stats->log_level_change_count;
-                stats->current_params.logging_level = new_level;
-            }
-
-            auto set_params(LoggingParams new_params) -> void
-            {
-                auto stats = pimpl->stats.synchronize();
-                ++stats->params_change_count;
-                stats->current_params = std::move(new_params);
-            }
-
-            auto log(LogRecord) -> void
-            {
-                pimpl->stats->log_count++;
-            }
-
-            auto enable_backtrace(size_t record_buffer_size) -> void
-            {
-                auto stats = pimpl->stats.synchronize();
-                ++stats->backtrace_enabled_count;
-                stats->backtrace_size = record_buffer_size;
-            }
-
-            auto disable_backtrace() -> void
-            {
-                auto stats = pimpl->stats.synchronize();
-                ++stats->backtrace_disabled_count;
-                stats->backtrace_size = 0;
-            }
-
-            auto log_backtrace() -> void
-            {
-                ++pimpl->stats->backtrace_log_count;
-            }
-
-            auto log_backtrace_no_guards() -> void
-            {
-                ++pimpl->stats->backtrace_log_no_guard_count;
-            }
-
-            auto flush(std::optional<log_source> source = {}) -> void
-            {
-                if (source)
-                {
-                    ++pimpl->stats->flush_specific_source_count;
-                }
-                else
-                {
-                    ++pimpl->stats->flush_all_count;
-                }
-            }
-
-            auto set_flush_threshold(log_level threshold_level) -> void
-            {
-                auto stats = pimpl->stats.synchronize();
-                ++stats->flush_threshold_change_count;
-                stats->flush_threshold = threshold_level;
-            }
-        };
-
-        static_assert(LogHandler<LogHandler_Tester>);
         static_assert(CanGoIntoAnyLogHandler<LogHandler_Tester>);
         static_assert(CanGoIntoAnyLogHandler<LogHandler_Tester*>);
-
-        struct LogHandler_NotMovable
-        {
-            LogHandler_NotMovable() = default;
-            LogHandler_NotMovable(const LogHandler_NotMovable&) = delete;
-            LogHandler_NotMovable(LogHandler_NotMovable&&) = delete;
-            LogHandler_NotMovable& operator=(const LogHandler_NotMovable&) = delete;
-            LogHandler_NotMovable& operator=(LogHandler_NotMovable&&) = delete;
-
-            // clang-format off
-            auto start_log_handling(LoggingParams, const std::vector<log_source>&) -> void {}
-            auto stop_log_handling(stop_reason) -> void {}
-            auto set_log_level(log_level) -> void {}
-            auto set_params(LoggingParams) -> void {}
-            auto log(LogRecord) -> void {}
-            auto enable_backtrace(size_t) -> void {}
-            auto disable_backtrace() -> void {}
-            auto log_backtrace() -> void {}
-            auto log_backtrace_no_guards() -> void {}
-            auto flush(std::optional<log_source> = {}) -> void {}
-            auto set_flush_threshold(log_level) -> void {}
-
-            // clang-format on
-        };
 
         static_assert(not CanGoIntoAnyLogHandler<LogHandler_NotMovable>);
         static_assert(CanGoIntoAnyLogHandler<LogHandler_NotMovable*>);

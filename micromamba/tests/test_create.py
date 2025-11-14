1d9614d2293bb83fe31e039d10e6a875b6b26cdc
Klaim (Joël Lamotte) <142265+Klaim@users.noreply.github.com>
2025-11-14 12:48:30 +0100

    more debug info

--- micromamba/tests/test_create.py
+++ micromamba/tests/test_create.py
@@ -153,15 +153,19 @@ def test_specs(tmp_home, tmp_root_prefix, tmp_path, source, file_type, create_cm
 def test_lockfile(tmp_home, tmp_root_prefix, tmp_path, lockfile_format):
     env_prefix = tmp_path / "myenv"
 
-    spec_file = tmp_path / lockfile_name("env-lock", lockfile_format)
+    lockfile_to_use = lockfile_name("env-lock", lockfile_format)
+    print("lockfile_to_use = ", lockfile_to_use)
+
+    spec_file = tmp_path / lockfile_to_use
 
     shutil.copyfile(lockfile_path(lockfile_format), spec_file)
 
     res = helpers.create("-p", env_prefix, "-f", spec_file, "--json")
+    print("create result:", res)
     assert res["success"]
 
     packages = helpers.umamba_list("-p", env_prefix, "--json")
-    print("packages:", packages)
+    print("packages installed:", packages)
     assert any(package["name"] == "zlib" and package["version"] == "1.2.11" for package in packages)
 
 

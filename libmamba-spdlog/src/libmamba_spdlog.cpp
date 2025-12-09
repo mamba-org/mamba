744e5eafe4223a610600b9375ef692ef4bf49681
Julien Jerphanion <git@jjerphan.xyz>
2025-12-09 10:50:32 +0100

    Introduce `libmamba-spdlog-dyn`
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

---
+++ libmamba-spdlog/src/libmamba_spdlog.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2025, QuantStack and Mamba Contributors
+//
+// Distributed under the terms of the BSD 3-Clause License.
+//
+// The full license is in the file LICENSE, distributed with this software.
+
+// This file exists to create a shared library for libmamba-spdlog.
+// The implementation is header-only, but we need at least one source file
+// to create a shared library.
+
+#include <mamba/spdlog/logging_spdlog.hpp>
+
+// Empty file - all implementation is in headers

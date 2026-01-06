e3d78e55f0c32bf381ad4a8e52ce2eb5764a8883
Klaim (Joël Lamotte) <142265+Klaim@users.noreply.github.com>
2026-01-06 11:25:09 +0100

    tweak to confirm issue

--- libmamba/src/core/transaction.cpp
+++ libmamba/src/core/transaction.cpp
@@ -1387,8 +1387,9 @@ namespace mamba
                 auto channels [[maybe_unused]] = channel_context.make_channel(
                     channel_info.name,
                     channel_info.urls,
-                    specs::Channel::UrlPriorty::high  // put the urls coming form this file on top
-                                                      // of the mirrors list
+                    //specs::Channel::UrlPriorty::high  // put the urls coming form this file on top
+                    //                                  // of the mirrors list
+                    specs::Channel::UrlPriorty::low
                 );
             }
         }

e235c80de752e91fc7b05392f1578fe374739d0d
Klaim (Joël Lamotte) <142265+Klaim@users.noreply.github.com>
2026-01-09 19:28:51 +0100

    restore intended code (fails on ci)

--- libmamba/src/core/transaction.cpp
+++ libmamba/src/core/transaction.cpp
@@ -1397,9 +1397,8 @@ namespace mamba
                 auto channels [[maybe_unused]] = channel_context.make_channel(
                     channel_info.name,
                     channel_info.urls,
-                    //specs::Channel::UrlPriorty::high  // put the urls coming form this file on top
-                    //                                  // of the mirrors list
-                    specs::Channel::UrlPriorty::low // FOR TESTING PURPOSE ONLY
+                    specs::Channel::UrlPriorty::high  // put the urls coming form this file on top
+                                                      // of the mirrors list
                 );
             }
         }

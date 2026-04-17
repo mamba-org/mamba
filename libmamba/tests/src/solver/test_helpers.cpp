mamba-org/mamba

commit a310d31da9ef91ba72ff74f342d4e9f677b0feeb

Improve Solution usage (#3148)
* Rename Solution > solver::Solution

* Try noarch relink in solution space

* Remove dead code

* Move relink computation to helpers

* Refactor find_installed_python

* Add Loop control break to Solution

* Refactor find_new_python_in_solution

* Add  solution_needs_python_relink

* Change Py relink decision

* Remove dead relink_noarch

* Delete TransationContext copy ctor in favor of default move ctor.

* Refactor transaction_to_solution

* Refactor MSolver::add_request

* Free MSolver::add_reinstall_job

* Refactor Solution filter

* Refactor is_any_of

* Add solver Request for_each_of

* Remove MSolcer remove/installed_specs

* Refactor add_job_impl

* Use expected_t in add_job_impl

* Remove MSolver::m_jobs

* Refactor request_to_decision_queue

libmamba/src/solver/helpers.cpp
@@ -1,0 +1,35 @@
+// Copyright (c) 2023, QuantStack and Mamba Contributors
+//
+// Distributed under the terms of the BSD 3-Clause License.
+//
+// The full license is in the file LICENSE, distributed with this software.
+
+#include "solver/helpers.hpp"
+
+namespace mamba::solver
+{
+    auto find_new_python_in_solution(const Solution& solution)
+        -> std::optional<std::reference_wrapper<const specs::PackageInfo>>
+    {
+        auto out = std::optional<std::reference_wrapper<const specs::PackageInfo>>{};
+        for_each_to_install(
+            solution.actions,
+            [&](const auto& pkg)
+            {
+                if (pkg.name == "python")
+                {
+                    out = std::cref(pkg);
+                    return util::LoopControl::Break;
+                }
+                return util::LoopControl::Continue;
+            }
+        );
+        return out;
+    }
+
+    auto python_binary_compatible(const specs::Version& older, const specs::Version& newer) -> bool
+    {
+        // Python binary compatiblity is defined athe the same MINOR level.
+        return older.compatible_with(newer, /* level= */ 2);
+    }
+}

libmamba/CMakeLists.txt
@@ -184,6 +184,7 @@ set(
     ${LIBMAMBA_SOURCE_DIR}/solv-cpp/solver.cpp
     ${LIBMAMBA_SOURCE_DIR}/solv-cpp/transaction.cpp
     # Solver interface
+    ${LIBMAMBA_SOURCE_DIR}/solver/helpers.cpp
     ${LIBMAMBA_SOURCE_DIR}/solver/libsolv/helpers.cpp
     ${LIBMAMBA_SOURCE_DIR}/solver/libsolv/parameters.cpp
     ${LIBMAMBA_SOURCE_DIR}/solver/libsolv/repo_info.cpp
@@ -281,6 +282,7 @@ set(
     ${LIBMAMBA_INCLUDE_DIR}/mamba/util/heap_optional.hpp
     ${LIBMAMBA_INCLUDE_DIR}/mamba/util/iterator.hpp
     ${LIBMAMBA_INCLUDE_DIR}/mamba/util/json.hpp
+    ${LIBMAMBA_INCLUDE_DIR}/mamba/util/loop_control.hpp
     ${LIBMAMBA_INCLUDE_DIR}/mamba/util/os_win.hpp
     ${LIBMAMBA_INCLUDE_DIR}/mamba/util/parsers.hpp
     ${LIBMAMBA_INCLUDE_DIR}/mamba/util/path_manip.hpp
@@ -307,6 +309,7 @@ set(
     ${LIBMAMBA_INCLUDE_DIR}/mamba/specs/version_spec.hpp
     # Solver Interface
     ${LIBMAMBA_INCLUDE_DIR}/mamba/solver/request.hpp
+    ${LIBMAMBA_INCLUDE_DIR}/mamba/solver/solution.hpp
     ${LIBMAMBA_INCLUDE_DIR}/mamba/solver/libsolv/parameters.hpp
     ${LIBMAMBA_INCLUDE_DIR}/mamba/solver/libsolv/repo_info.hpp
     # Artifacts validation
@@ -343,7 +346,6 @@ set(
     ${LIBMAMBA_INCLUDE_DIR}/mamba/core/query.hpp
     ${LIBMAMBA_INCLUDE_DIR}/mamba/core/run.hpp
     ${LIBMAMBA_INCLUDE_DIR}/mamba/core/shell_init.hpp
-    ${LIBMAMBA_INCLUDE_DIR}/mamba/core/solution.hpp
     ${LIBMAMBA_INCLUDE_DIR}/mamba/core/solver.hpp
     ${LIBMAMBA_INCLUDE_DIR}/mamba/core/subdirdata.hpp
     ${LIBMAMBA_INCLUDE_DIR}/mamba/core/thread_utils.hpp

libmamba/include/mamba/core/solver.hpp
@@ -22,12 +22,10 @@
 #include "mamba/core/pool.hpp"
 #include "mamba/core/satisfiability_error.hpp"
 #include "mamba/solver/request.hpp"
-#include "mamba/specs/match_spec.hpp"
 #include "mamba/specs/package_info.hpp"
 
 namespace mamba::solv
 {
-    class ObjQueue;
     class ObjSolver;
 }
 
@@ -70,8 +68,6 @@ namespace mamba
         MSolver(MSolver&&);
         MSolver& operator=(MSolver&&);
 
-        void add_request(const Request& request);
-
         void set_flags(const Flags& flags);  // TODO temporary Itf meant to be passed in ctor
         [[nodiscard]] auto flags() const -> const Flags&;
         [[deprecated]] void py_set_libsolv_flags(const std::vector<std::pair<int, int>>& flags);
@@ -92,8 +88,8 @@ namespace mamba
         [[nodiscard]] MPool& pool() &;
         [[nodiscard]] MPool&& pool() &&;
 
-        [[nodiscard]] const std::vector<specs::MatchSpec>& install_specs() const;
-        [[nodiscard]] const std::vector<specs::MatchSpec>& remove_specs() const;
+        void set_request(Request request);
+        [[nodiscard]] const Request& request() const;
 
         auto solver() -> solv::ObjSolver&;
         auto solver() const -> const solv::ObjSolver&;
@@ -101,26 +97,15 @@ namespace mamba
     private:
 
         std::vector<std::pair<int, int>> m_libsolv_flags;
-        std::vector<specs::MatchSpec> m_install_specs;
-        std::vector<specs::MatchSpec> m_remove_specs;
+        Request m_request;
         // Order of m_pool and m_solver is critical since m_pool must outlive m_solver.
         MPool m_pool;
         // Temporary Pimpl all libsolv to keep it private
         std::unique_ptr<solv::ObjSolver> m_solver;
-        std::unique_ptr<solv::ObjQueue> m_jobs;
         Flags m_flags = {};
         bool m_is_solved;
 
-        void add_reinstall_job(const specs::MatchSpec& ms, int job_flag);
         void apply_libsolv_flags();
-
-        void add_job_impl(const Request::Install& job);
-        void add_job_impl(const Request::Remove& job);
-        void add_job_impl(const Request::Update& job);
-        void add_job_impl(const Request::UpdateAll& job);
-        void add_job_impl(const Request::Freeze& job);
-        void add_job_impl(const Request::Keep& job);
-        void add_job_impl(const Request::Pin& job);
     };
 }  // namespace mamba
 

libmamba/include/mamba/core/transaction.hpp
@@ -15,9 +15,9 @@
 #include "mamba/core/package_cache.hpp"
 #include "mamba/core/pool.hpp"
 #include "mamba/core/prefix_data.hpp"
-#include "mamba/core/solution.hpp"
 #include "mamba/core/transaction_context.hpp"
 #include "mamba/fs/filesystem.hpp"
+#include "mamba/solver/solution.hpp"
 #include "mamba/specs/match_spec.hpp"
 #include "mamba/specs/package_info.hpp"
 
@@ -67,7 +67,7 @@ namespace mamba
         TransactionContext m_transaction_context;
         MultiPackageCache m_multi_cache;
         const fs::u8path m_cache_path;
-        Solution m_solution;
+        solver::Solution m_solution;
 
         History::UserRequest m_history_entry;
 

libmamba/include/mamba/core/transaction_context.hpp
@@ -33,10 +33,12 @@ namespace mamba
     public:
 
         TransactionContext();
+        TransactionContext(TransactionContext&&) = default;
 
         explicit TransactionContext(const Context& context);
 
-        TransactionContext& operator=(const TransactionContext&);
+        TransactionContext& operator=(TransactionContext&&) = default;
+
         TransactionContext(
             const Context& context,
             const fs::u8path& target_prefix,
@@ -68,7 +70,6 @@ namespace mamba
         bool always_softlink = false;
         bool compile_pyc = true;
         // this needs to be done when python version changes
-        bool relink_noarch = false;
         std::vector<specs::MatchSpec> requested_specs;
 
         const Context& context() const

libmamba/include/mamba/solver/request.hpp
@@ -7,9 +7,12 @@
 #ifndef MAMBA_SOLVER_REQUEST_HPP
 #define MAMBA_SOLVER_REQUEST_HPP
 
+#include <type_traits>
 #include <variant>
 
 #include "mamba/specs/match_spec.hpp"
+#include "mamba/util/loop_control.hpp"
+#include "mamba/util/type_traits.hpp"
 
 namespace mamba::solver
 {
@@ -63,5 +66,36 @@ namespace mamba::solver
 
         item_list items = {};
     };
+
+    template <typename... Item, typename Func>
+    void for_each_of(const Request& request, Func&& func)
+    {
+        for (const auto& unknown_itm : request.items)
+        {
+            const auto control = std::visit(
+                [&](const auto& itm) -> util::LoopControl
+                {
+                    using Itm = std::decay_t<decltype(itm)>;
+                    if constexpr (util::is_any_of_v<Itm, Item...>)
+                    {
+                        if constexpr (std::is_same_v<decltype(func(itm)), util::LoopControl>)
+                        {
+                            return func(itm);
+                        }
+                        else
+                        {
+                            func(itm);
+                        }
+                    }
+                    return util::LoopControl::Continue;
+                },
+                unknown_itm
+            );
+            if (control == util::LoopControl::Break)
+            {
+                break;
+            }
+        }
+    }
 }
 #endif

libmamba/include/mamba/solver/solution.hpp
@@ -12,15 +12,11 @@
 #include <vector>
 
 #include "mamba/specs/package_info.hpp"
+#include "mamba/util/loop_control.hpp"
+#include "mamba/util/type_traits.hpp"
 
-namespace mamba
+namespace mamba::solver
 {
-    namespace detail
-    {
-        template <typename T, typename... U>
-        inline constexpr bool is_any_of_v = std::disjunction_v<std::is_same<T, U>...>;
-    }
-
     struct Solution
     {
         struct Omit
@@ -62,10 +58,10 @@ namespace mamba
         };
 
         template <typename T>
-        inline static constexpr bool has_remove_v = detail::is_any_of_v<T, Upgrade, Downgrade, Change, Remove>;
+        inline static constexpr bool has_remove_v = util::is_any_of_v<T, Upgrade, Downgrade, Change, Remove>;
 
         template <typename T>
-        inline static constexpr bool has_install_v = detail::is_any_of_v<T, Upgrade, Downgrade, Change, Install>;
+        inline static constexpr bool has_install_v = util::is_any_of_v<T, Upgrade, Downgrade, Change, Install>;
 
         using Action = std::variant<Omit, Upgrade, Downgrade, Change, Reinstall, Remove, Install>;
         using action_list = std::vector<Action>;
@@ -118,6 +114,7 @@ namespace mamba
         }
     }
 
+    // TODO(C++20): Poor man's replacement to range filter transform
     template <typename Iter, typename UnaryFunc>
     void for_each_to_remove(Iter first, Iter last, UnaryFunc&& func)
     {
@@ -125,7 +122,17 @@ namespace mamba
         {
             if (auto* const ptr = detail::to_remove_ptr(*first))
             {
-                func(*ptr);
+                if constexpr (std::is_same_v<decltype(func(*ptr)), util::LoopControl>)
+                {
+                    if (func(*ptr) == util::LoopControl::Break)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    func(*ptr);
+                }
             }
         }
     }
@@ -169,11 +176,22 @@ namespace mamba
         {
             if (auto* const ptr = detail::to_install_ptr(*first))
             {
-                func(*ptr);
+                if constexpr (std::is_same_v<decltype(func(*ptr)), util::LoopControl>)
+                {
+                    if (func(*ptr) == util::LoopControl::Break)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    func(*ptr);
+                }
             }
         }
     }
 
+    // TODO(C++20): Poor man's replacement to range filter transform
     template <typename Range, typename UnaryFunc>
     void for_each_to_install(Range&& actions, UnaryFunc&& func)
     {
@@ -202,6 +220,7 @@ namespace mamba
         }
     }
 
+    // TODO(C++20): Poor man's replacement to range filter transform
     template <typename Iter, typename UnaryFunc>
     void for_each_to_omit(Iter first, Iter last, UnaryFunc&& func)
     {
@@ -209,7 +228,17 @@ namespace mamba
         {
             if (auto* const ptr = detail::to_omit_ptr(*first))
             {
-                func(*ptr);
+                if constexpr (std::is_same_v<decltype(func(*ptr)), util::LoopControl>)
+                {
+                    if (func(*ptr) == util::LoopControl::Break)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    func(*ptr);
+                }
             }
         }
     }

commit 2ad46a20398050e20b55bdf11a4ef31b3200d202

fix: relink noarch:python only on major.minor Python change
`solution_needs_python_relink` decides whether every installed
`noarch: python` package must be added to the transaction as a
`Solution::Reinstall` because the Python interpreter's ABI has changed.
The helper it relied on, `python_binary_compatible`, called
`Version::compatible_with` with the arguments in the wrong order:

```cpp
return older.compatible_with(newer, /* level= */ 2);  // wrong
```

`Version::compatible_with` is asymmetric (`>=` within `level`) and the
project convention is `newer.compatible_with(older, level)` (see
tests/src/specs/test_version.cpp). The reversed call reported every
real Python upgrade as ABI-incompatible, so every noarch:python package
was reinstalled on every Python micro upgrade (e.g. 3.11.14 -> 3.11.15).

Usually that just meant extra churn, but combined with
`conda/constructor`'s `transmute_file_type: .conda` option the fake
`.conda` URLs recorded in `conda-meta/*.json` turn those reinstalls
into 404s (the upstream channel only hosts the `.tar.bz2` variant).

Replace the `compatible_with` call with a direct major+minor equality
check. This matches conda's noarch:python relink gate in
conda/core/solve.py:

```python
gmm = get_major_minor_version
if gmm(prev_python.version) != gmm(curr_python.version):
    ...
```

and is symmetric across upgrade/downgrade.

The helper has been wrong since it was introduced in #3148 (Jan 2024);
its inline comment ("Python binary compatibility is defined at the same
MINOR level") already stated the intended semantics.

Also add `libmamba/tests/src/solver/test_helpers.cpp` covering the four
cases: identical version, same-minor upgrade, same-minor downgrade, and
different minor.

libmamba/src/solver/helpers.cpp
@@ -22,9 +22,18 @@ namespace mamba::solver
         return std::nullopt;
     }
 
-    auto python_binary_compatible(const specs::Version& older, const specs::Version& newer) -> bool
+    auto python_binary_compatible(const specs::Version& a, const specs::Version& b) -> bool
     {
-        // Python binary compatibility is defined at the same MINOR level.
-        return older.compatible_with(newer, /* level= */ 2);
+        // Python binary compatibility is defined at the same major + minor
+        // level. This matches conda's noarch:python relink gate in
+        // conda/core/solve.py, which compares
+        //   get_major_minor_version(prev) != get_major_minor_version(curr)
+        // and is symmetric across upgrade/downgrade.
+        const auto& a_ver = a.version();
+        const auto& b_ver = b.version();
+        return a.epoch() == b.epoch()                     //
+               && a_ver.size() >= 2 && b_ver.size() >= 2  //
+               && a_ver[0] == b_ver[0]                    //
+               && a_ver[1] == b_ver[1];
     }
 }

libmamba/tests/CMakeLists.txt
@@ -70,6 +70,7 @@ set(
     src/specs/test_version_spec.cpp
     src/specs/test_version.cpp
     # Solver tests
+    src/solver/test_helpers.cpp
     src/solver/test_problems_graph.cpp
     src/solver/test_request.cpp
     src/solver/test_solution.cpp

libmamba/tests/src/solver/test_helpers.cpp
@@ -1,0 +1,71 @@
+// Copyright (c) 2026, QuantStack and Mamba Contributors
+//
+// Distributed under the terms of the BSD 3-Clause License.
+//
+// The full license is in the file LICENSE, distributed with this software.
+
+#include <catch2/catch_all.hpp>
+
+#include "mamba/specs/version.hpp"
+
+#include "solver/helpers.hpp"
+
+using namespace mamba;
+using namespace mamba::solver;
+
+namespace
+{
+    auto parse_version(std::string_view s) -> specs::Version
+    {
+        return specs::Version::parse(s).value();
+    }
+
+    TEST_CASE("python_binary_compatible", "[mamba::solver]")
+    {
+        SECTION("identical versions are compatible")
+        {
+            const auto v = parse_version("3.11.14");
+            REQUIRE(python_binary_compatible(v, v));
+        }
+
+        SECTION("same-minor micro upgrade is compatible")
+        {
+            // Regression test for the argument-order bug in
+            // python_binary_compatible: the body used to call
+            //   older.compatible_with(newer, 2)
+            // but Version::compatible_with follows the convention
+            //   newer.compatible_with(older, level)
+            // (cf. libmamba/tests/src/specs/test_version.cpp).  The reversed
+            // call reported every real Python upgrade as ABI-incompatible,
+            // which caused solution_needs_python_relink() to emit a
+            // Solution::Reinstall for every installed noarch:python package
+            // on every micro upgrade.
+            const auto older = parse_version("3.11.14");
+            const auto newer = parse_version("3.11.15");
+            REQUIRE(python_binary_compatible(older, newer));
+        }
+
+        SECTION("same-minor micro downgrade is compatible")
+        {
+            // Python ABI compatibility is symmetric within a minor series,
+            // matching conda's get_major_minor_version equality check.
+            const auto newer = parse_version("3.11.15");
+            const auto older = parse_version("3.11.14");
+            REQUIRE(python_binary_compatible(newer, older));
+        }
+
+        SECTION("different minor versions are not compatible")
+        {
+            const auto older = parse_version("3.11.14");
+            const auto newer = parse_version("3.12.0");
+            REQUIRE_FALSE(python_binary_compatible(older, newer));
+        }
+
+        SECTION("different major versions are not compatible")
+        {
+            const auto older = parse_version("2.7.18");
+            const auto newer = parse_version("3.11.14");
+            REQUIRE_FALSE(python_binary_compatible(older, newer));
+        }
+    }
+}
mamba-org/mamba

commit 821de6b5c1178ce026c1ba988fae78b4eaaafaff

also complete for deactivate command (#1557)

micromamba/src/completer.cpp
@@ -90,8 +90,9 @@ add_activate_completion(CLI::App* app, std::vector<std::string>& completer_args,
     auto* current_subcom = app->get_subcommand("activate");
     app->remove_subcommand(current_subcom);
 
-    CLI::App* activate_subcom
-        = app->add_subcommand("activate", "Mock activate shell function for completion");
+    // Mock functions just for completion
+    CLI::App* activate_subcom = app->add_subcommand("activate");
+    CLI::App* deactivate_subcom = app->add_subcommand("deactivate");
     activate_subcom->callback(
         [app, &completer_args, &completed]()
         {
@@ -109,13 +110,12 @@ add_ps_completion(CLI::App* app, std::vector<std::string>& completer_args, bool&
     auto* current_subcom = app->get_subcommand("ps");
     app->remove_subcommand(current_subcom);
 
-    CLI::App* ps_subcom = app->add_subcommand("ps", "Mock ps function for completion");
+    // Mock functions just for completion
+    CLI::App* ps_subcom = app->add_subcommand("ps");
 
-    CLI::App* stop_subcom
-        = ps_subcom->add_subcommand("stop", "Mock ps stop function for completion");
+    CLI::App* stop_subcom = ps_subcom->add_subcommand("stop");
 
-    CLI::App* list_subcom
-        = ps_subcom->add_subcommand("list", "Mock ps list function for completion");
+    CLI::App* list_subcom = ps_subcom->add_subcommand("list");
 
     ps_subcom->callback([ps_subcom, &completer_args, &completed]()
                         { complete_options(ps_subcom, completer_args, completed); });

commit ed769f07729b981c78b23c3359b0f7b7cc730359

micromamba: remove duplicate deactivate from shell completion
When implementing the shell completion, the "deactivate" command is
introduced manually in add_activate_completion(). When first introduced
by 821de6b5 (also complete for deactivate command (#1557), 2022-03-03)
this was needed (although probably in the wrong function), because
micromamba didn't have a proper "deactivate" command.

However, commit 5206c2a9 (fix: Add `deactivate` command to the help
message (#4350), 2026-07-29) added this missing command to
set_umamba_command() (that is also used for handling the "completer"
command), but it didn't remove the extra "deactivate". Because of that,
running the autocompletion triggers the following error:

    terminate called after throwing an instance of 'CLI::OptionAlreadyAdded'
    what():  subcommand name or alias matches existing subcommand: deactivate is already added

Drop the manual and redundant addition of "deactivate" command in the
completer implementation, so the autocompletion gets back to working as
expected.

Fixes: 5206c2a9 (fix: Add `deactivate` command to the help message (#4350), 2026-07-29)
Signed-off-by: Henrique F. Simoes <henrique.simoes@lnls.br>

micromamba/src/completer.cpp
@@ -134,7 +134,6 @@ add_activate_completion(
 
     // Mock functions just for completion
     CLI::App* activate_subcom = app->add_subcommand("activate");
-    app->add_subcommand("deactivate");
     activate_subcom->callback(
         [app, &completer_args, &completed, &config]()
         {
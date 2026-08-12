a20941b0b2943f02ab89e40e5901f4d359a12cf0
Hind Montassif <hind.montassif@gmail.com>
2026-08-12 09:37:31 +0200

    Reapply "Test output in case of failure"
    This reverts commit a58e2c5d21950b778e9008392d2a9d83f67b5d0e.

--- libmamba/src/core/shell_init.cpp
+++ libmamba/src/core/shell_init.cpp
@@ -803,13 +803,13 @@ namespace mamba
         // The default root prefix is validated on the next invocation, so it must
         // be a valid conda prefix (with `pkgs`, `conda-meta` or `envs` folder) from the start.
         // It used to contain only the hook scripts -> every following run aborted silently.
-        std::error_code ec;
-        fs::create_directories(root_prefix / "conda-meta", ec);
-        if (ec)
-        {
-            LOG_ERROR << "Failed to create directory '" << (root_prefix / "conda-meta").string()
-                      << "' : " << ec.message();
-        }
+        // std::error_code ec;
+        // fs::create_directories(root_prefix / "conda-meta", ec);
+        // if (ec)
+        // {
+        //     LOG_ERROR << "Failed to create directory '" << (root_prefix / "conda-meta").string()
+        //               << "' : " << ec.message();
+        // }
         for (const auto& directory : paths.every_generated_directories_paths())
         {
             // Maybe the prefix isn't writable. No big deal, just keep going.
